9d781ab582ca2fa5db3d74d2a64c8c571c6eca4a
Daniel Karlsson <daniel@kardans.com>
2016-01-15 15:18:13 +0100

    [#3] Remove default settings
    Cleaned up the default database config from stettings.py

--- backend/runway/settings.py
+++ backend/runway/settings.py
@@ -84,13 +84,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
