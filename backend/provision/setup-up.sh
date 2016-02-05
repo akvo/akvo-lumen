8639f6f1d336265d49e0196577ffd8d976ec31ab
Daniel Karlsson <daniel@kardans.com>
2016-02-05 15:44:56 +0100

    [#20] Updated create db script
    Removed encoding and also a small note at the bottom.

--- backend/provision/setup-up.sh
+++ backend/provision/setup-up.sh
@@ -11,24 +11,9 @@ psql -c "CREATE ROLE dash WITH PASSWORD 'password' CREATEDB LOGIN;"
 psql -c "
 CREATE DATABASE dash
 OWNER dash
-ENCODING 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8'
 TEMPLATE template0;"
 
 
 echo ""
 echo "----------"
 echo "Done!"
-
-
-# Notes
-
-# One role to all tenants or should we have a role per tenant?
-# Maybe it's most logical to view roles as part of the database, and
-# give that also isolation.
-# psql -c "CREATE ROLE dash_user WITH PASSWORD 'password' LOGIN"
-# Setup Dash lord db
-# http://www.postgresql.org/docs/current/interactive/locale.html
-# http://www.postgresql.org/docs/current/interactive/collation.html
-# In the future change template_1 since we might create other dbs?
-
-# http://stackoverflow.com/questions/18389124/simulate-create-database-if-not-exists-for-postgresql
