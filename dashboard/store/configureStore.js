a1123c431f0522faf29e148f2e4b3298b27f29e3
gabemart <g4bemart@gmail.com>
2016-02-11 10:43:59 +0200

    [#34] Remove `dashboard` poc folder

--- dashboard/store/configureStore.js
+++
@@ -1,8 +0,0 @@
-/*
- * See https://github.com/gaearon/redux-devtools
- */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod');
-} else {
-  module.exports = require('./configureStore.dev');
-}
