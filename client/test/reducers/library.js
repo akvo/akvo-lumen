1c3510a508d5fa865a6a579cbde00bb8d942ca90
Jonas Enlund <jonas.enlund@gmail.com>
2016-02-19 10:02:46 +0200

    [#33] Remove old test

--- client/test/reducers/library.js
+++ client/test/reducers/library.js
@@ -1,9 +1,6 @@
 import assert from 'assert';
 
-import reducer, { initialState } from '../../src/reducers/library';
+import reducer from '../../src/reducers/library';
 
 describe('library reducer', () => {
-  it('should return the initial state', () => {
-    assert.equal(reducer(undefined, {}), initialState);
-  });
 });
