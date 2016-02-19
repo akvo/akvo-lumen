e0edbd617067d77f078b0d42160c246936c3ab14
Daniel Karlsson <daniel@kardans.com>
2016-02-19 06:31:15 +0100

    [#33] Added a broken last_modified field to library endpoint
    The last modified does not cover change of dataset name. To support
    that we need to either add a last_update field on the dataview table
    (with trigger?) or we can create a new table for dataset “data”. Then
    we don’t need to make updates but only inserts.

--- backend/src/org/akvo/dash/endpoint/library.sql
+++ backend/src/org/akvo/dash/endpoint/library.sql
@@ -1,12 +1,15 @@
 -- :name dataset-coll :? :*
 -- :doc returns the dataset collection
-SELECT DISTINCT ON (v.id)
-v.id AS view_id, v.dataset_name, j.status, j.ts AS modified, v.ts AS created
-FROM dataview v
-LEFT JOIN datasources s ON v.datasource = s.id
-LEFT JOIN (
-     SELECT DISTINCT ON (i.datasource)
-     i.id, i.datasource, i.revision, i.status, i.ts, r.noble
-     FROM imports i LEFT JOIN revisions r ON i.revision = r.digest
-     ORDER BY i.datasource, i.id DESC) j ON s.id = j.datasource
-ORDER BY v.id DESC
+SELECT a.view_id, a.dataset_name, a.status, GREATEST(a.trans_ts, a.import_ts, a.view_ts) AS last_modified, a.view_ts AS created
+FROM (
+     SELECT DISTINCT ON (v.id)
+     v.id AS view_id, v.dataset_name, j.status, t.id AS transformation_id, t.ts AS trans_ts, j.ts AS import_ts, v.ts AS view_ts
+     FROM dataview v
+     LEFT JOIN datasources s ON v.datasource = s.id
+     LEFT JOIN transformations t ON v.transformation = t.id
+     LEFT JOIN (
+          SELECT DISTINCT ON (i.datasource)
+          i.id, i.datasource, i.revision, i.status, i.ts, r.noble
+          FROM imports i LEFT JOIN revisions r ON i.revision = r.digest
+          ORDER BY i.datasource, i.id DESC) j ON s.id = j.datasource
+     ORDER BY v.id DESC) a
