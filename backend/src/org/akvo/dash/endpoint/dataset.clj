8ce687294235049a775753508db9ce0f28a9c018
Daniel Karlsson <daniel@kardans.com>
2016-02-19 17:46:29 +0100

    [#41] Use render response fn in dataset endpoints

--- backend/src/org/akvo/dash/endpoint/dataset.clj
+++ backend/src/org/akvo/dash/endpoint/dataset.clj
@@ -7,6 +7,7 @@
    [hugsql.core :as hugsql]
    [immutant.scheduling :as scheduling]
    [org.akvo.dash.endpoint.util :as u]
+   [org.akvo.dash.endpoint.util :refer [rr]]
    [org.akvo.dash.import :as import]
    [pandect.algo.sha1 :refer [sha1]]
    [ring.util.io :refer [string-input-stream]]))
@@ -21,6 +22,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; HUGSQL fns
 
+(hugsql/def-db-fns "org/akvo/dash/endpoint/library.sql")
+
+;; Organise!
 (hugsql/def-db-fns "org/akvo/dash/endpoint/sql/datasource.sql")
 (hugsql/def-db-fns "org/akvo/dash/endpoint/sql/import.sql")
 (hugsql/def-db-fns "org/akvo/dash/endpoint/sql/dataview.sql")
@@ -48,7 +52,10 @@
   (context "/datasets" []
 
     (GET "/" []
-      (u/fn->json-resp (all-datasets db)))
+      (rr (dataset-coll db)))
+
+    ;; (GET "/" []
+    ;;   (u/fn->json-resp (all-datasets db)))
 
     ;; POST blindly expect LINK kind
     (POST "/" [req]
@@ -104,24 +111,19 @@
     (context "/:id" [id]
 
       (GET "/" []
-
-        (let [r (dataset-by-id db {:id (str->uuid id)})
-              ;; Made error msg top level to show it's not implemented
-              e "Could not find dataset on remote server (404)"]
+        (let [r (dataset-by-id db
+                               {:id (str->uuid id)})]
           (if (nil? r)
-            {:status  404
-             :headers {"content-type" "text/plain"}
-             :body    "404 Not Found"}
-            (let [resp-map {:id     (str (:view_id r))
-                            :status (:status r)
-                            :name   (:dataset_name r)}
-                  resp     (condp = (:status r)
-                             "OK"      (assoc resp-map
-                                              :columns
-                                              (json/parse-string
-                                               (slurp (-> r :noble :noble))))
-                             "FAILURE" (assoc resp-map :reason e)
-                             resp-map)]
-              {:status  200
-               :headers {"content-type" "application/json"}
-               :body    (json/generate-string resp)})))))))
+            (rr "404 Not Found"
+                {:status  404
+                 :headers {"content-type" "text"}})
+            (rr
+             (let [resp-map (select-keys r [:id :name :status])]
+               (condp = (:status r)
+                 "OK"      (assoc resp-map
+                                  :columns (json/parse-string
+                                            (slurp (-> r :noble :noble))))
+                 "FAILURE" (assoc resp-map
+                                  :reason
+                                  "Could not find dataset on remote server (404)")
+                 resp-map))))))))) ;; "PENDING" do nothing
