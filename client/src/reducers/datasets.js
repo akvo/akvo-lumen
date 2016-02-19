d653521fa80b11112f8d44489a850fbae37f643d
Jonas Enlund <jonas.enlund@gmail.com>
2016-02-19 21:21:04 +0200

    [#33] Remove mock datasets

--- client/src/reducers/datasets.js
+++ client/src/reducers/datasets.js
@@ -1,40 +1,7 @@
 import * as constants from '../constants/dataset';
 import update from 'react-addons-update';
 
-export const initialState = {
-  101: {
-    id: 101,
-    name: 'dataset 1',
-    type: 'dataset',
-    created: '1449108685570',
-    modified: '1459109685570',
-    source: {
-      type: 'LINK',
-      url: 'http://www.example.com/example.csv',
-    },
-  },
-  105: {
-    id: 105,
-    name: 'dataset 2',
-    type: 'dataset',
-    created: '1430489504345',
-    source: {
-      type: 'LINK',
-      url: 'http://www.example.com/example.csv',
-    },
-  },
-  110: {
-    id: 110,
-    name: 'dataset 3',
-    type: 'dataset',
-    created: '1453895504081',
-    modified: '1459229685570',
-    source: {
-      type: 'LINK',
-      url: 'http://www.example.com/example.csv',
-    },
-  },
-};
+export const initialState = {};
 
 function createDataset(state, dataset) {
   const id = dataset.id;
