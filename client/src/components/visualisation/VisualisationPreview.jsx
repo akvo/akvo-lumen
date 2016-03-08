c2b375bca8e5998164c74e93668ec9e9316d96ea
gabemart <g4bemart@gmail.com>
2016-03-08 15:37:50 +0000

    [#70] Fix 0th column visualisation bug

--- client/src/components/visualisation/VisualisationPreview.jsx
+++ client/src/components/visualisation/VisualisationPreview.jsx
@@ -11,11 +11,14 @@ const getChartPreview = (visualisation, datasets) => {
   let output;
   let datasetColumn;
 
+  console.log("BINK");
+  console.dir(visualisation);
+
   switch (visualisation.visualisationType) {
     case 'bar':
       datasetColumn = visualisation.datasetColumnX;
 
-      if (datasetColumn) {
+      if (datasetColumn !== null) {
         output = <DashBarChart visualisation={visualisation} datasets={datasets} />;
       } else {
         output = <div>Pie chart image placeholder</div>;
@@ -26,7 +29,7 @@ const getChartPreview = (visualisation, datasets) => {
     case 'line':
       datasetColumn = visualisation.datasetColumnX;
 
-      if (datasetColumn) {
+      if (datasetColumn !== null) {
         output = <DashLineChart visualisation={visualisation} datasets={datasets} />;
       } else {
         output = <div>Line chart image placeholder</div>;
@@ -37,7 +40,7 @@ const getChartPreview = (visualisation, datasets) => {
     case 'area':
       datasetColumn = visualisation.datasetColumnX;
 
-      if (datasetColumn) {
+      if (datasetColumn !== null) {
         output = <DashAreaChart visualisation={visualisation} datasets={datasets} />;
       } else {
         output = <div>Area chart image placeholder</div>;
@@ -49,7 +52,7 @@ const getChartPreview = (visualisation, datasets) => {
     case 'pie':
       datasetColumn = visualisation.datasetColumnX;
 
-      if (datasetColumn) {
+      if (datasetColumn !== null) {
         output = <DashPieChart visualisation={visualisation} datasets={datasets} />;
       } else {
         output = <div>Pie chart image placeholder</div>;
@@ -58,7 +61,7 @@ const getChartPreview = (visualisation, datasets) => {
       return output;
 
     case 'scatter':
-      if (visualisation.datasetColumnX && visualisation.datasetColumnY) {
+      if (visualisation.datasetColumnX !== null && visualisation.datasetColumnY !== null) {
         output = <DashScatterChart visualisation={visualisation} datasets={datasets} />;
       } else {
         output = <div>Scatter chart image placeholder</div>;
