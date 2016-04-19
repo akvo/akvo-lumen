cd732397be9b3eea20040b028658c93d5a31f5f3
Daniel Karlsson <daniel@kardans.com>
2016-04-19 08:33:42 +0200

    [#91] Tenant manager migrations creates tables using like
    Also changed tenants to singular table name tenant.

---
+++ backend/resources/org/akvo/dash/migrations_tenant_manager/001-tenant.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS tenant (
+       id serial,
+       db_uri text,
+       enabled BOOLEAN NOT NULL DEFAULT TRUE,
+       label text,
+       title text
+);
+
+GRANT ALL ON tenant TO dash;
+
+CREATE TRIGGER tenant_history BEFORE
+INSERT OR DELETE OR UPDATE ON tenant
+FOR EACH ROW EXECUTE PROCEDURE history.log_change();
+
+
+--;;
+CREATE TABLE IF NOT EXISTS history.tenant (
+       LIKE public.tenant, _validrange tstzrange NOT NULL);
+
+GRANT ALL ON tenant TO dash;
+
+ALTER TABLE ONLY history.tenant
+ADD CONSTRAINT tenant_exclusion EXCLUDE
+USING gist (id WITH =, _validrange WITH &&);
+--;;
