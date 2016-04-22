504467344ca51555b19f4d30f29d071da5290aa8
Jonas Enlund <jonas.enlund@gmail.com>
2016-04-22 11:07:43 +0300

    [#84][#85] Docstring for make-dataset-data-table multimethod

--- backend/src/org/akvo/dash/import/common.clj
+++ backend/src/org/akvo/dash/import/common.clj
@@ -2,6 +2,19 @@
   (:require [cheshire.core :as json]))
 
 (defmulti make-dataset-data-table
+  "Creates a dataset data table with data populated according to the
+  data source spec. Returns a status map with keys
+
+  :success? - true if import was successful, false otherwise
+  :reason - Short description of the reason for a failed import
+  :columns - A sequence of 3-tuples of the form
+
+                 [title column-name type]
+
+             where title is the user visible title of the column,
+             column-name is the name of the column in the generated
+             table and type is the user visible type of the column
+             data (string/number/date)"
   (fn [tenant-conn config table-name spec]
     (get spec "type")))
 
