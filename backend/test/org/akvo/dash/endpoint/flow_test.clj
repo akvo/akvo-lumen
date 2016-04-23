17cfe7ec9681cfafb168c77608254b0e2a67a14b
Jonas Enlund <jonas.enlund@gmail.com>
2016-04-23 14:11:26 +0300

    [#85] Remove dummy test

--- backend/test/org/akvo/dash/endpoint/flow_test.clj
+++ backend/test/org/akvo/dash/endpoint/flow_test.clj
@@ -1,9 +1,3 @@
 (ns org.akvo.dash.endpoint.flow-test
   (:require [clojure.test :refer :all]
             [org.akvo.dash.endpoint.flow :as flow]))
-
-
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
