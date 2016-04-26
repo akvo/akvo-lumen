9556a8c4694fea1cb80da652442e96a4603c9fdb
Gabriel von Heijne <gabriel@akvo.org>
2016-04-26 22:26:54 +0200

    [#78] Add provisioning scripts for test databases
    All test DBs have the same name as the real ones with "test_" prepended to their names

--- backend/provision/helpers/create-dash.sql
+++ backend/provision/helpers/create-dash.sql
@@ -12,3 +12,10 @@ WITH OWNER = dash
      ENCODING = 'UTF8'
      LC_COLLATE = 'en_US.UTF-8'
      LC_CTYPE = 'en_US.UTF-8';
+
+CREATE DATABASE test_dash
+WITH OWNER = dash
+     TEMPLATE = template0
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.UTF-8'
+     LC_CTYPE = 'en_US.UTF-8';
--- backend/provision/tear-down.sh
+++ backend/provision/tear-down.sh
@@ -7,10 +7,9 @@ at http://postgresapp.com/documentation/cli-tools.html. Aborting."
 hash psql 2>/dev/null || { echo >&2 $CLI_ERR_MSG ; exit 1; }
 
 # Clean up test dbs
-psql -c "DROP DATABASE IF EXISTS dash_test_tenant_2;"
-psql -c "DROP DATABASE IF EXISTS dash_test_tenant_1;"
-psql -c "DROP DATABASE IF EXISTS dash_test;"
-psql -c "DROP USER IF EXISTS dash_test;"
+psql -c "DROP DATABASE IF EXISTS test_dash_tenant_2;"
+psql -c "DROP DATABASE IF EXISTS test_dash_tenant_1;"
+psql -c "DROP DATABASE IF EXISTS test_dash;"
 
 # Clean up dev dbs
 psql -c "DROP DATABASE IF EXISTS dash_tenant_2;"
