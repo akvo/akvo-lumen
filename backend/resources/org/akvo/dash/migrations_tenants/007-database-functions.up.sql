de400648f37d3b771d433f04fed30da7ac6a3e86
Iván Perdomo <ivan@akvo.org>
2016-04-27 16:31:14 +0200

    [#104] Initial setup of to_number() function

---
+++ backend/resources/org/akvo/dash/migrations_tenants/007-database-functions.up.sql
@@ -0,0 +1,29 @@
+
+CREATE OR REPLACE FUNCTION to_number(val jsonb, default_val numeric)
+  RETURNS jsonb AS
+$BODY$
+BEGIN
+  RETURN CAST (replace(val::text, '"', '') AS numeric);
+EXCEPTION
+  WHEN invalid_text_representation THEN
+    RETURN default_val;
+END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE STRICT;
+
+
+CREATE OR REPLACE FUNCTION to_number(val jsonb, raise_on_error boolean)
+  RETURNS jsonb AS
+$BODY$
+BEGIN
+  RETURN CAST (replace(val::text, '"', '') AS numeric);
+EXCEPTION
+  WHEN invalid_text_representation THEN
+    IF raise_on_error THEN
+      RAISE EXCEPTION 'Unable to convert value % to number', val;
+    ELSE
+      RETURN NULL;
+    END IF;
+END;
+$BODY$
+  LANGUAGE plpgsql IMMUTABLE STRICT;
