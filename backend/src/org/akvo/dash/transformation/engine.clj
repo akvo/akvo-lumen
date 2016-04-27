af01a75c39f01c653aa0e5ef27c9d7cca9758eb8
Iván Perdomo <ivan@akvo.org>
2016-04-27 07:38:09 +0200

    [#104] WIP - Some test running tranformation on JVM land
    - The references to ds_raw_1 is a table with c1..c19 columns with
      jsonb datatype
    - The data is the `artist` table from musicbrainz database

--- backend/src/org/akvo/dash/transformation/engine.clj
+++ backend/src/org/akvo/dash/transformation/engine.clj
@@ -1,5 +1,9 @@
 (ns org.akvo.dash.transformation.engine
-  (:import [javax.script ScriptEngine ScriptEngineManager]))
+  (:import [javax.script ScriptEngine ScriptEngineManager])
+  (:require [clojure.java.jdbc :as jdbc]
+            [hugsql.core :as hugsql]))
+
+(hugsql/def-db-fns "org/akvo/dash/transformation/engine.sql")
 
 (defn get-engine
   "Returns a Nashorn engine capable of evaluating
@@ -37,3 +41,39 @@
 (defn to-titlecase
   [engine value]
   (js-eval engine (str "toTitleCase('" value "')")))
+
+(comment
+
+  (import 'java.sql.DriverManager)
+  (Class/forName "org.postgresql.Driver")
+  (def conn (DriverManager/getConnection "jdbc:postgresql://localhost/dash_tenant_1?user=dash&password=password"))
+  (def e (get-engine))
+
+  (time
+   (let [e (get-engine)
+         db {:connection conn}]
+     (jdbc/with-db-transaction [tx db]
+       (doseq [row (select-col-1 db)]
+         (update-col-1 tx {:rnum (:rnum row) :c1 (to-number e (:c1 row))})))))
+
+  ;; "Elapsed time: 3031769.947961 msecs" - 50.529 min
+
+  (time
+   (let [e (get-engine)
+         db {:connection conn}]
+     (for [r (select-col-1 db)]
+       {:rnum (:rnum r) :c1 (to-number e (:c1 r))})))
+
+  ;; "Elapsed time: 6084.714584 msecs"
+  ;; "Elapsed time: 5986.01587 msecs"
+  ;; "Elapsed time: 6020.817246 msecs"
+
+  (time
+   (let [e (get-engine)
+         db {:connection conn}]
+     (doseq [r (select-col-1 db)]
+       (update-col-1 db {:rnum (:rnum r) :c1 (to-number e (:c1 r))}))))
+
+  ;; "Elapsed time: 3554636.151036 msecs" - 59.244 min
+
+  )
