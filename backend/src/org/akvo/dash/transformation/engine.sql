44250ac7b49c04ad39f158b6b7b6e56959531730
Iván Perdomo <ivan@akvo.org>
2016-05-23 14:27:38 +0200

    [#98] Execute lumen_change_data_type with returning result

--- backend/src/org/akvo/dash/transformation/engine.sql
+++ backend/src/org/akvo/dash/transformation/engine.sql
@@ -26,7 +26,7 @@ CREATE INDEX IF NOT EXISTS :i:index-name
 -- :name db-drop-index :!
 DROP INDEX IF EXISTS :i:index-name CASCADE
 
--- :name db-change-data-type :!
+-- :name db-change-data-type :<!
 SELECT lumen_change_data_type(:table-name, :column-name,
                               :new-type, :default-value::jsonb,
                               :parse-format, :on-error)
