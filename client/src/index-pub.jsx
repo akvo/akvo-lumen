97439dfbcd302c5926aec1626b4c9194744fef92
gabemart <g4bemart@gmail.com>
2016-05-24 13:52:41 +0100

    [#161] Add branding on published visualisations

--- client/src/index-pub.jsx
+++ client/src/index-pub.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import VisualisationPreview from './components/visualisation/VisualisationPreview';
+import LumenBranding from './components/common/LumenBranding';
 
 require('./styles/reset.global.scss');
 require('./styles/style.global.scss');
@@ -9,9 +10,12 @@ const rootElement = document.querySelector('#root');
 const data = window.LUMEN_DATA;
 
 render(
-  <VisualisationPreview
-    visualisation={data.visualisation}
-    datasets={data.datasets}
-  />,
+  <div>
+    <VisualisationPreview
+      visualisation={data.visualisation}
+      datasets={data.datasets}
+    />
+    <LumenBranding />
+  </div>,
   rootElement
 );
