d19700104678ef459ee9251189e9b0228e5670a0
gabemart <g4bemart@gmail.com>
2016-05-24 16:29:17 +0100

    [#139] Improve error messages on bad sidebar type

--- client/src/components/dataset/DataTableSidebar.jsx
+++ client/src/components/dataset/DataTableSidebar.jsx
@@ -144,7 +144,7 @@ export default class DataTableSidebar extends Component {
         return (null);
 
       default:
-        throw new Error('Unkown sidebar type suppled to DataTableSidebar');
+        throw new Error(`Unkown sidebar type ${sidebarType} supplied to getInputs`);
     }
   }
 
@@ -158,7 +158,7 @@ export default class DataTableSidebar extends Component {
         return 'Filter';
 
       default:
-        throw new Error('Unkown sidebar type suppled to DataTableSidebar');
+        throw new Error(`Unkown sidebar type ${sidebarType} supplied to getPositiveButtonText`);
     }
   }
 
