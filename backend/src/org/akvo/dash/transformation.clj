16fbeded16396c4cc4386d5394f0adc906712284
Iván Perdomo <ivan@akvo.org>
2016-05-27 15:44:45 +0200

    [#98] Hand made validation of operation spec
    * The shape of the operations could be changed to something like
      core.spec (when is available)

--- backend/src/org/akvo/dash/transformation.clj
+++ backend/src/org/akvo/dash/transformation.clj
@@ -1,8 +1,81 @@
-(ns org.akvo.dash.transformation)
+(ns org.akvo.dash.transformation
+  (require [org.akvo.dash.transformation.engine :refer [available-ops]]))
 
-(defn- valid?
-  [transformations]
+
+
+;; TODO: Potential change op-spec validation `core.spec`
+
+(def ops-set (set (keys available-ops)))
+(def on-error-set #{"fail" "default-value" "delete-row"})
+(def type-set #{"number" "text" "date"})
+(def sort-direction-set #{"ASC" "DESC"})
+
+(defn required-keys
+  [{:strs [op args onError]}]
+  (boolean (and (ops-set op)
+                (not-empty args)
+                (re-find #"c\d+" (args "columnName"))
+                (on-error-set onError))))
+
+(defmulti valid-op?
+  (fn [op-spec]
+    (keyword (op-spec "op"))))
+
+(defmethod valid-op? :default
+  [op-spec]
+  (throw (Exception. "Invalid operation " (op-spec "op"))))
+
+(defmethod valid-op? :core/change-datatype
+  [{:strs [op args] :as op-spec}]
+  (boolean (and (required-keys op-spec)
+                (type-set (args "newType"))
+                (not-empty (args "defaultValue"))
+                (if (= "date" (args "newType"))
+                  (not-empty (args "parseFormat"))
+                  true))))
+
+(defmethod valid-op? :core/change-column-title
+  [{:strs [args] :as op-spec}]
+  (boolean (and (required-keys op-spec)
+                (not-empty (args "columnTitle")))))
+
+(defmethod valid-op? :core/sort-column
+  [{:strs [args] :as op-spec}]
+  (boolean (and (required-keys op-spec)
+                (sort-direction-set (args "sortDirection")))))
+
+(defmethod valid-op? :core/remove-sort
+  [op-spec]
+  (required-keys op-spec))
+
+(defmethod valid-op? :core/filter
+  [op-spec]
   true)
+
+(defmethod valid-op? :core/to-titlecase
+  [op-spec]
+  (required-keys op-spec))
+
+(defmethod valid-op? :core/to-lowercase
+  [op-spec]
+  (required-keys op-spec))
+
+(defmethod valid-op? :core/to-uppercase
+  [op-spec]
+  (required-keys op-spec))
+
+(defmethod valid-op? :core/trim
+  [op-spec]
+  (required-keys op-spec))
+
+(defmethod valid-op? :core/trim-doublespace
+  [op-spec]
+  (required-keys op-spec))
+
+(defn valid?
+  [transformations]
+  (and (sequential? transformations)
+       (every? valid-op? transformations)))
 
 (defn schedule
   [tenant-conn transformations]
