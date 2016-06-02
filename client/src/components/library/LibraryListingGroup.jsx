ab4ecb7ebc1a262ee7b0f37a1105b27f90ffbaf2
gabemart <g4bemart@gmail.com>
2016-06-02 11:55:34 +0100

    [#190] Add missing semi

--- client/src/components/library/LibraryListingGroup.jsx
+++ client/src/components/library/LibraryListingGroup.jsx
@@ -65,7 +65,7 @@ const convertEntity = entity => {
   }
 
   return output;
-}
+};
 
 export default function LibraryListingGroup({
   listGroup, displayMode, sortOrder, isReverseSort, onSelectEntity, onEntityAction }) {
