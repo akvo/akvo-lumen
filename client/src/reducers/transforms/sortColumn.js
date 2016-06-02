b62488bcc7dd6d8eace027c83433f57de171e5bc
Jonas Enlund <jonas.enlund@gmail.com>
2016-06-02 10:42:26 +0200

    [#178] Mirror the semantics of sql 'order by' when sorting multiple columns

--- client/src/reducers/transforms/sortColumn.js
+++ client/src/reducers/transforms/sortColumn.js
@@ -1,7 +1,7 @@
 import cloneDeep from 'lodash/cloneDeep';
 import { columnIndex } from '../../utilities/dataset';
 
-const comparator = (type, sortDirection, colIndex) => (rowA, rowB) => {
+const columnComparator = (type, sortDirection, colIndex) => (rowA, rowB) => {
   let n;
   const a = rowA[colIndex];
   const b = rowB[colIndex];
@@ -19,23 +19,50 @@ const comparator = (type, sortDirection, colIndex) => (rowA, rowB) => {
   return sortDirection === 'DESC' ? -1 * n : n;
 };
 
+function composeComparators(comparators) {
+  return comparators.reduce(
+    (composedComparator, comparator) => (a, b) => {
+      const n = composedComparator(a, b);
+      return n === 0 ? comparator(a, b) : n;
+    },
+    () => 0
+  );
+}
+
+
 const nextSortLevel = columns => {
-  const sortLevels = columns.map(column => column.sort).filter(n => n != null);
+  const sortLevels = columns
+    .map(column => column.sort)
+    .filter(n => n != null);
   if (sortLevels.length === 0) {
     return 1;
   }
   return sortLevels[sortLevels.length - 1] + 1;
 };
 
+function sortedColumns(columns) {
+  const cols = columns
+    .map((column, colIndex) => Object.assign({}, column, { colIndex }))
+    .filter(column => column.sort != null);
+  cols.sort((colA, colB) => (colA.sort < colB.sort ? -1 : 1));
+  return cols;
+}
+
+function buildComparator(columns) {
+  const columnsToSort = sortedColumns(columns);
+  const columnComparators = columnsToSort.map(column =>
+    columnComparator(column.type, column.direction, column.colIndex)
+  );
+  return composeComparators(columnComparators);
+}
+
 export default function sortColumn(dataset, { args }) {
   const { columnName, sortDirection } = args;
   const ds = cloneDeep(dataset);
-
   const colIndex = columnIndex(columnName, dataset.columns);
-  const type = ds.columns[colIndex].type;
-  ds.rows.sort(comparator(type, sortDirection, colIndex));
   ds.columns[colIndex].sort = nextSortLevel(ds.columns);
   ds.columns[colIndex].direction = sortDirection;
-
+  const comp = buildComparator(ds.columns);
+  ds.rows.sort(comp);
   return ds;
 }
