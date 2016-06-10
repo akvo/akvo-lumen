5dca0921ebfc9d120928bba3d50ee6f9cf5cc6c0
Iván Perdomo <ivan@perdomo.me>
2016-06-10 08:16:53 +0200

    [#248] Detect csv file encoding via ICU's CharsetDetector (#249)
    * [#248] Detect csv file encoding via ICU's CharsetDetector
    
    * [#248] Handle encoding for CSV headers

--- backend/test/org/akvo/lumen/import/csv_test.clj
+++ backend/test/org/akvo/lumen/import/csv_test.clj
@@ -10,12 +10,12 @@
            (get-create-table-sql "uuid1" 3 "text" true)))
     (is (= "CREATE TABLE uuid2 (rnum integer primary key, c1 jsonb, c2 jsonb, c3 jsonb)"
            (get-create-table-sql "uuid2" 3 "jsonb" false)))
-    (is (= "COPY uuid1 (c1, c2, c3) FROM STDIN WITH (FORMAT CSV)"
-           (get-copy-sql "uuid1" 3 false)))
-    (is (= "COPY uuid2 (c1, c2, c3) FROM STDIN WITH (FORMAT CSV, HEADER true)"
-           (get-copy-sql "uuid2" 3 true)))
+    (is (= "COPY uuid1 (c1, c2, c3) FROM STDIN WITH (FORMAT CSV, ENCODING 'UTF-8')"
+           (get-copy-sql "uuid1" 3 false "UTF-8")))
+    (is (= "COPY uuid2 (c1, c2, c3) FROM STDIN WITH (FORMAT CSV, ENCODING 'ISO-8859-1', HEADER true)"
+           (get-copy-sql "uuid2" 3 true "ISO-8859-1")))
     (is (= "INSERT INTO uuid1 (rnum, c1, c2) SELECT rnum, to_json(replace(c1, '\\', '\\\\'))::jsonb, to_json(replace(c2, '\\', '\\\\'))::jsonb FROM t_uuid1"
            (get-insert-sql "t_uuid1" "uuid1" 2)))
-    (is (= 19 (get-num-cols (io/file (io/resource "artist")) \tab)))
-    (is (= 23 (get-num-cols (io/file (io/resource "products")) \,)))
-    (is (= 60 (get-num-cols (io/file (io/resource "rural-population")) \,)))))
+    (is (= 19 (get-num-cols (io/file (io/resource "artist")) \tab "UTF-8")))
+    (is (= 23 (get-num-cols (io/file (io/resource "products")) \, "UTF-8")))
+    (is (= 60 (get-num-cols (io/file (io/resource "rural-population")) \, "UTF-8")))))
