cae92558cf4d1f5a355376e9c19c048a445c6745
Iván Perdomo <ivan@perdomo.me>
2016-06-13 06:49:26 +0200

    [#188] Use a local running Keycloak server for auth
    * Change references from Keycloak server to localhost:8080
    * Adds a script to build|run|stop a Keycloak server via Docker
    * TODO: Move the hardcoded URLs to config file

---
+++ keycloak/server.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+usage() {
+  echo "Usage: $0 {build|run|stop}";
+  exit 1;
+}
+
+[[ $# -eq 1 ]] || usage
+
+docker images > /dev/null || exit 1
+
+case "$1" in
+    build)
+      docker build -t akvo/keycloak .
+      ;;
+    run)
+      docker run -d -p 8080:8080 --name lumen-keycloak -t akvo/keycloak
+      ;;
+    stop)
+      docker stop lumen-keycloak
+      ;;
+    *)
+      usage
+      ;;
+esac
