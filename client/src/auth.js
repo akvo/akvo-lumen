be5d24b2e47b5588d03d5a34aa57d3ac81dce231
Daniel Karlsson <daniel@kardans.com>
2016-06-16 15:06:36 +0200

    [#261] Make keycloak url a config (#269)
    * [#261] Make keycloak url a config
    
    * [#261] Config Keyclock as env var
    
    This applies to both frontend and backend.
    
    * [#261] Disabled wrap-jwt-test
    
    This tests gets certs from an external keycloak (which now is a
    config), hence this breaks at the moment. We should review how to deal
    with this when rethinking the testing strategy.
    
    * [#261] Removed unused import
    
    * [#261] Add require again
    
    * [#261] Removed broken test
    
    * [#261] Removed pprint in favour of printStackTrace

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 export default new Keycloak({
-  url: 'http://localhost:8080/auth',
+  url: process.env.LUMEN_KEYCLOAK_URL || 'http://localhost:8080/auth',
   realm: 'akvo',
   clientId: 'akvo-lumen',
 });
