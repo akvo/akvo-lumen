9f5d30d1f2388889c0f9025962c3386b8d24f466
Daniel Karlsson <daniel@kardans.com>
2016-08-23 13:03:16 +0200

    [#329] Make sure correct env vars are used (#330)
    * [#329] Make sure correct env vars are used
    
    It should be LUMEN_FILE_UPLOAD_PATH
    
    * [#329] Bump backend version

--- backend/project.clj
+++ backend/project.clj
@@ -1,4 +1,4 @@
-(defproject org.akvo.lumen "0.1.1-SNAPSHOT"
+(defproject org.akvo.lumen "0.2-SNAPSHOT"
   :description "Akvo Lumen backend"
   :url "https://github.com/akvo/akvo-lumen"
   :license {:name "GNU Affero General Public License 3.0"
