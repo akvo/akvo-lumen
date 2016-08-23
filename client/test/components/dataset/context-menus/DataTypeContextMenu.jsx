bd43d54fe1377881c7446da6bbd3b3777a4c13f3
Jonas Enlund <jonas.enlund@gmail.com>
2016-08-23 11:03:54 +0300

    Issue/264 unit testing components (#328)
    * [#264] First cut at component unit testing using enzyme
    
    * [#264] Add babel-register
    
    * [#264] columns should be a Immutable.js object
    
    * [#264] Bump react versions

---
+++ client/test/components/dataset/context-menus/DataTypeContextMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import assert from 'assert';
+import Immutable from 'immutable';
+import DataTypeContextMenu from
+  '../../../../src/components/dataset/context-menus/DataTypeContextMenu';
+import ContextMenu from '../../../../src/components/common/ContextMenu';
+
+
+describe('<DataTypeContextMenu />', () => {
+  const baseProps = {
+    column: Immutable.fromJS({}),
+    dimensions: { top: 0, left: 0 },
+    onContextMenuItemSelected: () => {},
+  };
+
+  it('should render without error', () => {
+    shallow(<DataTypeContextMenu {...baseProps} />);
+  });
+  it('renders a ContextMenu', () => {
+    const wrapper = shallow(<DataTypeContextMenu {...baseProps} />);
+    assert.equal(wrapper.find(ContextMenu).length, 1);
+  });
+});
