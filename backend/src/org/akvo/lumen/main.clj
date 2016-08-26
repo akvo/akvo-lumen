b515b8d5c8118fd8da2a7d972893e86049dcbb4d
Jonas Enlund <jonas.enlund@gmail.com>
2016-08-26 11:46:01 +0300

    [#340] Assert correct env vars in production (#341)
    * [#340] Assert correct env vars in production
    
    * [#340] Fix env vars missing error messages

--- backend/src/org/akvo/lumen/main.clj
+++ backend/src/org/akvo/lumen/main.clj
@@ -1,18 +1,19 @@
 (ns org.akvo.lumen.main
-    (:gen-class)
-    (:require [akvo.commons.psql-util]
-              [clojure.java.io :as io]
-              [com.stuartsierra.component :as component]
-              [duct.util.runtime :refer [add-shutdown-hook]]
-              [duct.util.system :refer [load-system]]
-              [environ.core :refer [env]]
-              [org.akvo.lumen.config :as config]
-              [org.akvo.lumen.migrate :as migrate]))
+  (:gen-class)
+  (:require [akvo.commons.psql-util]
+            [clojure.java.io :as io]
+            [com.stuartsierra.component :as component]
+            [duct.util.runtime :refer [add-shutdown-hook]]
+            [duct.util.system :refer [load-system]]
+            [environ.core :refer [env]]
+            [org.akvo.lumen.config :as config]
+            [org.akvo.lumen.migrate :as migrate]))
 
 
 (defn -main [& args]
-  (let [system   (load-system [(io/resource "org/akvo/lumen/system.edn")]
-                              config/bindings)]
+  (config/assert-bindings)
+  (let [system (load-system [(io/resource "org/akvo/lumen/system.edn")]
+                            (config/bindings))]
     (println "Starting HTTP server on port" (-> system :http :port))
     (migrate/migrate)
     (add-shutdown-hook ::stop-system #(component/stop system))
