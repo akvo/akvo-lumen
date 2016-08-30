eeedc600027ed962a58046b80e426a4ff80420db
Daniel Karlsson <daniel@kardans.com>
2016-08-30 20:00:26 +0200

    [#326] Add fish functions to call API from terminal
    This is the hipster version, using fish and httpie.

---
+++ backend/dev_helpers/fish/functions/set-lumen-request-token.fish
@@ -0,0 +1,10 @@
+function set-lumen-request-token --description "Prepare lumen-request."
+         set url http://localhost:8080/auth/realms/akvo/protocol/openid-connect/token
+         set -g -x AKVO_LUMEN_ACCESS_TOKEN (
+             http --form POST $url username='lumen' \
+                  password='lumen' \
+                  client_id='akvo-lumen' \
+                  grant_type=password \
+                  | jq '.access_token' | tr -d '"')
+        echo $AKVO_LUMEN_ACCESS_TOKEN
+end
