4dcf7f557bc820c978693e0a9e25d41e71118858
Daniel Karlsson <daniel@kardans.com>
2016-09-05 15:41:33 +0200

    [#260] Explicit require

--- backend/src/org/akvo/lumen/lib/share.clj
+++ backend/src/org/akvo/lumen/lib/share.clj
@@ -1,29 +1,29 @@
 (ns org.akvo.lumen.lib.share
   ""
   (:require
-   [org.akvo.lumen.lib.share-impl :refer :all]))
+   [org.akvo.lumen.lib.share-impl :as impl]))
 
 
 (defn collection
   "Returns all shared item for tenant."
   [conn]
-  (all-shares conn))
+  (impl/all-shares conn))
 
 (defn share-visualisation
   "Share a visualisation"
   [tenant-conn visualisation-id]
-  (first (insert-visualisation-share tenant-conn
-                                     {:id (random-url-safe-string)
-                                      :visualisation-id visualisation-id})))
+  (first (impl/insert-visualisation-share tenant-conn
+                                          {:id (impl/random-url-safe-string)
+                                           :visualisation-id visualisation-id})))
 
 (defn share-dashboard
   "Share a dashboard"
   [tenant-conn dashboard-id]
-  (first (insert-dashboard-share tenant-conn
-                                 {:id (random-url-safe-string)
-                                  :dashboard-id dashboard-id})))
+  (first (impl/insert-dashboard-share tenant-conn
+                                      {:id (impl/random-url-safe-string)
+                                       :dashboard-id dashboard-id})))
 
 (defn end-share
   "Delete the share."
   [conn id]
-  (delete-share-by-id conn {:id id}))
+  (impl/delete-share-by-id conn {:id id}))
