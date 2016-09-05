2c2ba5a65992786fd17455436970bf64b3ff8ade
Daniel Karlsson <daniel@kardans.com>
2016-09-05 15:13:44 +0200

    [#260]
    Move the domain logic for sharing visualisation and dashboards to it’s
    own /lib/ namespace. Also moved implementation to the share-impl
    namespace in a try to get a easy parsable API in the non -impl
    namespace.

--- backend/test/org/akvo/lumen/endpoint/share_test.clj
+++ backend/test/org/akvo/lumen/endpoint/share_test.clj
@@ -4,7 +4,7 @@
             [hugsql.core :as hugsql]
             [org.akvo.lumen.component.tenant-manager :as tm]
             [org.akvo.lumen.endpoint.dashboard :as dashboard]
-            [org.akvo.lumen.endpoint.share :as share]
+            [org.akvo.lumen.lib.share :as share]
             [org.akvo.lumen.fixtures :refer [db-fixture test-conn]]
             [org.akvo.lumen.util :refer [squuid gen-table-name]]))
 
