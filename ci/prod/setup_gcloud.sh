1e96547997075c0ef9ebeeed97b045c1864f097a
Daniel Karlsson <daniel@kardans.com>
2016-11-11 07:18:20 +0100

    [#414] WIP on test env setup
    - New akvo/lumen-db docker image for use in test env.
    - Moved / organised files into ci/[dev, mini, prod]

---
+++ ci/prod/setup_gcloud.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet components update kubectl
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud config set project $PROJECT_NAME
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
+
+sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
