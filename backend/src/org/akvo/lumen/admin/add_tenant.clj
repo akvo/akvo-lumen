a5b0f6386beac98736c6f832530b1e4b177f5632
Jonas Enlund <jonas.enlund@gmail.com>
2016-11-29 07:33:43 +0200

    [#408] Small tweaks to add-tenant script

--- backend/src/org/akvo/lumen/admin/add_tenant.clj
+++ backend/src/org/akvo/lumen/admin/add_tenant.clj
@@ -1,30 +1,30 @@
 (ns org.akvo.lumen.admin.add-tenant
-  (:require [environ.core :refer [env]]
-            [clojure.java.jdbc :as jdbc]
+  (:require [clojure.java.jdbc :as jdbc]
+            [clojure.string :as s]
+            [environ.core :refer [env]]
             [org.akvo.lumen.util :refer [squuid]]))
 
 (defn -main [label title]
-  (let [tenant (str "tenant_" label)
-        tenant-password (clojure.string/replace (squuid) "-" "")
-        db-uri (format "jdbc:postgresql://%s/%s?user=%s&password=%s" (env :pghost) (env :pgdatabase) (env :pguser) (env :pgpassword))
-
-        tenant-db-uri (format "jdbc:postgresql://%s/%s?user=%s&password=%s" (env :pghost) tenant tenant tenant-password)]
-    (jdbc/execute! db-uri
-                   [(format "CREATE ROLE %s WITH PASSWORD '%s' LOGIN;"
-                            tenant tenant-password)])
-    (jdbc/execute! db-uri
-                   [(format (str "CREATE DATABASE %s WITH OWNER "
-                                 "= %s TEMPLATE = template0 "
-                                 "ENCODING = 'UTF8' "
-                                 "LC_COLLATE = 'en_US.UTF-8' "
-                                 "LC_CTYPE = 'en_US.UTF-8';")
-                            tenant tenant)])
-
-    (jdbc/execute! db-uri
-                   ["CREATE EXTENSION IF NOT EXISTS btree_gist WITH SCHEMA public;"])
-    (jdbc/execute! db-uri
-                   ["CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;"])
-    (jdbc/execute! db-uri
-                   [(format (str "INSERT INTO tenants (db_uri, label, title)"
-                                 "VALUES ('%s', '%s', '%s');")
-                            db-uri label title)])))
+  (let [{pg-host :pghost
+         pg-database :pgdatabase
+         pg-user :pguser
+         pg-password :pgpassword} env
+        tenant (str "tenant_" label)
+        tenant-password (s/replace (squuid) "-" "")
+        db-uri (format "jdbc:postgresql://%s/%s?user=%s&password=%s"
+                       pg-host pg-database pg-user pg-password)
+        tenant-db-uri (format "jdbc:postgresql://%1$s/%2$s?user=%2$s&password=%3$s"
+                              pg-host tenant tenant-password)
+        exec! (fn [format-str & args]
+                (jdbc/execute! db-uri [(apply format format-str args)]))]
+    (exec! "CREATE ROLE %s WITH PASSWORD '%s' LOGIN;" tenant tenant-password)
+    (exec! (str "CREATE DATABASE %1$s "
+                "WITH OWNER = %1$s "
+                "TEMPLATE = template0 "
+                "ENCODING = 'UTF8' "
+                "LC_COLLATE = 'en_US.UTF-8' "
+                "LC_CTYPE = 'en_US.UTF-8';")
+           tenant)
+    (exec! db-uri "CREATE EXTENSION IF NOT EXISTS btree_gist WITH SCHEMA public;")
+    (exec! db-uri "CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;")
+    (jdbc/insert! db-uri :tenants {:db_uri db-uri :label label :title title})))
