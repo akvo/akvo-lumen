8231913daf6c5205b8ae92a82b6f16aa32e7f31e
Jonas Enlund <jonas.enlund@gmail.com>
2016-12-08 18:00:27 +0200

    [#429] wip nashorn interop

---
+++ backend/src/org/akvo/lumen/transformation/js.clj
@@ -0,0 +1,49 @@
+(ns org.akvo.lumen.transformation.js
+  (:import [javax.script ScriptEngineManager ScriptEngine Invocable]
+           [jdk.nashorn.api.scripting NashornScriptEngineFactory ClassFilter]))
+
+(set! *warn-on-reflection* true)
+
+(defn derive-column-function [code]
+  (format "var deriveColumn = function(row) {  return %s; }" code))
+
+(def ^ClassFilter class-filter
+  (reify ClassFilter
+    (exposeToScripts [this s]
+      false)))
+
+(defn row-transform [code]
+  (let [factory (NashornScriptEngineFactory.)
+        engine (.getScriptEngine factory class-filter)]
+    (.eval ^ScriptEngine engine ^String (derive-column-function code))
+    (fn [row]
+      (.invokeFunction ^Invocable engine "deriveColumn" (object-array [row])))))
+
+(def fun (row-transform "quit()"))
+
+
+(fun {"age" 10
+      "foo" 6})
+
+
+
+(time (def f (row-transform "return java.lang.Thread.interrupted()")))
+
+(comment
+  (f {})
+
+  (System/exit 1)
+  )
+
+;;; return row['a'] + (row['b'] < 10 ? row['c'] : row['c']
+;;; return <statement>
+
+;;; return while(true) {}
+
+
+#_(time (dotimes [_ 10000]
+          (f {"a" 3 "b" 7})))
+
+#_(let [m {"a" 1 "b" 2}]
+    (time (dotimes [_ 10000]
+            (+ (get m "a") (get m "b")))))
