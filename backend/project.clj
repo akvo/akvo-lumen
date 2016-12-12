c05ba125f24872dcbf315555f7c79529d0e47035
Daniel Karlsson <daniel@kardans.com>
2016-12-12 07:50:10 +0100

    [#441] Renamed backend artefact

--- backend/project.clj
+++ backend/project.clj
@@ -1,4 +1,4 @@
-(defproject org.akvo.lumen "0.2-SNAPSHOT"
+(defproject org.akvo/lumen "0.3-SNAPSHOT"
   :description "Akvo Lumen backend"
   :url "https://github.com/akvo/akvo-lumen"
   :license {:name "GNU Affero General Public License 3.0"
