0d0fd6af3122f240f01ad9f774f56c3051262fae
gabemart <g4bemart@gmail.com>
2017-01-25 13:10:32 +0000

    [#470] Update default colors to match color picker

--- client/src/utilities/defaultColors.js
+++ client/src/utilities/defaultColors.js
@@ -1,3 +1,17 @@
-const defaultColors = ['#1abc9c', '#f1c40f', '#9b59b6', '#e67e22', '#34495e', '#e74c3c', '#95a5a6'];
+const defaultColors = ['#AA200A',
+  '#1746CB',
+  '#3A8A1E',
+  '#F6CE34',
+  '#4A00E5',
+  '#F4D2C4',
+  '#2A6975',
+  '#C7E1C6',
+  '#2F6EDA',
+  '#FDF4C1',
+  '#CC4A14',
+  '#C8DDF5',
+  '#D1C3F9',
+  '#C3D9DC',
+];
 
 export default defaultColors;
