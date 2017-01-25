9dd5d4ac5eda30bf7f7193bba3d96fb77ed929d9
Daniel Karlsson <daniel@kardans.com>
2017-01-25 09:59:28 +0100

    [#455] First cut on dev emailer

--- backend/src/akvo/lumen/lib/invite_impl.clj
+++ backend/src/akvo/lumen/lib/invite_impl.clj
@@ -1,5 +1,6 @@
 (ns akvo.lumen.lib.invite-impl
   (:require [akvo.lumen.component.keycloak :as keycloak]
+            [akvo.lumen.component.emailer :as emailer]
             [cheshire.core :as json]
             [clj-time.core :as t]
             [clj-time.coerce :as c]
@@ -13,21 +14,23 @@
   [tenant-label tenant-conn keycloak claimed-roles]
   (response (select-active-invites tenant-conn)))
 
-(defn do-create-invite [tenant-conn email claims]
+(defn do-create-invite [tenant-conn emailer email claims]
   ;; create invite
   ;; email invite
   (let [expiration-time (c/to-sql-time (t/plus (t/now) (t/weeks 2)))
-        db-rec (insert-invite tenant-conn
-                              {:email email
-                               :expiration_time expiration-time
-                               :author claims})]
-    (clojure.pprint/pprint db-rec)))
+        db-rec (first (insert-invite tenant-conn
+                                     {:email email
+                                      :expiration_time expiration-time
+                                      :author claims}))
+        host "http://t1.lumen.localhost:3000"
+        mail-body (format "%s/verify/%s" host (:id db-rec))]
+    (emailer/send-email emailer mail-body)))
 
 (defn create
   "Creates a new invite"
-  [tenant-conn keycloak roles {:strs [email]} claims]
+  [tenant-conn emailer keycloak roles {:strs [email]} claims]
   ;; if not existing active invite on same email
-  (do-create-invite tenant-conn email claims)
+  (do-create-invite tenant-conn emailer email claims)
   (response {:invite-job-status "started"}))
 
 #_(defn accept-invite
