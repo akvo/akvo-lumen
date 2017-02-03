2dcf542ce676b906dba15b1b280441baecda76db
Jonas Enlund <jonas.enlund@gmail.com>
2017-02-03 12:11:41 +0200

    [#532] Switch to query params for pivot queries

--- backend/src/akvo/lumen/endpoint/pivot.clj
+++ backend/src/akvo/lumen/endpoint/pivot.clj
@@ -1,10 +1,18 @@
 (ns akvo.lumen.endpoint.pivot
   (:require [akvo.lumen.component.tenant-manager :refer [connection]]
+            [akvo.lumen.http :as http]
             [akvo.lumen.lib.pivot :as pivot]
-            [compojure.core :refer :all]))
+            [cheshire.core :as json]
+            [compojure.core :refer :all])
+  (:import com.fasterxml.jackson.core.JsonParseException))
 
 (defn endpoint [{:keys [tenant-manager config]}]
-  (context "/api/pivot" {:keys [body tenant] :as request}
+  (context "/api/pivot" {:keys [tenant query-params] :as request}
     (let-routes [tenant-conn (connection tenant-manager tenant)]
       (GET "/:id" [id]
-        (pivot/query tenant-conn id body)))))
+        (if-let [query (get query-params "query")]
+          (try
+            (pivot/query tenant-conn id (json/parse-string query))
+            (catch com.fasterxml.jackson.core.JsonParseException e
+              (http/bad-request {:message (.getMessage e)})))
+          (http/bad-request {:message "No query supplied"}))))))
