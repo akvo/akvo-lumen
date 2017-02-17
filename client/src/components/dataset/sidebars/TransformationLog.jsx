99e4066fbb15738bc8240e40d780d70601534943
gabemart <g4bemart@gmail.com>
2017-02-17 11:11:12 +0000

    [#534] Use <code> for transformation log formulae

--- client/src/components/dataset/sidebars/TransformationLog.jsx
+++ client/src/components/dataset/sidebars/TransformationLog.jsx
@@ -9,8 +9,7 @@ function deriveTransformationDescription(transformation) {
   const code = transformation.getIn(['args', 'code']);
   return (
     <div>
-      Derived {newColumnTitle} using
-      <pre style={{ fontFamily: 'monospace' }}>{code}</pre>
+      Derived {newColumnTitle} using <code>{code}</code>
     </div>
   );
 }
