8e29d8932114cbae3123133913b466f8848fef2e
Daniel Karlsson <daniel@kardans.com>
2017-03-07 17:46:15 +0100

    [#533] WIP Extend add-tenant script
    Disabled database setup while developing the Keycloak part. Don’t
    handle the case when the user not exists yet.

--- backend/src/akvo/lumen/admin/util.clj
+++ backend/src/akvo/lumen/admin/util.clj
@@ -1,5 +1,6 @@
 (ns akvo.lumen.admin.util
   (:require [akvo.lumen.component.keycloak :as keycloak]
+            [akvo.lumen.config :refer [error-msg]]
             [clojure.java.jdbc :as jdbc]
             [environ.core :refer [env]]))
 
@@ -20,3 +21,23 @@
           password (env :pgpassword)}}]
    (format "jdbc:postgresql://%s/%s?user=%s&password=%s&ssl=true"
            host database user password)))
+
+(defn- keycloak-url
+  "Hardcoded urls, seemed like a fair thing to do."
+  []
+  (case (:keycloak env)
+    "prod" "https://login.akvo.org/auth"
+    "test" "https://kc.akvotest.org/auth"
+    "dev" "http://localhost:8080/auth"))
+
+(defn create-keycloak []
+  (assert (:keycloak env) (error-msg "KEYCLOAK"))
+  (assert (:keycloak-id env) (error-msg "KEYCLOAK_ID"))
+  (assert (:keycloak-secret env) (error-msg "KEYCLOAK_SECRET"))
+  (let [url (keycloak-url)
+        issuer (format "%s/realms/akvo" url)]
+    {:api-root (format "%s/admin/realms/akvo" url)
+     :issuer issuer
+     :openid-config (keycloak/fetch-openid-configuration issuer)
+     :credentials {"client_id" (:keycloak-id env)
+                   "client_secret" (:keycloak-secret env)}}))
