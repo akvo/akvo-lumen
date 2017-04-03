65116ce1bf7bd8c9242c3706c7bdf67d399c619a
Jonas Enlund <jonas.enlund@gmail.com>
2017-04-03 08:04:12 +0300

    [#431] Small backend code cleanups

--- backend/src/akvo/lumen/lib/aggregation/pie.clj
+++ backend/src/akvo/lumen/lib/aggregation/pie.clj
@@ -4,22 +4,21 @@
             [akvo.lumen.lib.aggregation.utils :as utils]
             [clojure.java.jdbc :as jdbc]))
 
-(defn run-query [tenant-conn table-name column-name filter-sql]
+(defn- run-query [tenant-conn table-name column-name filter-sql]
   (rest (jdbc/query tenant-conn
-                    [(format "SELECT %s, count(*) FROM %s WHERE %s GROUP BY %s"
-                             column-name table-name filter-sql column-name)]
+                    [(format "SELECT %1$s, count(*) FROM %2$s WHERE %3$s GROUP BY %1$s"
+                             column-name table-name filter-sql)]
                     {:as-arrays? true})) )
 
-(defn query [tenant-conn dataset query]
-  (let [columns (:columns dataset)
-        filter-sql (filter/sql-str (:columns dataset) (get query "filters"))
+(defn query
+  [tenant-conn {:keys [columns table-name]} query]
+  (let [filter-sql (filter/sql-str columns (get query "filters"))
         bucket-column (utils/find-column columns (get query "bucketColumn"))
         bucket-column-name (get bucket-column "columnName")
         bucket-column-title (get bucket-column "title")
-        table-name (:table-name dataset)
         counts (run-query tenant-conn table-name bucket-column-name filter-sql)]
     (http/ok {"metadata" {"bucketColumnTitle" bucket-column-title}
               "data" (mapv (fn [[bucket-value bucket-count]]
                              {"bucketValue" bucket-value
                               "bucketCount" bucket-count})
-                           counts)})) )
+                           counts)})))
