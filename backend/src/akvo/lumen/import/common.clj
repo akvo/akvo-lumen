5557fe9181f5445ad2375f118745b8b342159b7e
Jonas Enlund <jonas.enlund@gmail.com>
2017-05-04 19:16:35 +0200

    proof-of-concept improved import abstractions

--- backend/src/akvo/lumen/import/common.clj
+++ backend/src/akvo/lumen/import/common.clj
@@ -2,6 +2,47 @@
   (:require [cheshire.core :as json])
   (:import org.apache.commons.io.input.UnixLineEndingInputStream))
 
+(defprotocol DatasetImporter
+  "
+  A protocol for importing datasets into Lumen. A typical implementation
+  should also implement java.io.CLoseable because some data sources are
+  backed by resources that needs to be released.
+
+  Example:
+  (reify
+    Closeable
+    (close [this])
+
+    DatasetImporter
+    (columns [this]
+      [{:id :a :type :text :title \"A\"}
+       {:id :b :type :number :title \"B\"}
+       {:id :c :type :date :title \"C\"}])
+    (records [this]
+      [{:a \"foo\"
+        :b 42
+        :c (Instant/now)}
+       {:a \"bar\"
+        :b 3.14
+        :c (Instant/now)}
+  "
+
+  (columns [this]
+    "Returns a sequence of column specifications of the dataset to be imported.
+     A column specification is a map with keys
+
+       :type - The lumen type of the column. Currently :text, :number or :date
+       :title - The title of the column
+       :id - The internal id of the column (as a keyword). The id must be
+             lowercase alphanumeric ([a-z][a-z0-9]*)")
+  (records [this]
+    "Returns a sequence of record data. A record is a map of column ids to values.
+     The type of the value depends on the type of the column where
+
+       :text - java.lang.String
+       :number - java.lang.Number
+       :date - java.time.Instant"))
+
 (defn dispatch-on-kind [spec]
   (let [kind (get spec "kind")]
     (if (#{"LINK" "DATA_FILE"} kind)
@@ -24,6 +65,11 @@
 (defmulti authorized?
   "Authorize the data source import request based on the users set of jwt claims"
   (fn [claims config spec]
+    (dispatch-on-kind spec)))
+
+(defmulti dataset-importer
+  "Creates a DatasetImporter according to the spec"
+  (fn [spec config]
     (dispatch-on-kind spec)))
 
 (defmulti make-dataset-data-table
