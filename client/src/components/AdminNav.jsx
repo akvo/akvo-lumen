b74fc6a6d3c809a8830504eaba7f6e5c865cdd8b
Daniel Karlsson <daniel@kardans.com>
2017-05-11 14:47:20 +0200

    [#673] First steps on new admin nav sidebar

--- client/src/components/AdminNav.jsx
+++ client/src/components/AdminNav.jsx
@@ -1,14 +1,47 @@
-import React, { PropTypes } from 'react';
-/*
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import OrganizationMenu from './workspace-nav/OrganizationMenu';
+
 class AdminNav extends Component {
   render() {
     return (
-      <h1>Admin</h1>
+      <nav
+        className="WorkspaceNav noSelect"
+      >
+        <div className="header">
+          <div className="rowPrimary">
+            <div
+              className="menuIcon"
+            />
+            <h1><Link to="/">Lumen</Link></h1>
+          </div>
+          <OrganizationMenu profile={this.props.profile} />
+        </div>
+      </nav>
+
     );
   }
 }
-*/
 
+AdminNav.propTypes = {
+  profile: PropTypes.object.isRequired,
+  location: PropTypes.object,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  const collections = Object.keys(state.collections).map(key => state.collections[key]);
+  return {
+    collections,
+    profile: state.profile,
+  };
+}
+
+export default connect(
+  mapStateToProps
+)(AdminNav);
+/*
 export const AdminNav = () => (
   <h1>Admin</h1>
 );
@@ -19,3 +52,4 @@ AdminNav.propTypes = {
 };
 
 export { AdminNav as default };
+*/
