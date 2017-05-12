91bce711d55f6c7333c038aba9c2f768457d3be7
gabemart <g4bemart@gmail.com>
2017-05-12 11:12:16 +0100

    [#674] Fix linter error

--- client/test/reducers/collections.js
+++ client/test/reducers/collections.js
@@ -1,7 +1,6 @@
 import assert from 'assert';
 
 import reducer, { initialState } from '../../src/reducers/collections';
-import { createCollection } from '../../src/actions/collection';
 
 describe('collections reducer', () => {
   it('should return the initial state', () => {
