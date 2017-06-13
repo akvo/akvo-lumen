50a3379c3741ec6334928d6e256b9d7b30283b69
Jonas Enlund <jonas.enlund@gmail.com>
2017-06-13 15:44:41 +0300

    [#841] Require akvo.lumen.endpoint namespace on startup

--- backend/src/akvo/lumen/main.clj
+++ backend/src/akvo/lumen/main.clj
@@ -1,13 +1,14 @@
 (ns akvo.lumen.main
   (:gen-class)
   (:require [akvo.commons.psql-util]
+            [akvo.lumen.config :as config]
+            [akvo.lumen.endpoint]
+            [akvo.lumen.migrate :as migrate]
             [clojure.java.io :as io]
             [com.stuartsierra.component :as component]
             [duct.util.runtime :refer [add-shutdown-hook]]
             [duct.util.system :refer [load-system]]
-            [environ.core :refer [env]]
-            [akvo.lumen.config :as config]
-            [akvo.lumen.migrate :as migrate]))
+            [environ.core :refer [env]]))
 
 
 (defn -main [& args]
