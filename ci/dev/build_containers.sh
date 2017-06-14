d50a296c98ed4da4dfd6da78216c5ac0fc056d1c
Daniel Karlsson <daniel@kardans.com>
2017-06-14 15:32:48 +0200

    [#681] WIP one more try

--- ci/dev/build_containers.sh
+++ ci/dev/build_containers.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+cd backend
+lein uberjar
+cd ..
+
 docker build -t eu.gcr.io/${PROJECT_NAME}/lumen-backend:$CIRCLE_SHA1 ./backend
 docker tag eu.gcr.io/${PROJECT_NAME}/lumen-backend:$CIRCLE_SHA1 eu.gcr.io/${PROJECT_NAME}/lumen-backend:develop
 docker build -t eu.gcr.io/${PROJECT_NAME}/lumen-client:$CIRCLE_SHA1 ./client
