3dea55631165e35aac9a014442196515e8300314
Paul Burt <paul.burt@gmail.com>
2017-06-30 10:56:16 +0100

    [#816] Restored export default in utilities/validators. Without the default keyword, the client app does not render. Needs further discussion.

--- client/src/utilities/validators.js
+++ client/src/utilities/validators.js
@@ -1,6 +1,4 @@
-/* eslint import/prefer-default-export: "off" */
-
-export function isValidEmail(email) {
+export default function isValidEmail(email) {
   const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,9})+$/;
   return regex.test(email);
 }
