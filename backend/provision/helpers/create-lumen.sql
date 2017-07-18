a9029ded1276ba6b28632392b4d051d5f57e911b
Daniel Lebrero <dlebrero@gmail.com>
2017-07-18 18:30:21 +0200

    restoring the setup.sh and the sql scripts to populate the test db.
    This means that the scripts are duplicated from the postgres directory and they are slightly different. We will need to look at how to merge them together

---
+++ backend/provision/helpers/create-lumen.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE lumen
+WITH OWNER = lumen
+     TEMPLATE = template0
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.UTF-8'
+     LC_CTYPE = 'en_US.UTF-8';
+
+CREATE DATABASE test_lumen
+WITH OWNER = lumen
+     TEMPLATE = template0
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.UTF-8'
+     LC_CTYPE = 'en_US.UTF-8';
