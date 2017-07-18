8878a1c9d37f0cbdc17a5c4dc2eb2c636895e2fa
Daniel Lebrero <dlebrero@gmail.com>
2017-07-18 10:56:45 +0200

    windshaft + redis containers
    windshaft always connects to the lumen_tenant_1 database
    lumen_tenant_1 db has the liberia table and data
    windshaft completely open but just configured in the webpack dev server
    added windshaft example ui to /client. Available at http://t1.lumen.localhost:3030/viewer/index.html

---
+++ postgres/provision/helpers/import_csv.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+INPUT=$1
+
+test -z "$1" && {
+  echo "Usage: $0 <csv>" >&2
+  exit 1
+}
+
+FIELDS=`head -1 $INPUT | tr [A-Z] [a-z] |
+  sed 's/ //g;s/\?//;s/^/"/;s/$/" varchar/;s/,/" varchar, "/g'`
+TABLE=`basename $INPUT | sed 's/\.[^.]*//' | tr '[A-Z]' '[a-z]'`
+
+echo "BEGIN;"
+echo "CREATE TABLE \"${TABLE}\" ( ${FIELDS} );"
+echo "COPY \"${TABLE}\" FROM stdin DELIMITER ',';"
+tail -n +2 ${INPUT}
+echo "\\."
+echo "ALTER TABLE \"${TABLE}\" ADD last_updated TIMESTAMPTZ DEFAULT NOW();"
+echo "COMMIT;"
+
