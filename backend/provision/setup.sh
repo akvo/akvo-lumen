9f07a7a9e52d1d7dd8665b80a9d5a385b5d7f328
Daniel Lebrero <dlebrero@gmail.com>
2017-07-25 10:21:19 +0200

    removing provision scripts as they dont seem to be used at all

--- backend/provision/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sudo /bin/bash -c "echo '127.0.0.1 t1.lumen.localhost' >> /etc/hosts"
-sudo /bin/bash -c "echo '127.0.0.1 t2.lumen.localhost' >> /etc/hosts"
-sudo /bin/bash -c "echo '127.0.0.1 auth.lumen.localhost' >> /etc/hosts"
-sudo /bin/bash -c "echo '127.0.0.1 postgres' >> /etc/hosts"
