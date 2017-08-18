b72e5c4a322912d7872248cdb55583b79be10294
Daniel Karlsson <daniel@kardans.com>
2017-08-18 10:58:55 +0200

    [#1010] Down migration to unlimited plans
    Also tweaked the tenant foreign key constraint on plan to delete on
    cascade. This to allow us to delete a tenant cleanly.

--- backend/resources/akvo/lumen/migrations/tenant_manager/003-unlimited.up.sql
+++ backend/resources/akvo/lumen/migrations/tenant_manager/003-unlimited.up.sql
@@ -1,4 +1,11 @@
 ALTER TABLE plan
+      DROP CONSTRAINT plan_tenant_fkey,
+      ADD CONSTRAINT plan_tenant_fkey
+      FOREIGN KEY (tenant)
+      REFERENCES tenants (id)
+      ON DELETE CASCADE;
+
+ALTER TABLE plan
       ALTER COLUMN tier TYPE text,
       ALTER COLUMN tier DROP DEFAULT;
 
