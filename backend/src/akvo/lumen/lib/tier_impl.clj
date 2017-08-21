b75076309a8dc6ed45c80e375b46406fdab37c0d
Daniel Karlsson <daniel@kardans.com>
2017-08-21 06:49:10 +0200

    [#1010] Use nil as unlimited value at backend.

--- backend/src/akvo/lumen/lib/tier_impl.clj
+++ backend/src/akvo/lumen/lib/tier_impl.clj
@@ -3,8 +3,8 @@
    [ring.util.response :refer [response]]))
 
 (defn all [tenant-conn]
-  (response {"tiers" {"unlimited" {"numberOfExternalDatasets" "*"
-                                   "numberOfVisualisations" "*"}
+  (response {"tiers" {"unlimited" {"numberOfExternalDatasets" nil
+                                   "numberOfVisualisations" nil}
                       "standard" {"numberOfExternalDatasets" 5
                                   "numberOfVisualisations" 50}
                       "pro" {"numberOfExternalDatasets" 20
