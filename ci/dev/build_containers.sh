36d8a9cec9b014888b2caab7a616c9d40f005613
Daniel Lebrero <dlebrero@gmail.com>
2017-08-30 19:13:24 +0200

    [#901] Adding Windshaft container to Lumen pod and adding a Redis as another deployment + service.
    Just test environment

--- ci/dev/build_containers.sh
+++ ci/dev/build_containers.sh
@@ -9,5 +9,9 @@ cd ..
 
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/lumen-backend:$CIRCLE_SHA1 ./backend
 docker tag eu.gcr.io/${PROJECT_NAME}/lumen-backend:$CIRCLE_SHA1 eu.gcr.io/${PROJECT_NAME}/lumen-backend:develop
+
 docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/lumen-client:$CIRCLE_SHA1 ./client
 docker tag eu.gcr.io/${PROJECT_NAME}/lumen-client:$CIRCLE_SHA1 eu.gcr.io/${PROJECT_NAME}/lumen-client:develop
+
+docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/lumen-maps:$CIRCLE_SHA1 ./windshaft
+docker tag eu.gcr.io/${PROJECT_NAME}/lumen-maps:$CIRCLE_SHA1 eu.gcr.io/${PROJECT_NAME}/lumen-maps:develop
