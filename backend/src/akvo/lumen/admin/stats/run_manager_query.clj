27c47b394252e1c08ddccdc336dd54b78d0c2e1a
Jonas Enlund <jonas.enlund@gmail.com>
2017-08-31 17:04:07 +0300

    Some helper scripts to run remote sql queries

---
+++ backend/src/akvo/lumen/admin/stats/run_manager_query.clj
@@ -0,0 +1,15 @@
+(ns akvo.lumen.admin.stats.run-manager-query
+  (:require [akvo.lumen.admin.util :as util]
+            [cheshire.core :as json]
+            [clojure.java.jdbc :as jdbc]
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as s]))
+;; Same env vars as the rest of admin scripts
+
+;; Run query in lumen (tenant manager)
+(defn -main [query]
+  (let [uri (util/db-uri {:database "lumen"})]
+    (json/generate-stream
+     (jdbc/with-db-connection [conn uri {:read-only? true}]
+       (jdbc/query conn [query]))
+     *out*)))
