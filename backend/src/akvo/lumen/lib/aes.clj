47294e91fa1ae2d7e5f0733e7e97630fd9bcc276
Daniel Karlsson <daniel@kardans.com>
2017-09-14 08:42:58 +0200

    [#1006] Removed old node example
    Also:
    - Tweaked type hints
    - Updated env var reference

--- backend/src/akvo/lumen/lib/aes.clj
+++ backend/src/akvo/lumen/lib/aes.clj
@@ -6,11 +6,11 @@
 
 (defn encrypt
   "Accepts secret & clear text strings and returns a base64 encoded byte array."
-  [secret #^String clear-text]
+  [^String secret ^String clear-text]
   {:pre [(string? secret) (string? clear-text)]
    :post [(string? %)(Base64/isBase64 (.getBytes %))]}
   (let [cipher (Cipher/getInstance "AES/ECB/PKCS5Padding")
-        key-bytes (DigestUtils/sha256 ^String secret)
+        key-bytes (DigestUtils/sha256 secret)
         clear-text-bytes (.getBytes clear-text)
         key-spec (SecretKeySpec. key-bytes "AES")
         _ (.init cipher Cipher/ENCRYPT_MODE key-spec)
@@ -19,14 +19,14 @@
 
 (defn decrypt
   "Accepts string secret and a base64 encoded byte array and returns clear text."
-  [secret #^String cipher-text]
+  [^String secret ^String cipher-text]
   {:pre [(string? secret) (string? cipher-text)
-         (Base64/isBase64 (.getBytes ^String cipher-text))]
+         (Base64/isBase64 (.getBytes cipher-text))]
    :post [(string? %)]}
   (let [cipher (Cipher/getInstance "AES/ECB/PKCS5Padding")
-        key-bytes (DigestUtils/sha256 ^String secret)
+        key-bytes (DigestUtils/sha256 secret)
         key-spec (SecretKeySpec. key-bytes "AES")
         _ (.init cipher Cipher/DECRYPT_MODE key-spec)
-        cipher-bytes (Base64/decodeBase64 ^String cipher-text)
+        cipher-bytes (Base64/decodeBase64 cipher-text)
         output (.doFinal cipher cipher-bytes)]
     (String. output)))
