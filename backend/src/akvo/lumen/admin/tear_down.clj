8d43eebd4f08515ec0793ec91a208cc0a01084bd
Daniel Karlsson <daniel@kardans.com>
2017-09-14 09:30:04 +0200

    [#1006] Add tenant script uses encrypted db_uris

--- backend/src/akvo/lumen/admin/tear_down.clj
+++ backend/src/akvo/lumen/admin/tear_down.clj
@@ -1,7 +1,7 @@
 (ns akvo.lumen.admin.tear-down
-  (:require [clojure.java.jdbc :as jdbc]
-            [akvo.lumen.admin.remove-tenant :refer (remove-tenant)]
-            [akvo.lumen.admin.util :as util]))
+  (:require [akvo.lumen.admin.remove-tenant :refer (remove-tenant)]
+            [akvo.lumen.admin.util :as util]
+            [clojure.java.jdbc :as jdbc]))
 
 ;; The following env vars are assumed to be present:
 ;; PGHOST,  PGDATABASE, PGUSER, PGPASSWORD
@@ -11,12 +11,12 @@
 ;;     lein run -m akvo.lumen.admin.tear-down
 
 (defn -main []
-  (let [db-uri (util/db-uri)
-        lumen-db-uri (util/db-uri {:database "lumen" :user "lumen"})
-        labels (map :label
-                    (jdbc/query lumen-db-uri
-                                ["SELECT label FROM tenants"]))]
-    (doseq [label labels]
-      (remove-tenant label))
-    (util/exec! db-uri "DROP DATABASE lumen")
-    (util/exec! db-uri "DROP ROLE lumen")))
+  #_(let [db-uri (util/db-uri)
+          lumen-db-uri (util/db-uri {:database "lumen" :user "lumen"})
+          labels (map :label
+                      (jdbc/query lumen-db-uri
+                                  ["SELECT label FROM tenants"]))]
+      (doseq [label labels]
+        (remove-tenant label))
+      (util/exec! db-uri "DROP DATABASE lumen")
+      (util/exec! db-uri "DROP ROLE lumen")))
