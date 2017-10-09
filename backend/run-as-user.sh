9aa485513a900963c7ec484c238ab698d870c636
Iván Perdomo <ivan@perdomo.me>
2017-10-09 12:36:34 +0200

    [#1098] Run lein commands as with host's user and group id
    * Setup an .env file with the host's user and group id
    * Use lein image based on Alpine linux: 96MB (lein-2.7.1-alpine) vs 313MB (lein-2.7.1)
    * The `run-as-user` makes sure the `akvo` user and group exists
      with the hosts UID and GID
    * docker-compose file adjustments to use .env settings

---
+++ backend/run-as-user.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eu
+
+USER_EXISTS=$(awk -F ':' '$3 ~ /^'${HOST_UID}'$/' /etc/passwd)
+GROUP_EXISTS=$(awk -F ':' '$3 ~ /^'${HOST_GID}'$/' /etc/group)
+
+if [ -z "${GROUP_EXISTS}" ]; then
+    addgroup -g "${HOST_GID}" akvo
+else
+    sed -i -e "s/\(.*\):\(.*\):${HOST_GID}:\(.*\)/akvo:\2:${HOST_GID}:\3/" /etc/group
+fi
+
+if [ -z "${USER_EXISTS}" ]; then
+    adduser -h /home/akvo \
+	    -s /bin/sh \
+	    -G akvo \
+	    -D \
+	    -u "${HOST_UID}" \
+	    akvo
+else
+    sed -i -e "s|^\(.*\):\(.*\):${HOST_UID}:\(.*\)$|akvo:x:${HOST_UID}:${HOST_UID}:akvo:/home/akvo:/bin/bash|" /etc/passwd
+fi
+
+su akvo -c "$@"
