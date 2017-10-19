44fcf3ccfa45d7534b54b4e683f13d02a63c6ef9
gabemart <g4bemart@gmail.com>
2017-10-19 15:54:41 +0100

    [#1077] Remove old client side maps code

--- client/src/components/charts/MapVisualisation.jsx
+++ client/src/components/charts/MapVisualisation.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
-import { Map, CircleMarker, Popup, TileLayer } from 'react-leaflet';
 import leaflet from 'leaflet';
 import { isEqual, cloneDeep } from 'lodash';
 import leafletUtfGrid from '../../vendor/leaflet.utfgrid';
@@ -27,75 +26,6 @@ const isImage = (value) => {
     return true;
   }
   return false;
-};
-
-const calculateBounds = (layerArray) => {
-  if (layerArray.length === 0) {
-    return null;
-  }
-
-  let maxLat = -90;
-  let minLat = 90;
-  let maxLong = -180;
-  let minLong = 180;
-
-  layerArray.forEach((layer) => {
-    const bounds = layer.bounds;
-    const minArray = bounds[0];
-    const maxArray = bounds[1];
-
-    minLat = Math.min(minLat, minArray[0]);
-    minLong = Math.min(minLong, minArray[1]);
-    maxLat = Math.max(maxLat, maxArray[0]);
-    maxLong = Math.max(maxLong, maxArray[1]);
-  });
-
-  /*
-   * If there's a single geo point (or if all the geo points happens to overlap) the min/max
-   * coordinates will be the same. Expand them so that we get a sensible bounding box.
-   */
-  if (minLat === maxLat) {
-    minLat -= 1;
-    maxLat += 1;
-  }
-  if (minLong === maxLong) {
-    minLong -= 1;
-    maxLong += 1;
-  }
-
-  return [[minLat, minLong], [maxLat, maxLong]];
-};
-
-const getDataLayers = (layers, datasets) => {
-  const displayLayers = [];
-
-  layers.forEach((layer) => {
-    if (layer.visible && layer.datasetId && layer.latitude !== null && layer.longitude !== null) {
-      const chartData = chart.getMapData(layer, datasets);
-
-      if (chartData) {
-        const chartValues = chartData.values;
-        const sortFunc =
-          chart.getPointColorMappingSortFunc(chartData.metadata.pointColorColumnType);
-        const pointColorMapping =
-          Object.keys(chartData.metadata.pointColorMapping).map(value => ({
-            value,
-            color: chartData.metadata.pointColorMapping[value],
-          }))
-          .sort(sortFunc);
-
-        const bounds = chartData.metadata.bounds;
-
-        displayLayers.push(Object.assign({}, layer, {
-          chartValues,
-          pointColorMapping,
-          bounds,
-        }));
-      }
-    }
-  });
-
-  return displayLayers;
 };
 
 const getBaseLayerAttributes = ((baseLayer) => {
@@ -153,72 +83,6 @@ Legend.propTypes = {
   pointColorMapping: PropTypes.array.isRequired,
   title: PropTypes.string,
   position: PropTypes.string,
-};
-
-const DataLayer = ({ displayLayer }) => {
-  const { chartValues } = displayLayer;
-  const radius = 2 + (parseInt(displayLayer.pointSize, 10) * 2);
-
-  return (
-    <div>
-      {chartValues.map((entry, index) =>
-        <CircleMarker
-          center={[entry.latitude, entry.longitude]}
-          radius={radius}
-          fillColor={entry.pointColor || '#000000'}
-          fillOpacity={1}
-          opacity={1}
-          color="#FFFFFF"
-          weight={1}
-          key={index}
-        >
-          {entry.popup.length > 0 &&
-          <Popup>
-            <ul
-              className="popupContainer"
-            >
-              {entry.popup.map((popupObject, popupIndex) =>
-                <li
-                  className="popupItem"
-                  key={popupIndex}
-                >
-                  <h4>
-                    {popupObject.title}
-                  </h4>
-                  <span>
-                    {isImage(popupObject.value) ?
-                      <a
-                        href={popupObject.value}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <div className="imageContainer">
-                          <img
-                            src={popupObject.value}
-                            role="presentation"
-                          />
-                        </div>
-                      </a>
-                          :
-                      <span>
-                        {popupObject.value}
-                      </span>
-                        }
-                  </span>
-                </li>
-                )}
-            </ul>
-          </Popup>
-            }
-        </CircleMarker>
-        )
-      }
-    </div>
-  );
-};
-
-DataLayer.propTypes = {
-  displayLayer: PropTypes.object.isRequired,
 };
 
 const PopupContent = ({ data, layerDataset, onImageLoad }) => (
@@ -438,16 +302,10 @@ export default class MapVisualisation extends Component {
   }
 
   render() {
-    const { visualisation, datasets, width, height } = this.props;
-    /*
-    const displayLayers = getDataLayers(visualisation.spec.layers, datasets);
-    const bounds = calculateBounds(displayLayers);
-    */
+    const { visualisation, width, height } = this.props;
     const title = visualisation.name || '';
     const titleLength = title.toString().length;
     const titleHeight = titleLength > 48 ? 56 : 36;
-    const mapHeight = height - titleHeight;
-    const { tileUrl, tileAttribution } = getBaseLayerAttributes(visualisation.spec.baseLayer);
 
     return (
       <div
@@ -457,44 +315,6 @@ export default class MapVisualisation extends Component {
           height,
         }}
       >
-        {/*
-        {displayLayers.map((displayLayer, outerIndex) =>
-          <div
-            className="legendContainer"
-            key={outerIndex}
-          >
-            {(displayLayer.pointColorMapping &&
-              displayLayer.pointColorMapping.length > 0 &&
-              displayLayer.legend.visible) &&
-              <div
-                className={`legend ${displayLayer.legend.position}`}
-              >
-                <h4>{displayLayer.legend.title}</h4>
-                <ul>
-                  {displayLayer.pointColorMapping.map((mappingEntry, innerIndex) =>
-                    <li
-                      className="legendEntry"
-                      key={innerIndex}
-                    >
-                      <i
-                        className="colorIndicator"
-                        style={{
-                          backgroundColor: mappingEntry.color,
-                        }}
-                      />
-                      <span
-                        className={`colorLabel
-                          ${chart.replaceLabelIfValueEmpty(mappingEntry.value, true)}`}
-                      >
-                        {chart.replaceLabelIfValueEmpty(mappingEntry.value)}
-                      </span>
-                    </li>
-                  )}
-                </ul>
-              </div>
-            }
-          </div>
-        )} */}
         <h2
           style={{
             height: titleHeight,
@@ -506,32 +326,6 @@ export default class MapVisualisation extends Component {
             {visualisation.name}
           </span>
         </h2>
-        {/*
-        <Map
-          center={[0, 0]}
-          {... bounds ? { bounds } : {}} // Do not set a bounds prop if we have no bounds
-          zoom={2}
-          scrollWheelZoom={false}
-          key={width}
-          style={{
-            width,
-            height: mapHeight,
-          }}
-        >
-          <TileLayer
-            key={tileUrl}
-            url={tileUrl}
-            attribution={tileAttribution}
-          />
-          {displayLayers.map((displayLayer, index) =>
-            <DataLayer
-              key={index}
-              displayLayer={displayLayer}
-            />
-          )}
-        </Map>
-        <hr />
-      */}
         <div className="mapContainer">
           <div
             className="leafletMap"
