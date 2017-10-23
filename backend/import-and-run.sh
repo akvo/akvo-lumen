63dcd5344f7f0ba48061fbda52b4e5bfd385295c
Iván Perdomo <ivan@perdomo.me>
2017-10-23 09:22:10 +0200

    [#1098] Apply changes to client (js) code
    - Use an alpine based image
    - Install requirements for node/canvas
    - Upgrade node-sass to a version that supports alpine linux
    - Run `npm *` commands as akvo user

--- backend/import-and-run.sh
+++ backend/import-and-run.sh
@@ -2,4 +2,4 @@
 
 keytool -import -trustcacerts -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
 
-./run-as-user.sh 'lein repl :headless'
+./run-as-user.sh lein repl :headless
--- client/install-and-start.sh
+++ client/install-and-start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 npm install
 npm rebuild node-sass
