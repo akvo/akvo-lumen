a439e4887269e27c4ab70eb4b555913e75121447
Iván Perdomo <ivan@perdomo.me>
2017-11-02 14:27:58 +0100

    [#1096] Fixes references to auth.lumen.localhost -> auth.lumen.local

--- backend/dev_helpers/fish/functions/set-lumen-request-token.fish
+++ backend/dev_helpers/fish/functions/set-lumen-request-token.fish
@@ -1,5 +1,5 @@
 function set-lumen-request-token --description "Prepare lumen-request."
-        set url http://auth.lumen.localhost:8080/auth/realms/akvo/protocol/openid-connect/token
+        set url http://auth.lumen.local:8080/auth/realms/akvo/protocol/openid-connect/token
 
         set -g -x AKVO_LUMEN_ACCESS_TOKEN (
              http --form POST $url username='jerome' \
