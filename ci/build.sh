52f0e7623a7da976f6b3192b9d7be664bdd65a5d
Rubén Baztán <ruazulgrana10@gmail.com>
2017-11-08 12:25:32 +0100

    [#1096] Change "wait for backend" loops in build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,19 +1,18 @@
-docker-compose up &
+docker-compose up -d
 BACKEND=""
-CLIENT=""
 while [ -z "$BACKEND" ] ; do
   echo "Waiting for backend to start."
-  sleep 1;
-  BACKEND=$(echo sanpedro43 | sudo -S lsof -Pi :3000 -sTCP:LISTEN -t)
-done &
-wait $!
-while [ -z "$CLIENT" ]; do
-  echo "ESPERANDO CLIENTE"
-  sleep 1;
-  CLIENT=$(sudo lsof -Pi :3030 -sTCP:LISTEN -t)
-done &
-wait $!
+  sleep 1
+  BACKEND=$(curl -s --connect-timeout 1 "http://t1.lumen.local:3030/env")
+done
+CLIENT=""
+while [ -z "$CLIENT" ] ; do
+  echo "Waiting for client to start."
+  CLIENT=$(curl -s "http://t1.lumen.local:3030/")
+done
 docker run --interactive --tty --shm-size 1G --rm --network=akvolumen_default \
 -v "$PWD/client/ui-test/script-test.js":/app/index.js --link akvolumen_client_1:t1.lumen.local \
 --link akvolumen_keycloak_1:auth.lumen.local --link akvolumen_client_1:t1.lumen.local \
---link akvolumen_keycloak_1:auth.lumen.local --sysctl net.ipv6.conf.all.disable_ipv6=1 alekzonder/puppeteer:latest
+--link akvolumen_keycloak_1:auth.lumen.local --sysctl net.ipv6.conf.all.disable_ipv6=1 alekzonder/puppeteer:0.12.0
+
+#docker-compose down
--- client/ui-test/script-test.js
+++ client/ui-test/script-test.js
@@ -19,7 +19,7 @@
 const puppeteer = require('puppeteer');
 /* Si se cambia la variable name, también hay que cambiar el nombre en la
    búsqueda del ID  */
-const name = 'AJI';
+const name = 'puppeteer test';
 (async() => {
 	const browser = await puppeteer.launch({
         args: [
@@ -35,28 +35,28 @@ const name = 'AJI';
 		await page.setViewport({width: 1024, height: 768});
 		await console.log("Accessing to http://t1.lumen.local:3030...");
 		await page.goto('http://t1.lumen.local:3030/');
-		await page.waitForSelector('#username', {timeout:5000});
+		await page.waitForSelector('#username', {timeout:10000});
 		await console.log("Typing username...");
 		await page.type('#username','jerome');
 		await console.log("Typing password...");
 		await page.type('#password','password');
 		await console.log("Trying login...");
 		await page.click('#kc-login');
-		await page.waitForSelector('button[data-test-id="dataset"]', {timeout:5000});
+		await page.waitForSelector('button[data-test-id="dataset"]', {timeout:10000});
 		//Dataset adding
 		//Click Dataset+ option
 		await console.log("Accessing to dataset creation...");
 		await page.click('button[data-test-id="dataset"]');
-		await page.waitForSelector('button[data-test-id="btn next clickable positive-footer"]', {timeout:5000});
+		await page.waitForSelector('button[data-test-id="btn next clickable positive-footer"]', {timeout:10000});
 		//Select link option
 		await console.log("Typing dataset link...");
 		await page.click('input[data-test-id="source-option"][value="LINK"]');
 		await page.click('button[data-test-id="btn next clickable positive-footer"]');
-		await page.waitForSelector('#linkFileInput', {timeout:5000});
+		await page.waitForSelector('#linkFileInput', {timeout:10000});
 		//Insert link
 		await page.type('#linkFileInput','https://github.com/lawlesst/vivo-sample-data/raw/master/data/csv/people.csv');
 		await page.click('button[data-test-id="btn next clickable positive-footer"]');
-		await page.waitForSelector('input[data-test-id="dataset-name"]', {timeout:5000});
+		await page.waitForSelector('input[data-test-id="dataset-name"]', {timeout:10000});
 		//Insert name
 		await console.log("Typing dataset name...");
 		await page.type('input[data-test-id="dataset-name"]', 'Dataset '+name);
@@ -70,7 +70,7 @@ const name = 'AJI';
 		id = await page.evaluate(() => {
 			var tags = Array.from(document.getElementsByTagName("h3"));
 			for (var i = 0; i < tags.length; i++) {
-			  if (tags[i].textContent == 'Dataset AJI') {
+			  if (tags[i].textContent == 'Dataset puppeteer test') {
 				var found = tags[i];
 				break;
 			  }
@@ -83,44 +83,41 @@ const name = 'AJI';
 		await console.log("Accessing to visualisation creation...");
 		await page.click('button[data-test-id="visualisation"]');
 		await console.log("Selecting pivot table option...");
-		await page.waitForSelector('li[data-test-id="button-pivot-table"]', {timeout:5000});
+		await page.waitForSelector('li[data-test-id="button-pivot-table"]', {timeout:10000});
 		await page.click('li[data-test-id="button-pivot-table"]');
 		await console.log("Selecting dataset...");
-		await page.waitForSelector('label[data-test-id="dataset-menu"]+div', {timeout:5000});
+		await page.waitForSelector('label[data-test-id="dataset-menu"]+div', {timeout:10000});
 		await page.click('label[data-test-id="dataset-menu"]+div');
-		
-		await page.waitForSelector('div[data-test-id="input-group"]>div>div>div:nth-of-type(2)', {timeout:5000});
+		await page.waitForSelector('div[data-test-id="input-group"]>div>div>div:nth-of-type(2)', {timeout:10000});
 		await page.click('div[data-test-id="input-group"]>div>div>div:nth-of-type(2)');
-		
-		await page.waitForSelector('label[data-test-id="categoryColumnInput"]+div', {timeout:5000});
+		await page.waitForSelector('label[data-test-id="categoryColumnInput"]+div', {timeout:10000});
 		await page.click('label[data-test-id="categoryColumnInput"]+div');
-		await page.waitForSelector('label[data-test-id="categoryColumnInput"]+div>div>div:nth-of-type(2)', {timeout:5000});
+		await page.waitForSelector('label[data-test-id="categoryColumnInput"]+div>div>div:nth-of-type(2)', {timeout:10000});
 		await page.click('label[data-test-id="categoryColumnInput"]+div>div>div:nth-of-type(2)');
-		await page.waitForSelector('input[name="categoryColumnInput"]', {timeout:5000});
+		await page.waitForSelector('input[name="categoryColumnInput"]', {timeout:10000});
 		await console.log("Selecting columns...");
 		await page.$eval('input[name="categoryColumnInput"]', el => el.value='c9');
 		await page.click('div[data-test-id="entity-title"]');
 		await console.log("Typing visualisation name...");
-		await page.type('input[data-test-id="entity-title"]', 'Visualizacion de '+name);
+		await page.type('input[data-test-id="entity-title"]', 'Visualisation of '+name);
 		await console.log("Saving visualisation...");
 		await page.click('button[data-test-id="save-changes"]');
-		await page.click('i[data-test-id="fa-arrow"]');
+		await page.goto('http://t1.lumen.local:3030/library');
+		await page.waitForSelector('button[data-test-id="dashboard"]', {timeout:10000});
 		await console.log("Visualisation "+name+" was successfully created.\n");
 		//Dashboard
 		await console.log("Accessing to dashboard creation...");
-		await page.waitForSelector('button[data-test-id="dashboard"]', {timeout:5000});
 		await page.click('button[data-test-id="dashboard"]');
 		await console.log("Selecting visualisation...");
-		await page.waitForSelector('li[class^="listItem"]', {timeout:5000});
+		await page.waitForSelector('li[class^="listItem"]', {timeout:10000});
 		await page.click('li[class^="listItem"]');
 		await console.log("Typing dashboard name...");
-		await page.waitForSelector('div[data-test-id="dashboard-canvas-item"]', {timeout:5000});
+		await page.waitForSelector('div[data-test-id="dashboard-canvas-item"]', {timeout:10000});
 		await page.click('div[data-test-id="entity-title"]');
-		await page.type('input[data-test-id="entity-title"]', 'Dashboard de '+name);
+		await page.type('input[data-test-id="entity-title"]', 'Dashboard of '+name);
 		await console.log("Saving dashboard...");
 		await page.click('button[data-test-id="save-changes"]');
 		await page.click('i[data-test-id="fa-arrow"]');
-		await page.waitForSelector('div[data-test-id="library"]', {timeout:5000});
 		await console.log("Dashboard "+name+" was successfully created.\n");
 		await console.log("THE TEST WAS SUCCESSFUL");
 		//Delete
