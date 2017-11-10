4daccbd11b091486f0212a0f8c44ebead6962bc8
Rubén Baztán <ruazulgrana10@gmail.com>
2017-11-10 13:00:24 +0100

    [#1096] Add tests using docker-compose exec

--- ci/build.sh
+++ ci/build.sh
@@ -18,6 +18,10 @@ while [ -z "${CLIENT}" ] ; do
     CLIENT=$(docker-compose logs --no-color client | grep "webpack: Compiled successfully" || echo "")
 done
 
+docker-compose exec client su-exec akvo npm run lint
+docker-compose exec client su-exec akvo npm run test
+docker-compose exec backend su-exec akvo lein do clean, check, test :all
+
 docker run --interactive --tty --shm-size 1G --rm --network=akvolumen_default \
        --volume "$PWD/client/ui-test/script-test.js":/app/index.js --link akvolumen_client_1:t1.lumen.local \
        --link akvolumen_keycloak_1:auth.lumen.local --link akvolumen_client_1:t1.lumen.local \
