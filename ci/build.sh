5a465e369a0c3791b1d6da0f6409eda7b46cc1e9
Rubén Baztán <ruazulgrana10@gmail.com>
2017-11-13 12:43:07 +0100

    [#1096] Add timeout when waiting to backend and client services

--- ci/build.sh
+++ ci/build.sh
@@ -3,19 +3,22 @@
 set -eu
 
 docker-compose up --no-color --build -d
-
+MAX=30
+TRIES=0
 BACKEND=""
-while [ -z "${BACKEND}" ] ; do
+while [[ -z "${BACKEND}" && ${TRIES} -lt ${MAX} ]]; do
     echo "Waiting for backend to start ..."
     sleep 5
     BACKEND=$(docker-compose logs --no-color backend | grep "nREPL server started" || echo "")
+    let TRIES=${TRIES}+1
 done
-
+TRIES=0
 CLIENT=""
-while [ -z "${CLIENT}" ] ; do
+while [[ -z "${CLIENT}" && ${TRIES} -lt ${MAX} ]]; do
     echo "Waiting for client to start ..."
     sleep 5
     CLIENT=$(docker-compose logs --no-color client | grep "webpack: Compiled successfully" || echo "")
+    let TRIES=${TRIES}+1
 done
 
 docker-compose exec client su-exec akvo npm run lint
