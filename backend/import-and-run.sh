207e38e2c36cb3c3e57a3a9cdf5fe55963ed3620
Iván Perdomo <ivan@perdomo.me>
2017-11-14 13:46:30 +0100

    [#1161] Initial changes for Dev environment
    - Install postgis, gdal-bin, gosu in an Debian based env
    - We can't use alpine becasue gdal package is only available on edge/testing

--- backend/import-and-run.sh
+++ backend/import-and-run.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-keytool -import -trustcacerts -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
+set -eu
+
+keytool -import -trustcacerts -keystore "${JAVA_HOME}/jre/lib/security/cacerts" -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
 
 ./run-as-user.sh lein repl :headless
