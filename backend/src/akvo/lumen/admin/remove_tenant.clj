f8c1950770546d3eb3c2d9d618355be64fb7b363
Daniel Karlsson <daniel@kardans.com>
2017-11-17 09:35:08 +0100

    [#1156] Update doc string on remove-tenant script

--- backend/src/akvo/lumen/admin/remove_tenant.clj
+++ backend/src/akvo/lumen/admin/remove_tenant.clj
@@ -1,14 +1,29 @@
 (ns akvo.lumen.admin.remove-tenant
+  "The following env vars are assumed to be present:
+  ENCRYPTION_KEY, KC_URL, KC_SECRET, PG_HOST, PG_DATABASE, PG_USER, PG_PASSWORD
+  ENCRYPTION_KEY is a key specific for the Kubernetes environment used for
+  encrypting the db_uri.
+  The PG_* env vars can be found in the ElephantSQL console for the appropriate
+  instance. KC_URL is the url to keycloak (without trailing /auth).
+  KC_SECRET is the client secret found in the Keycloak admin at
+  > Realms > Akvo > Clients > akvo-lumen-confidential > Credentials > Secret.
+  Use this as follow
+  $ env ENCRYPTION_KEY=*** \\
+  KC_URL=https://*** KC_SECRET=*** \\
+  PG_HOST=***.db.elephantsql.com PG_DATABASE=*** \\
+  PG_USER=*** PG_PASSWORD=*** \\
+  lein run -m akvo.lumen.admin.remove-tenant label
+  KC_URL is probably one of:
+  - http://localhost:8080 for local development
+  - https://login.akvo.org for production
+  - https://kc.akvotest.org for the test environment
+
+  It's not possible to delete a database if open connections exist, workaround:
+  https://dba.stackexchange.com/questions/11893/force-drop-db-while-others-may-be-connected"
   (:require [akvo.lumen.admin.util :as util]
             [akvo.lumen.component.keycloak :as keycloak]
             [cheshire.core :as json]
             [clj-http.client :as client]))
-
-;; The following env vars are assumed to be present:
-;; PG_HOST,  PG_DATABASE, PG_USER, PG_PASSWORD, KC_URL, KC_SECRET
-;; See add-tenant script for instructions on where to find these values
-;;
-;; $ lein run -m akvo.lumen.admin.remove-tenant <label>
 
 (defn remove-group
   "Remove keycloak group by id"
