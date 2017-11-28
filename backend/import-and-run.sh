533dc566b227ccb82f7d465e64b6483b78bd913a
Iván Perdomo <ivan@perdomo.me>
2017-11-28 06:30:51 +0100

    [#1161] Attempt to start backend once keycloak is available

--- backend/import-and-run.sh
+++ backend/import-and-run.sh
@@ -4,4 +4,18 @@ set -eu
 
 keytool -import -trustcacerts -keystore "${JAVA_HOME}/jre/lib/security/cacerts" -storepass changeit -noprompt -alias postgrescert -file /pg-certs/server.crt
 
+MAX_ATTEMPTS=30
+KEYCLOAK=""
+ATTEMPTS=0
+
+while [[ -z "${KEYCLOAK}" && "${ATTEMPTS}" -lt "${MAX_ATTEMPTS}" ]]; do
+    printf "Waiting for Keycloak "
+    printf "."
+    sleep 5
+    KEYCLOAK=$((curl -v "http://auth.lumen.local:8080/auth/realms/akvo/.well-known/openid-configuration" 2>&1 | grep "HTTP/1.1 200 OK") || echo "")
+    let ATTEMPTS=${ATTEMPTS}+1
+done
+
+echo ""
+
 ./run-as-user.sh lein repl :headless
