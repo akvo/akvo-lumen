f1c5af210d4cd7f2b9cd41bbf982f0a427663938
gabemart <g4bemart@gmail.com>
2017-12-05 15:37:42 +0000

    [#1163] Remove unused code and move styles to stylesheet

--- client/src/components/visualisation/VisualisationViewerContainer.jsx
+++ client/src/components/visualisation/VisualisationViewerContainer.jsx
@@ -2,20 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AsyncVisualisationViewer from '../charts/AsyncVisualisationViewer';
 
-const viewportLimits = [
-  {
-    limit: 720,
-    name: 'small',
-  },
-  {
-    limit: 1024,
-    name: 'medium',
-  },
-  {
-    limit: Infinity,
-    name: 'large',
-  },
-];
+require('./VisualisationViewerContainer.scss');
 
 export default class VisualisationViewerContainer extends Component {
   constructor() {
@@ -23,7 +10,6 @@ export default class VisualisationViewerContainer extends Component {
     this.state = {
       clientHeight: 500,
       clientWidth: 1080,
-      viewportType: 'large',
     };
 
     this.handleResize = this.handleResize.bind(this);
@@ -31,9 +17,7 @@ export default class VisualisationViewerContainer extends Component {
 
   componentDidMount() {
     this.handleResize();
-    window.addEventListener(
-      'resize', () => this.handleResize
-    );
+    window.addEventListener('resize', this.handleResize);
   }
 
   componentWillUnmount() {
@@ -43,21 +27,10 @@ export default class VisualisationViewerContainer extends Component {
   handleResize() {
     const height = this.VisualisationViewerContainer.clientHeight;
     const width = this.VisualisationViewerContainer.clientWidth;
-    let viewport;
-
-    for (let i = 0; i < viewportLimits.length; i += 1) {
-      const entry = viewportLimits[i];
-
-      if (width < entry.limit) {
-        viewport = entry.name;
-        break;
-      }
-    }
 
     this.setState({
       clientHeight: height,
       clientWidth: width,
-      viewportType: viewport,
     });
   }
 
@@ -65,7 +38,7 @@ export default class VisualisationViewerContainer extends Component {
     const { datasets, visualisation } = this.props;
     return (
       <div
-        style={{ width: '100%', height: '100%' }}
+        className="VisualisationViewerContainer"
         ref={(ref) => { this.VisualisationViewerContainer = ref; }}
       >
         <AsyncVisualisationViewer
