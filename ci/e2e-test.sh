22b7427538fd472eb6450376cc728179df2a2a0a
Dan Lebrero <dlebrero@gmail.com>
2018-01-26 11:07:40 +0100

    [#1239] end to end test parametrization of docker project name

--- ci/e2e-test.sh
+++ ci/e2e-test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker run --interactive --tty --shm-size 1G --rm --network=akvolumen_default \
-       --volume "$PWD/client/e2e-test/script-test.js":/app/index.js --link akvolumen_client_1:t1.lumen.local \
-       --link akvolumen_keycloak_1:auth.lumen.local --sysctl net.ipv6.conf.all.disable_ipv6=1 alekzonder/puppeteer:0.13.0
+DOCKER_COMPOSE_PROJECT=${1:-akvolumen}
+
+docker run --interactive --tty --shm-size 1G --network=${DOCKER_COMPOSE_PROJECT}_default \
+       --volume "$PWD/client/e2e-test/script-test.js":/app/index.js --link ${DOCKER_COMPOSE_PROJECT}_client_1:t1.lumen.local \
+       --link ${DOCKER_COMPOSE_PROJECT}_keycloak_1:auth.lumen.local --sysctl net.ipv6.conf.all.disable_ipv6=1 alekzonder/puppeteer:0.13.0
