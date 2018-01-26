9ba0629e7c36487c8bc27281605cd2f2cdbb60c2
Dan Lebrero <dlebrero@gmail.com>
2018-01-26 16:17:28 +0100

    [#1239] Deploy when merge to master or develop

---
+++ ci/deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -eu
+
+export PROJECT_NAME=akvo-lumen
+
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    exit 0
+fi
+
+# Making sure gcloud and kubectl are installed and up to date
+gcloud components install kubectl
+gcloud components update
+gcloud version
+which gcloud kubectl
+
+# Authentication with gcloud and kubectl
+gcloud auth activate-service-account --key-file ci/gcloud-service-account.json
+gcloud config set project akvo-lumen
+gcloud config set container/cluster europe-west1-d
+gcloud config set compute/zone europe-west1-d
+gcloud config set container/use_client_certificate True
+
+if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+    gcloud container clusters get-credentials test
+else
+    gcloud container clusters get-credentials lumen
+fi
+
+# Pushing images
+gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/lumen-backend
+gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/lumen-client
+gcloud docker -- push eu.gcr.io/${PROJECT_NAME}/lumen-maps
+
+# Deploying
+sed -e "s/\${BUILD_HASH}/$TRAVIS_COMMIT/" ci/deployment.yaml.template > deployment.yaml
+
+kubectl apply -f deployment.yaml
+kubectl apply -f ci/redis-master-windshaft.yaml
