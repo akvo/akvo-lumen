02a95cbbf3a56dfa43ce21cd1821417fe4782b27
Dan Lebrero <dlebrero@gmail.com>
2018-01-29 09:47:04 +0100

    [#1239] Deleting old Travis build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,28 +1,54 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
-docker-compose up --no-color --build -d
-MAX=30
-TRIES=0
-BACKEND=""
-while [[ -z "${BACKEND}" && ${TRIES} -lt ${MAX} ]]; do
-    echo "Waiting for backend to start ..."
-    sleep 5
-    BACKEND=$(docker-compose logs --no-color backend | grep "nREPL server started" || echo "")
-    let TRIES=${TRIES}+1
-done
-TRIES=0
-CLIENT=""
-while [[ -z "${CLIENT}" && ${TRIES} -lt ${MAX} ]]; do
-    echo "Waiting for client to start ..."
-    sleep 5
-    CLIENT=$(docker-compose logs --no-color client | grep "webpack: Compiled successfully" || echo "")
-    let TRIES=${TRIES}+1
-done
+function log {
+   echo "`date +"%T"` - INFO - $@"
+}
 
-docker-compose exec client su-exec akvo npm run lint
-docker-compose exec client su-exec akvo npm run test
-docker-compose exec backend su-exec akvo lein do clean, check, test :all
+BRANCH_NAME="${TRAVIS_BRANCH:=unknown}"
+export PROJECT_NAME=akvo-lumen
 
-./ci/e2e-test.sh
+if [ -z "$TRAVIS_COMMIT" ]; then
+    export TRAVIS_COMMIT=local
+fi
+
+log Bulding container to run the backend tests
+docker build --rm=false -t akvo-lumen-backend-dev:develop backend -f backend/Dockerfile-dev
+log Running Backend unit tests and building uberjar
+docker run --env-file=.env -v $HOME/.m2:/home/akvo/.m2 -v `pwd`/backend:/app akvo-lumen-backend-dev:develop /app/run-as-user.sh lein do test, uberjar
+
+cp backend/target/uberjar/akvo-lumen.jar backend
+
+log Creating Production Backend image
+docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/lumen-backend:${TRAVIS_COMMIT} ./backend
+docker tag eu.gcr.io/${PROJECT_NAME}/lumen-backend:${TRAVIS_COMMIT} eu.gcr.io/${PROJECT_NAME}/lumen-backend:develop
+
+rm backend/akvo-lumen.jar
+
+log Building container to run the client tests
+docker build --rm=false -t akvo-lumen-client-dev:develop client -f client/Dockerfile-dev
+log Running Client linting, unit tests and creating production assets
+docker run --env-file=.env -v `pwd`/client:/lumen --rm=false -t akvo-lumen-client-dev:develop /lumen/run-as-user.sh /lumen/ci-build.sh
+
+log Creating Production Client image
+docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/lumen-client:${TRAVIS_COMMIT} ./client
+docker tag eu.gcr.io/${PROJECT_NAME}/lumen-client:${TRAVIS_COMMIT} eu.gcr.io/${PROJECT_NAME}/lumen-client:develop
+
+log Creating Production Windshaft image
+docker build --rm=false -t eu.gcr.io/${PROJECT_NAME}/lumen-maps:${TRAVIS_COMMIT} ./windshaft
+docker tag eu.gcr.io/${PROJECT_NAME}/lumen-maps:${TRAVIS_COMMIT} eu.gcr.io/${PROJECT_NAME}/lumen-maps:develop
+
+log Starting Docker Compose environment
+docker-compose -p akvo-lumen-ci -f docker-compose.yml -f docker-compose.ci.yml up --no-color -d --build
+
+bash ci/wait-for-docker-compose-to-start.sh
+
+log Running Backend functional tests
+docker-compose -p akvo-lumen-ci -f docker-compose.yml -f docker-compose.ci.yml run --no-deps backend-functional-tests /app/import-and-run.sh functional-and-seed
+
+log Running the end to end tests
+./ci/e2e-test.sh akvolumenci http://t1.lumen.local/
+
+log Done
+#docker-compose -p akvo-lumen-ci -f docker-compose.yml -f docker-compose.ci.yml down
