9c8ef16c3955cc223baa4e5278fd56f96928e86d
Dan Lebrero <dlebrero@gmail.com>
2018-01-31 00:11:28 +0100

    [#1243] Switching deployment branch back to develop

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,7 +8,7 @@ function log {
 
 export PROJECT_NAME=akvo-lumen
 
-if [[ "${TRAVIS_BRANCH}" != "issue-1243/k8s-tests" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
     exit 0
 fi
 
