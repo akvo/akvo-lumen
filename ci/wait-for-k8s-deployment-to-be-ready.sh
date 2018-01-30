49988021751bbe060b7ad8015d5a5b40dade564a
Dan Lebrero <dlebrero@gmail.com>
2018-01-30 15:12:30 +0100

    [#1243] Added script that waits for the lumen deploy to be done

---
+++ ci/wait-for-k8s-deployment-to-be-ready.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=akvo-lumen
+
+starttime=`date +%s`
+
+while [ $(( $(date +%s) - 300 )) -lt ${starttime} ]; do
+
+    lumen_status=`kubectl get pods -l "lumen-version=$TRAVIS_COMMIT,run=lumen" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
+
+# We want to make sure that when we hit the ingress from the integration test, we are hitting the new containers,
+# hence we wait until the old pods are gone.
+# Another possibility could be to check that the service is pointing just to the new containers.
+    old_lumen_status=`kubectl get pods -l "lumen-version!=$TRAVIS_COMMIT,run=lumen" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'`
+
+    if [[ ${lumen_status} =~ "ready=true" ]] && ! [[ ${old_lumen_status} =~ "ready" ]] ; then
+        exit 0
+    else
+        echo "Waiting for the containers to be ready"
+        sleep 10
+    fi
+done
+
+echo "Containers not ready after 5 minutes or old containers not stopped"
+
+kubectl get pods -l "run=lumen" -ao jsonpath='{range .items[*].status.containerStatuses[*]}{@.name}{" ready="}{@.ready}{"\n"}{end}'
+
+exit 1
