76f788f0a4df9304a2a2ffbda96ab4a0a207e524
Dan Lebrero <dlebrero@gmail.com>
2018-02-05 16:02:37 +0100

    [#1253] Checking that the color is either blue or green

--- ci/dark-color.sh
+++ ci/dark-color.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 LIVE_COLOR=$1
-if [ ${LIVE_COLOR} == "blue" ]; then
+if [ "${LIVE_COLOR}" = "blue" ]; then
     DARK_COLOR=green
-else
+elif [ "${LIVE_COLOR}" = "green" ]; then
     DARK_COLOR=blue
+else
+  echo "Color must be blue or green"
+  exit 1
 fi
 echo ${DARK_COLOR}
