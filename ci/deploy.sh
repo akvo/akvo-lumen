ec25f2367818430ad57832a910cedcf0203fa758
Dan Lebrero <dlebrero@gmail.com>
2018-02-06 00:48:39 +0100

    [#1253] End to end tests running against the dark deployment

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -64,6 +64,6 @@ log Waiting for k8s to be healthy
 ./ci/helpers/wait-for-k8s-deployment-to-be-healthy.sh
 
 log Running end to end tests against the Kubernetes TEST environment
-./ci/e2e-test.sh script-test akvolumenci https://lumencitest.akvotest.org/ $USERNAME $PASSWORD
+./ci/e2e-test.sh script-test akvolumenci https://dark-lumencitest.akvotest.org/ $USERNAME $PASSWORD
 log Cleaning up environment
-./ci/e2e-test.sh clean-all akvolumenci https://lumencitest.akvotest.org/ $USERNAME $PASSWORD || echo "Ignoring error during cleanup"
+./ci/e2e-test.sh clean-all akvolumenci https://dark-lumencitest.akvotest.org/ $USERNAME $PASSWORD || echo "Ignoring error during cleanup"
