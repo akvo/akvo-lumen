a1a0128ff60abf94328e2b87440729a230806dc4
Dan Lebrero <dlebrero@gmail.com>
2018-02-06 18:55:54 +0100

    [#1253] Adding annotations about who and when the flip was done
    (cherry picked from commit f472589)
    [skip ci]

--- ci/set-live-deployment.sh
+++ ci/set-live-deployment.sh
@@ -12,6 +12,11 @@ COLOR=${1}
 DARK_COLOR=$("${DIR}"/helpers/dark-color.sh "${COLOR}")
 
 log Setting "${COLOR}" as LIVE
-sed -e "s/\${LIVE_COLOR}/${COLOR}/" -e "s/\${DARK_COLOR}/${DARK_COLOR}/" "${DIR}"/k8s/service.yaml > "${DIR}"/service.yaml
+FLIP_DATE=$(echo -n $(date -u +'%Y-%m-%dT%H:%M:%SZ'))
+ACCOUNT=$(echo -n $(gcloud config get-value core/account))
+sed -e "s/\${LIVE_COLOR}/${COLOR}/" -e "s/\${DARK_COLOR}/${DARK_COLOR}/" \
+  -e "s/\${UTCDATE}/${FLIP_DATE}/" \
+  -e "s/\${ACCOUNT}/${ACCOUNT}/" \
+  "${DIR}"/ci/k8s/service.yaml > "${DIR}"/service.yaml
 
 kubectl apply -f service.yaml
