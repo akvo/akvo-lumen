61920a4cf433b4a0d7ff725b7edc6d1c2ffc08ae
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2018-02-13 09:30:53 +0200

    stop endless reload with new webpack setup

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -16,7 +16,7 @@ export default {
       'webpack-hot-middleware/client?reload=true&http://0.0.0.0:3030',
       path.resolve(__dirname, 'src/index.jsx'), // Defining path seems necessary for this to work consistently on Windows machines.
     ],
-    pub: path.resolve(__dirname, 'src/index.jsx'),
+    pub: path.resolve(__dirname, 'src/index-pub.jsx'),
   },
   target: 'web',
   output: {
