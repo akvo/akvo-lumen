86effb8161a133d87616ad22afb275c40139d2e9
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2018-02-14 14:15:39 +0200

    #1278 debugging if client static server serves files correctly in ci build

--- client/ci-build.sh
+++ client/ci-build.sh
@@ -19,3 +19,6 @@ npm run build
 
 log Starting static test server
 npm run start:static:forever
+
+log TODO remove this
+npm run ping
---
+++ client/tools/pingStaticServer.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget -qO- http://localhost:3030/index.html
+echo 
+echo 
+echo 
+wget -qO- http://localhost:3030/assets/index-pub.html
