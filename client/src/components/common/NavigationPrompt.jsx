ee5231bd5da0378328261e508cfd49de59bb34f0
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2018-04-30 11:05:57 +0300

    #878 no prompt for unsaved changes fixes

--- client/src/components/common/NavigationPrompt.jsx
+++ client/src/components/common/NavigationPrompt.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 
 class NavigationPrompt extends React.Component {
@@ -14,7 +15,10 @@ class NavigationPrompt extends React.Component {
     const { message, shouldPrompt, router, route, routes } = this.props;
     router.setRouteLeaveHook(
       routes.length ? routes[routes.length - 1] : route,
-      () => shouldPrompt && confirm(message)
+      () => {
+        if (!shouldPrompt) return true;
+        return confirm(message);
+      }
     );
     window.onbeforeunload = shouldPrompt && (() => message);
   }
