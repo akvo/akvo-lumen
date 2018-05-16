007008502d076dec01ee2253e27f43a126d0ec81
Juan A. Ruz <juanantonioruz@gmail.com>
2018-05-16 11:53:38 +0200

    Measure js-execution with big-clj-rows

--- backend/test/akvo/lumen/transformation/derive/js_engine_test.clj
+++ backend/test/akvo/lumen/transformation/derive/js_engine_test.clj
@@ -11,11 +11,26 @@
   (testing "simple js fn invocation "
     (let [js-engine (js-engine/js-engine)
           fun-name "test_fun"
-          
           row-fun (time* :row-fun (#'js-engine/column-function fun-name "row.prop"))]
       (->> row-fun (#'js-engine/eval* js-engine))
       (doseq [n (mapv first (next (take 20 (partition 1000 (range)))))]
        (time* (keyword (str "doseq-" n))
               (doseq [x (range n)]
-                (assert (= x ((#'js-engine/invocation js-engine fun-name) {"prop" x})) x)))))))
+                (assert (= x ((#'js-engine/invocation js-engine fun-name) {"prop" x})) x))))
+      (is (int (rand-int 100)))))
+
+  (testing "trying with big rows"
+    (let [js-engine (js-engine/js-engine)
+          fun-name "test_fun"
+          idx (rand-int 100)
+          message (apply str (conj (repeat 2000 "x") "-"))
+          row (reduce #(assoc % (str "prop-" %2) (str %2 message)) {} (range 100))
+          row-fun (time* :row-fun (#'js-engine/column-function fun-name (str "row['prop-" idx "']")))]
+      (->> row-fun (#'js-engine/eval* js-engine)) 
+      (doseq [n (mapv first (next (take 20 (partition 1000 (range)))))]
+        (time* (keyword (str "doseq-" n))
+               (doseq [x (range n)]
+                 (assert (=  (str idx message) ((#'js-engine/invocation js-engine fun-name) row))))))
+      (is (int (rand-int 100)))))
+  )
 
