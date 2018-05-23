8200355428c74e14162a69bc537fca4751530811
Juan A. Ruz <juanantonioruz@gmail.com>
2018-05-23 11:49:27 +0200

    Fix travis

--- backend/test/akvo/lumen/db/tardis_test.clj
+++ backend/test/akvo/lumen/db/tardis_test.clj
@@ -1,4 +1,5 @@
 (ns akvo.lumen.db.tardis-test
+  {:functional true}
   (:require [akvo.lumen.fixtures :refer [*tenant-conn* tenant-conn-fixture]]
             [clojure.string :as str]
             [clojure.test :refer :all]
