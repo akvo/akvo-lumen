2fee3bf4596fef8fa0555d5d4ca6301da11ea5f5
gabemart <g4bemart@gmail.com>
2018-06-01 09:14:50 +0100

    [#1381] Fix sharing of Area charts

--- backend/src/akvo/lumen/lib/public_impl.clj
+++ backend/src/akvo/lumen/lib/public_impl.clj
@@ -44,6 +44,19 @@
       {"visualisations" {(:id visualisation) (assoc visualisation :data query-result)}
         "datasets" { dataset-id (dissoc dataset :rows)}})))
 
+(defmethod visualisation "area"
+  [tenant-conn visualisation config]
+  (let [dataset-id (:datasetId visualisation)
+        [dataset-tag dataset] (dataset/fetch tenant-conn dataset-id)
+        [tag query-result] (aggregation/query tenant-conn
+                                              dataset-id
+                                              "line"
+                                              (:spec visualisation))]
+    (when (and (= tag ::lib/ok)
+               (= dataset-tag ::lib/ok))
+      {"visualisations" {(:id visualisation) (assoc visualisation :data query-result)}
+        "datasets" { dataset-id (dissoc dataset :rows)}})))
+
 (defmethod visualisation "pie"
   [tenant-conn visualisation config]
   (let [dataset-id (:datasetId visualisation)
