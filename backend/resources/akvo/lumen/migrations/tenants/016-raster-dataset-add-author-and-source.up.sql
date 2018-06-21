2c4dc80e9b0c90278c58e4498ec654ff31bf326f
Juan A. Ruz <juanantonioruz@gmail.com>
2018-06-21 17:03:29 +0200

    Adding source column to raster-dataset
    Renaming files

--- backend/resources/akvo/lumen/migrations/tenants/016-raster-dataset-add-author-and-source.up.sql
+++ backend/resources/akvo/lumen/migrations/tenants/016-raster-dataset-add-author-and-source.up.sql
@@ -1,2 +1,4 @@
 ALTER TABLE public.raster_dataset ADD COLUMN author jsonb;
+ALTER TABLE public.raster_dataset ADD COLUMN source jsonb;
 ALTER TABLE history.raster_dataset ADD COLUMN author jsonb;
+ALTER TABLE history.raster_dataset ADD COLUMN source jsonb;
