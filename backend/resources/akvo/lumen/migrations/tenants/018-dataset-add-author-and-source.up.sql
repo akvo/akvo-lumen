84b247f89b09e4988c926b5a69a3ac11d924adfb
Juan A. Ruz <juanantonioruz@gmail.com>
2018-06-22 11:19:47 +0200

    Update migration: dataset-add-author-and-source

--- backend/resources/akvo/lumen/migrations/tenants/018-dataset-add-author-and-source.up.sql
+++ backend/resources/akvo/lumen/migrations/tenants/018-dataset-add-author-and-source.up.sql
@@ -1,3 +1,5 @@
 ALTER TABLE public.dataset ADD COLUMN author jsonb;
 ALTER TABLE history.dataset ADD COLUMN author jsonb;
+ALTER TABLE public.dataset ADD COLUMN source jsonb;
+ALTER TABLE history.dataset ADD COLUMN source jsonb;
 
