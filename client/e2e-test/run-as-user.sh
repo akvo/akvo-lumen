2db64b687a5d59fa2bfa3e52171b36ab3ced3a87
Dan Lebrero <dlebrero@gmail.com>
2018-07-03 18:21:15 +0200

    [#1443] Not running groupmod or usermod for Macs

--- client/e2e-test/run-as-user.sh
+++ client/e2e-test/run-as-user.sh
@@ -6,15 +6,15 @@ NEW_UID=$(stat -c '%u' /app/Dockerfile)
 NEW_GID=$(stat -c '%g' /app/Dockerfile)
 
 if [ ${NEW_GID} -eq 0 ]; then
-    /bin/bash
+    echo "Assuming you are running in a Mac"
 else
     groupmod -g "$NEW_GID" -o akvo >/dev/null 2>&1
     usermod -u "$NEW_UID" -o akvo >/dev/null 2>&1
-
-    exec chpst -u akvo:akvo -U akvo:akvo env \
-            HOME=/home/akvo \
-            CYPRESS_LUMEN_URL=${LUMEN_URL} \
-            CYPRESS_LUMEN_USER=${LUMEN_USER} \
-            CYPRESS_LUMEN_PASSWORD=${LUMEN_PASSWORD} \
-            "/app/run.sh"
 fi
+
+exec chpst -u akvo:akvo -U akvo:akvo env \
+        HOME=/home/akvo \
+        CYPRESS_LUMEN_URL=${LUMEN_URL} \
+        CYPRESS_LUMEN_USER=${LUMEN_USER} \
+        CYPRESS_LUMEN_PASSWORD=${LUMEN_PASSWORD} \
+        "/app/run.sh"
