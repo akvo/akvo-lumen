d852a98444f0cd91c1ac23828991b7484e663476
Dan Lebrero <dlebrero@gmail.com>
2018-07-04 09:20:25 +0200

    [#1443] Revert: Breaking test to test CI

--- client/e2e-test/cypress/integration/index.spec.js
+++ client/e2e-test/cypress/integration/index.spec.js
@@ -54,8 +54,8 @@ context('Kitchen Sink', () => {
     cy.get('[data-test-id="dataset-menu"] .Select-control').click();
     // Finding dataset option
     cy.get('[role="option"]')
-      .contains("aasdfasdfsadfsdfasdsadffdas")
-      .should('have.attr', 'idxadfasdfqw12312312323423421341')
+      .contains(datasetName)
+      .should('have.attr', 'id')
       .then(optionId => {
         // Selecting dataset
         cy.get(`#${optionId}`).click();
