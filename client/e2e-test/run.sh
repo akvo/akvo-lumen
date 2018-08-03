9dc6af4958d305f496240526387369bec7bbd806
Daniel Karlsson <daniel@kardans.com>
2018-08-03 14:37:43 +0200

    [#952] Peek at docker compose

--- client/e2e-test/run.sh
+++ client/e2e-test/run.sh
@@ -1,10 +1,6 @@
 echo "running cypress against $LUMEN_URL"
 
 
-echo "About to run docker-compose check"
-docker-compose ps
-docker-compose logs backend
-echo "ran docker-compose check"
 
 if [ -z "${CYPRESS_RECORD_KEY}" ]; then
     npm run cypress:run
