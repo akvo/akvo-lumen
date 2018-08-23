6243589c9925864823693b04f82f239c516269eb
Juan A. Ruz <juanantonioruz@gmail.com>
2018-08-23 09:05:43 +0200

    Split logic/function to get next-column-int from next-colum-name [#1571]

--- backend/src/akvo/lumen/transformation/engine.clj
+++ backend/src/akvo/lumen/transformation/engine.clj
@@ -90,16 +90,23 @@
 (defn args [op-spec]
   (get op-spec "args"))
 
-(defn next-column-name [columns]
+(defn derivation-column-name
+  [i]
+  {:pre [(int? i)]}
+  (format "d%s" i))
+
+(defn next-column-index [columns]
   (let [nums (->> columns
                   (map #(get % "columnName"))
                   (filter #(re-find #"^d\d+$" %))
                   (map #(subs % 1))
                   (map #(Long/parseLong %)))]
-    (str "d"
-         (if (empty? nums)
-           1
-           (inc (apply max nums))))))
+    (if (empty? nums)
+      1
+      (inc (apply max nums)))))
+
+(defn next-column-name [columns]
+  (derivation-column-name (next-column-index columns)))
 
 (defn- deliver-promise-success [promise dataset-id dataset-version-id job-execution-id]
   (deliver promise {:status "OK"
