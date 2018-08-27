8738a620b7109e281c1a41165984143ae3568f76
Juan A. Ruz <juanantonioruz@gmail.com>
2018-08-27 11:12:44 +0200

    Add core/extract-multiple transformation op [#1571]

--- backend/src/akvo/lumen/endpoint/transformation.clj
+++ backend/src/akvo/lumen/endpoint/transformation.clj
@@ -3,17 +3,17 @@
             [akvo.lumen.transformation :as t]
             [compojure.core :refer :all]))
 
-(defn endpoint [{:keys [tenant-manager]}]
+(defn endpoint [{:keys [tenant-manager caddisfly]}]
   (context "/api/transformations" {:keys [tenant] :as request}
     (let-routes [tenant-conn (connection tenant-manager tenant)]
       (context "/:dataset-id" [dataset-id]
         (POST "/transform" {:keys [body] :as request}
-              (t/apply {:tenant-conn tenant-conn}
+              (t/apply {:tenant-conn tenant-conn :caddisfly caddisfly}
                    dataset-id
                    {:type :transformation
                     :transformation body}))
 
         (POST "/undo" _
-              (t/apply {:tenant-conn tenant-conn}
+              (t/apply {:tenant-conn tenant-conn :caddisfly caddisfly}
                    dataset-id
                    {:type :undo}))))))
