3df157995665297783f61113ab4cb9be55a7978f
Juan A. Ruz <juanantonioruz@gmail.com>
2018-10-04 12:28:52 +0200

    Clean endpoint.healthz

--- backend/src/akvo/lumen/endpoint/healthz.clj
+++ backend/src/akvo/lumen/endpoint/healthz.clj
@@ -1,19 +1,16 @@
 (ns akvo.lumen.endpoint.healthz
   (:require [akvo.lumen.http :as http]
+            [compojure.core :refer :all]
             [environ.core :as env]
-            [integrant.core :as ig]
-            [clojure.tools.logging :as log]
-            [compojure.core :refer :all]))
+            [integrant.core :as ig]))
 
 (defn endpoint [_]
   (GET "/healthz" {:as req}
-    (http/ok {:healthz "ok"
-              :pod (env/env :pod-name)
-              :blue-green-status (get-in req [:headers "x-blue-green-state"])})))
+       (http/ok {:healthz           "ok"
+                 :pod               (env/env :pod-name)
+                 :blue-green-status (get-in req [:headers "x-blue-green-state"])})))
 
 (defmethod ig/init-key :akvo.lumen.endpoint.healthz/healthz  [_ opts]
-  (log/debug "init-key" :akvo.lumen.endpoint.healthz :opts opts)
   (endpoint opts))
 
-(defmethod ig/halt-key! :akvo.lumen.endpoint.healthz/healthz  [_ opts]
-  (log/debug "halt-key" :akvo.lumen.endpoint.healthz opts))
+(defmethod ig/halt-key! :akvo.lumen.endpoint.healthz/healthz  [_ opts])
