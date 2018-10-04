1a892506f55ee4b735babe71112999f4d2e4f152
Juan A. Ruz <juanantonioruz@gmail.com>
2018-10-04 10:39:09 +0200

    Clean component.http

--- backend/src/akvo/lumen/component/http.clj
+++ backend/src/akvo/lumen/component/http.clj
@@ -1,33 +1,18 @@
 (ns akvo.lumen.component.http
   "Immutant web Duct component."
-  (:require [com.stuartsierra.component :as component]
-            [clojure.tools.logging :as log]
-            [integrant.core :as ig]
+  (:require [integrant.core :as ig]
             [immutant.web :as web]))
 
-(defrecord Http [app]
-  component/Lifecycle
-
-  (start [this]
+(defmethod ig/init-key :akvo.lumen.component.http/http  [_ {:keys [config app] :as opts}]
+  (let [this (assoc (-> config :http) :app app)]
     (if (:server this)
       this
       (assoc this :server (web/run
                             (-> this :app :handler)
                             (assoc {:host "0.0.0.0"}
-                                   :port (:port this))))))
+                                   :port (:port this)))))))
 
-  (stop [this]
-    (when-some [server (:server this)]
-      (web/stop server))
-    (dissoc this :server)))
-
-(defn http [options]
-  (map->Http options))
-
-(defmethod ig/init-key :akvo.lumen.component.http/http  [_ {:keys [config app] :as opts}]
-  (log/debug "init-key"  opts)
-  (component/start (http (assoc (-> config :http) :app app))))
-
-(defmethod ig/halt-key! :akvo.lumen.component.http/http  [_ opts]
-  (log/debug "halt-key"  opts)
-  (component/stop opts))
+(defmethod ig/halt-key! :akvo.lumen.component.http/http  [_ {:keys [server] :as opts}]
+  (when server
+    (web/stop server))
+    (dissoc opts :server))
