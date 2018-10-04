f7bc651d27373b7082f99c8c8ba62c803d4f0b06
Juan A. Ruz <juanantonioruz@gmail.com>
2018-10-04 18:38:57 +0200

    Clean main namespace

--- backend/src/akvo/lumen/main.clj
+++ backend/src/akvo/lumen/main.clj
@@ -1,37 +1,25 @@
 (ns akvo.lumen.main
   (:gen-class)
-  (:require [akvo.commons.psql-util]
-            [akvo.lumen.config :as config]
-            [akvo.lumen.middleware]
+  (:require [akvo.lumen.config :as config]
             [akvo.lumen.endpoint]
+            [akvo.lumen.middleware]
             [akvo.lumen.migrate :as migrate]
             [clojure.java.io :as io]
             [duct.core :as duct]
-            [com.stuartsierra.component :as component]
-            [integrant.core :as ig]
-            [environ.core :refer [env]]))
+            [integrant.core :as ig]))
+
+(def config-file "akvo/lumen/config.edn")
 
 (defn read-config []
-  (duct/read-config (io/resource "akvo/lumen/config.edn")))
+  (duct/read-config (io/resource config-file)))
 
 (defn -main [& args]
   (config/assert-bindings)
   (derive :akvo.lumen.component.emailer/mailjet-emailer :akvo.lumen.component.emailer/emailer)
   (derive :akvo.lumen.component.caddisfly/prod :akvo.lumen.component.caddisfly/caddisfly)
   (derive :akvo.lumen.component.error-tracker/prod :akvo.lumen.component.error-tracker/error-tracker)
-  #_(underive :akvo.lumen.component.emailer/mailjet-emailer :akvo.lumen.component.emailer/emailer)
-  #_(underive :akvo.lumen.component.caddisfly/prod :akvo.lumen.component.caddisfly/caddisfly)
-  #_(underive :akvo.lumen.component.error-tracker/prod :akvo.lumen.component.error-tracker/error-tracker)
-  (migrate/migrate "akvo/lumen/config.edn")
+  (migrate/migrate config-file)
   (let [config ((comp duct/prep read-config))
         _ (ig/load-namespaces config)
         system (ig/init config)]
-    system
-    )
-
-  #_(let [system (load-system [(io/resource "akvo/lumen/system.edn")]
-                              (config/bindings))]
-      (println "Starting HTTP server on port" (-> system :http :port))
-      (migrate/migrate)
-      (add-shutdown-hook ::stop-system #(component/stop system))
-      (component/start system)))
+    system))
