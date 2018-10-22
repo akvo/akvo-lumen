ecfdd7ab7130b2a7751487e60d5517beb61a2e01
Juan A. Ruz <juanantonioruz@gmail.com>
2018-10-22 13:55:57 +0200

    Refactor

--- backend/src/akvo/lumen/dataset.clj
+++ backend/src/akvo/lumen/dataset.clj
@@ -8,7 +8,7 @@
             [clojure.tools.logging :as log]
             [clojure.java.jdbc :as jdbc]
             [clojure.string :as str]
-            [clojure.set :refer (rename-keys) :as set]
+            [clojure.set :refer (rename-keys)]
             [hugsql.core :as hugsql]))
 
 (hugsql/def-db-fns "akvo/lumen/dataset.sql")
