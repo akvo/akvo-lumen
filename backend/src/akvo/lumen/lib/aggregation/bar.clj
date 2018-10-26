076e281e71a2c46174492f539e68e96e586d54dd
Juan A. Ruz <juanantonioruz@gmail.com>
2018-10-26 11:44:28 +0200

    Update bar.clj

--- backend/src/akvo/lumen/lib/aggregation/bar.clj
+++ backend/src/akvo/lumen/lib/aggregation/bar.clj
@@ -28,7 +28,7 @@
         column-y-name (get column-y "columnName")
         column-y-title (get column-y "title")
 
-        aggregation-method (if (and (not column-y) column-subbucket) "count" (get query "metricAggregation"))
+        aggregation-method (if (and (not column-y) column-x) "count" (get query "metricAggregation"))
         truncate-size (or (get query "truncateSize") "ALL")
         sql-sort-subquery (case (get query "sort")
                             nil "ORDER BY x ASC"
@@ -78,9 +78,9 @@
             sort_table.include_value IS NOT NULL
           " sql-sort-subbucket-subquery)
 
-        valid-spec (boolean (and column-x (or column-y (and (not column-y) column-subbucket))))
+        valid-spec (boolean column-x)
         sql-text (if valid-spec (if column-subbucket sql-text-with-subbucket sql-text-without-subbucket) "SELECT NULL")
-        sql-response (run-query tenant-conn table-name sql-text column-x-name column-y-name filter-sql aggregation-method truncate-size column-subbucket-name)
+        sql-response (run-query tenant-conn table-name sql-text column-x-name (or column-y-name column-x-name) filter-sql aggregation-method truncate-size column-subbucket-name)
         bucket-values (distinct
                        (mapv
                         (fn [[x-value y-value s-value]] x-value)
