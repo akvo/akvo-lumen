7732913c4581e2082ad9becf0ea0f24d9962da30
Juan A. Ruz <juanantonioruz@gmail.com>
2018-12-12 09:37:42 +0100

    [#1853] Add clj data importer with update test support

---
+++ backend/test/akvo/lumen/lib/import/clj_data_importer_test.clj
@@ -0,0 +1,75 @@
+(ns akvo.lumen.lib.import.clj-data-importer-test
+  {:functional true}
+  (:require [akvo.lumen.fixtures :refer [*tenant-conn*
+                                         tenant-conn-fixture
+                                         *error-tracker*
+                                         error-tracker-fixture]]
+            [clojure.tools.logging :as log]
+            [akvo.lumen.specs.import :as i-c]
+            [akvo.lumen.lib.import.clj-data-importer :as i]
+            [akvo.lumen.test-utils :refer [import-file update-file]]
+            [akvo.lumen.utils.logging-config :refer [with-no-logs]]
+            [clojure.string :as string]
+            [clojure.test :refer :all]
+            [hugsql.core :as hugsql])
+  (:import [java.util.concurrent ExecutionException]))
+
+(hugsql/def-db-fns "akvo/lumen/lib/job-execution.sql")
+(hugsql/def-db-fns "akvo/lumen/lib/transformation_test.sql")
+(hugsql/def-db-fns "akvo/lumen/lib/transformation.sql")
+
+(use-fixtures :once tenant-conn-fixture error-tracker-fixture)
+
+(deftest ^:functional test-import
+  (testing "Testing import"
+    (let [dataset-id (import-file *tenant-conn* *error-tracker* 
+                                  {:dataset-name "Padded titles"
+                                   :kind "clj"
+                                   :data (i-c/sample-imported-dataset [:text :number] 2) })
+          dataset (dataset-version-by-dataset-id *tenant-conn* {:dataset-id dataset-id
+                                                                :version 1})
+          stored-data (->> (latest-dataset-version-by-dataset-id *tenant-conn*
+                                                                 {:dataset-id dataset-id})
+                           (get-data *tenant-conn*))]
+      (is (= (map keys (:columns dataset)) '(("key"
+	                                      "sort"
+	                                      "direction"
+	                                      "title"
+	                                      "type"
+	                                      "multipleType"
+	                                      "hidden"
+	                                      "multipleId"
+	                                      "columnName")
+                                             ("key"
+	                                      "sort"
+	                                      "direction"
+	                                      "title"
+	                                      "type"
+	                                      "multipleType"
+	                                      "hidden"
+	                                      "multipleId"
+	                                      "columnName"))))
+
+      (is (= (map #(select-keys % ["type" "columnName"]) (:columns dataset))
+             '({"type" "text", "columnName" "c0"}
+	       {"type" "number", "columnName" "c1"})))
+
+      (is (= (map keys stored-data) '((:rnum :c0 :c1) (:rnum :c0 :c1)))))))
+
+(deftest ^:functional test-update
+  (testing "Testing update"
+    (let [[job dataset] (import-file *tenant-conn* *error-tracker* 
+                                     {:dataset-name "Padded titles"
+                                      :kind "clj"
+                                      :data (i-c/sample-imported-dataset [:text :number] 2) 
+                                      :with-job? true})
+          dataset-id (:dataset_id dataset)
+          dataset (dataset-version-by-dataset-id *tenant-conn* {:dataset-id dataset-id
+                                                                :version 1})
+          stored-data (->> (latest-dataset-version-by-dataset-id *tenant-conn*
+                                                                 {:dataset-id dataset-id})
+                           (get-data *tenant-conn*))
+          updated-res (update-file *tenant-conn* *error-tracker* (:dataset_id job) (:data_source_id job)
+                        {:kind "clj"
+                         :data (i-c/sample-imported-dataset [:text :number] 2)})]
+      (is (some? updated-res)))))
--- backend/test/akvo/lumen/test_utils.clj
+++ backend/test/akvo/lumen/test_utils.clj
@@ -1,13 +1,16 @@
 (ns akvo.lumen.test-utils
   (:require [akvo.lumen.component.tenant-manager :refer [pool]]
+            [akvo.lumen.lib.import :as import]
+            [akvo.lumen.lib.import.clj-data-importer]
+            [akvo.lumen.lib.update :as update]
+            [akvo.lumen.postgres]
             [akvo.lumen.util :refer [squuid]]
             [clojure.edn :as edn]
-            [akvo.lumen.lib.import :as import]
-            [diehard.core :as dh]
-            [clojure.tools.logging :as log]
-            [clojure.test :as t]
-            [clojure.spec.test.alpha :as stest]
             [clojure.java.io :as io]
+            [clojure.spec.test.alpha :as stest]
+            [clojure.test :as t]
+            [clojure.tools.logging :as log]
+            [diehard.core :as dh]
             [hugsql.core :as hugsql]))
 
 (hugsql/def-db-fns "akvo/lumen/lib/job-execution.sql")
@@ -43,11 +46,12 @@
 
 (defn import-file
   "Import a file and return the dataset-id, or the job-execution-id in case of FAIL status"
-  [tenant-conn error-tracker {:keys [file dataset-name has-column-headers?]}]
+  [tenant-conn error-tracker {:keys [file dataset-name has-column-headers? kind data with-job?]}]
   (let [spec {"name" (or dataset-name file)
-              "source" {"path" (.getAbsolutePath (io/file (io/resource file)))
-                        "kind" "DATA_FILE"
+              "source" {"path" (when file (.getAbsolutePath (io/file (io/resource file))))
+                        "kind" (or kind "DATA_FILE")
                         "fileName" (or dataset-name file)
+                        "data" data
                         "hasColumnHeaders" (boolean has-column-headers?)}}
         [tag {:strs [importId]}] (import/handle tenant-conn {} error-tracker {} spec)]
     (t/is (= tag :akvo.lumen.lib/ok))
@@ -55,8 +59,31 @@
                     :max-retries 20
                     :delay-ms 100}
       (let [job (job-execution-by-id tenant-conn {:id importId})
+            status (:status job)
+            dataset (dataset-id-by-job-execution-id tenant-conn {:id importId})
+            res (when (not= "PENDING" status)
+                  (if (= "OK" status)
+                    (:dataset_id dataset)
+                    importId))]
+        (when res
+          (if  with-job?
+            [job dataset]
+            res))))))
+
+(defn update-file
+  "Update a file and return the dataset-id, or the job-execution-id in case of FAIL status"
+  [tenant-conn error-tracker dataset-id data-source-id {:keys [data has-column-headers? kind]}]
+  (let [spec {"source" {"kind" kind
+                        "hasColumnHeaders" (boolean has-column-headers?)
+                        "data" data}}
+        [tag {:strs [updateId] :as res}] (update/update-dataset tenant-conn {} error-tracker dataset-id data-source-id spec)]
+    (t/is (= tag :akvo.lumen.lib/ok))
+    (dh/with-retry {:retry-if (fn [v e] (not v))
+                    :max-retries 20
+                    :delay-ms 100}
+      (let [job (job-execution-by-id tenant-conn {:id updateId})
             status (:status job)]
-                 (when (not= "PENDING" status)
-                   (if (= "OK" status)
-                     (:dataset_id (dataset-id-by-job-execution-id tenant-conn {:id importId}))
-                     importId))))))
+        (when (not= "PENDING" status)
+          (if (= "OK" status)
+            (:dataset_id (dataset-id-by-job-execution-id tenant-conn {:id updateId}))
+            updateId))))))
