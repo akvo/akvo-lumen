5d582e931279dc3194ca2941b1a10992c71f388e
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2018-12-13 11:13:47 +0200

    #1854 long text inside bubble legend doesnt work

--- client/src/components/charts/BubbleLegend.jsx
+++ client/src/components/charts/BubbleLegend.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './BubbleLegend.scss';
+import { FormattedMessage, injectIntl } from 'react-intl';
 
-const BubbleLegend = ({ title }) => (
+const MAX_LENGTH = 30;
+
+const BubbleLegend = ({ title }) => title.length > MAX_LENGTH ? (
   <div className="BubbleLegend">
-    <div className="BubbleLegend__bubble BubbleLegend__bubble--sm" />
-    <div className="BubbleLegend__bubble BubbleLegend__bubble--md" />
-    <div className="BubbleLegend__bubble BubbleLegend__bubble--lg" />
+    <h4>
+      <FormattedMessage id="size" />
+    </h4>
+    <p>{title}</p>
+  </div>
+) : (
+  <div className="BubbleLegend">
+    <div className="BubbleLegend__bubbles">
+      <div className="BubbleLegend__bubble BubbleLegend__bubble--sm" />
+      <div className="BubbleLegend__bubble BubbleLegend__bubble--md" />
+      <div className="BubbleLegend__bubble BubbleLegend__bubble--lg" />
+    </div>
     <div className="BubbleLegend__title">
       {title}
     </div>
@@ -18,4 +30,4 @@ BubbleLegend.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
-export default BubbleLegend;
+export default injectIntl(BubbleLegend);
