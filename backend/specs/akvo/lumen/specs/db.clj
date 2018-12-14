f4f1de1fd029b9106f48f11d9f7c68ee326c64a1
Juan A. Ruz <juanantonioruz@gmail.com>
2018-12-14 12:44:09 +0100

    [#1441]Add db related specs

---
+++ backend/specs/akvo/lumen/specs/db.clj
@@ -0,0 +1,16 @@
+(ns akvo.lumen.specs.db
+  (:require [clojure.spec.alpha :as s])
+  (:import com.zaxxer.hikari.HikariDataSource))
+
+(s/def ::uri (s/with-gen string?
+	       #(s/gen #{"jdbc:postgresql://postgres/lumen?user=lumen&password=password&ssl=true"})))
+
+(s/def ::datasource (s/with-gen (partial instance? HikariDataSource)
+		      #(s/gen #{(HikariDataSource.)})))
+
+(s/def ::spec (s/keys :req-un [::datasource]))
+
+(s/def ::db (s/keys :req-un [::datasource]
+		    :opt-un [::uri]))
+
+(s/def ::tenant-connection ::spec)
