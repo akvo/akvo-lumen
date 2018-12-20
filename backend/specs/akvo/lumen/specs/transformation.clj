d82957e0d3b31129167ab02443ab27f5ce85dbdb
Juan A. Ruz <juanantonioruz@gmail.com>
2018-12-20 13:09:55 +0100

    WIP Add transformation opts specs

--- backend/specs/akvo/lumen/specs/transformation.clj
+++ backend/specs/akvo/lumen/specs/transformation.clj
@@ -56,9 +56,62 @@
 
 (s/def ::db.dsv/columns (s/coll-of ::db.dsv/column :kind vector? :distinct true))
 
-
-
 (s/def ::next-dataset-version (s/keys :req-un [::db.dsv/id ::db.dsv/dataset-id
                                                ::db.dsv/job-execution-id ::db.dsv/table-name
                                                ::db.dsv/imported-table-name ::db.dsv/version
                                                ::db.dsv/transformations ::db.dsv/columns]))
+
+(s/def ::type #{:transformation :undo})
+
+
+(defmulti command-spec ::type)
+
+(defmethod command-spec :undo [_]
+  (s/keys :req [::type]))
+
+(defmethod command-spec :transformation [_]
+  (s/keys :req [::type]))
+
+(s/def ::command (s/multi-spec command-spec ::type))
+
+(lumen.s/sample ::command)
+
+(create-ns  'akvo.lumen.specs.transformation.engine)
+(alias 'transformation.engine 'akvo.lumen.specs.transformation.engine)
+
+(s/def ::transformation.engine/onError #{"leave-empty" "fail" "delete-row" "default-value"})
+
+(s/def ::transformation.engine/op #{:core/change-datatype
+				    :core/combine
+				    :core/delete-column
+				    :core/derive
+				    :core/filter-column
+				    :core/generate-geopoints
+				    :core/merge-datasets
+				    :core/remove-sort
+				    :core/rename-column
+				    :core/reverse-geocode
+				    :core/sort-column
+				    :core/to-lowercase
+				    :core/to-titlecase
+				    :core/to-uppercase
+				    :core/trim
+				    :core/trim-doublespace})
+
+(defmulti op-spec ::transformation.engine/op)
+
+(s/def ::transformation.engine/op-spec
+  (s/multi-spec op-spec ::transformation.engine/op))
+
+(create-ns  'akvo.lumen.specs.transformation.delete-column)
+(alias 'transformation.delete-column 'akvo.lumen.specs.transformation.delete-column)
+
+(s/def ::transformation.delete-column/args
+  (s/keys :req-un [::db.dsv.column/columnName]))
+
+(defmethod op-spec :core/delete-column  [_]
+  (s/keys
+   :req-un [::transformation.delete-column/args
+	    ::transformation.engine/onError]))
+
+(lumen.s/sample ::transformation.engine/op-spec 10)
