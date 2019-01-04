e2f57c6d2379c6a3af1bc8b9ff70d92dbb850150
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2019-01-04 14:14:01 +0200

    #1838 refactoring client api utility

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -1,27 +1,21 @@
-// import fetch from 'whatwg-fetch';
 import { omit } from 'lodash';
 
 import * as auth from './auth';
 
-function wrapUpdateToken(fetchRequestThunk) {
-  return auth.token()
-    .then(token => fetchRequestThunk(token));
-}
+const wrapUpdateToken = callback => auth.token().then(token => callback(token));
 
-function requestHeaders(token, additionalHeaders = {}) {
-  return Object.assign({}, additionalHeaders, {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${token}`,
-  });
-}
+const requestHeaders = (token, additionalHeaders = {}) => ({
+  ...additionalHeaders,
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${token}`,
+});
 
-function getQueryString(queryParams) {
-  return Object.keys(queryParams).map((key, index) =>
+const getQueryString = queryParams =>
+  Object.keys(queryParams).map((key, index) =>
     `${index > 0 ? '&' : ''}${key}=${encodeURIComponent(queryParams[key])}`
   ).join('');
-}
 
-export function get(url, queryParams, headers) {
+export const get = (url, queryParams, headers) => {
   const urlWithOptionalParams = queryParams == null ? url : `${url}?${getQueryString(queryParams)}`;
   return wrapUpdateToken(token =>
     fetch(urlWithOptionalParams, {
@@ -29,43 +23,40 @@ export function get(url, queryParams, headers) {
       headers: omit(requestHeaders(token, headers), 'Content-Type'),
     })
   );
-}
+};
 
-export function post(url, body, headers) {
-  return wrapUpdateToken(token =>
+export const post = (url, body, headers) =>
+  wrapUpdateToken(token =>
     fetch(url, {
       method: 'POST',
       headers: requestHeaders(token, headers),
       body: JSON.stringify(body),
     })
   );
-}
 
-export function put(url, body, headers) {
-  return wrapUpdateToken(token =>
+export const put = (url, body, headers) =>
+  wrapUpdateToken(token =>
     fetch(url, {
       method: 'PUT',
       headers: requestHeaders(token, headers),
       body: JSON.stringify(body),
     })
   );
-}
 
-export function del(url, headers) {
-  return wrapUpdateToken(token =>
+export const del = (url, headers) =>
+  wrapUpdateToken(token =>
     fetch(url, {
       method: 'DELETE',
       headers: requestHeaders(token, headers),
     })
   );
-}
 
-export function patch(url, body, headers) {
-  return wrapUpdateToken(token =>
+export const patch = (url, body, headers) =>
+  wrapUpdateToken(token =>
     fetch(url, {
       method: 'PATCH',
       headers: requestHeaders(token, headers),
       body: JSON.stringify(body),
     })
   );
-}
+
