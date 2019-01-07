c5b4db1d6c6b1cd026eea51c6f0028c1f1099d18
Juan A. Ruz <juanantonioruz@gmail.com>
2019-01-07 14:14:17 +0100

    Add specs for a.l.specs.postgres.filter

---
+++ backend/specs/akvo/lumen/specs/postgres.clj
@@ -0,0 +1,45 @@
+(ns akvo.lumen.specs.postgres
+  (:require [akvo.lumen.specs :as lumen.s :refer (sample)]
+            [akvo.lumen.specs.db :as db.s]
+            [akvo.lumen.postgres :as postgres]
+            [akvo.lumen.postgres.filter :as postgres.filter]
+            [akvo.lumen.util :as u]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.string :as string]
+            [clojure.tools.logging :as log]))
+
+(alias 'db.dsv 'akvo.lumen.specs.db.dataset-version)
+
+(s/def ::postgres.filter/strategy #{"isHigher" "isLower" "is" "isEmpty"})
+
+(s/def ::postgres.filter/operation #{"keep"  "remove"})
+
+(s/def ::postgres.filter/aggregation #{"mean" "sum" "min" "max" "count"})
+
+(s/def ::postgres.filter/value (s/nilable string?))
+
+(s/def ::postgres.filter/filter (s/keys :req-un [::postgres.filter/operation
+                                                 ::postgres.filter/strategy
+                                                 ::postgres.filter/value
+                                                 ::db.dsv/column]))
+
+(s/def ::postgres.filter/filters (s/coll-of ::postgres.filter/filter :gen-max 3))
+
+(s/def ::postgres.filter/categoryColumn ::db.dsv/column)
+
+(s/def ::postgres.filter/rowColumn ::db.dsv/column)
+
+(s/def ::postgres.filter/valueColumn ::db.dsv/column)
+
+(s/def ::postgres.filter/aggregation string?)
+
+(s/def ::postgres.filter/comparison-op #{">" "<=" "<" ">=" "=" "<>"})
+
+(s/fdef postgres.filter/sql-str
+  :args (s/cat
+	 :columns ::db.dsv/columns
+	 :filters ::postgres.filter/filters)
+  :ret string?)
+
+
