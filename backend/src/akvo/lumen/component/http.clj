3880b86384a1cd6c36d336ff27735aeb83cd8b9c
Juan A. Ruz <juanantonioruz@gmail.com>
2019-01-22 12:58:52 +0100

    [#1928] Change http component config initialisation

--- backend/src/akvo/lumen/component/http.clj
+++ backend/src/akvo/lumen/component/http.clj
@@ -3,14 +3,14 @@
   (:require [integrant.core :as ig]
             [immutant.web :as web]))
 
-(defmethod ig/init-key :akvo.lumen.component.http/http  [_ {:keys [config app] :as opts}]
-  (let [this (assoc (-> config :http) :app app)]
-    (if (:server this)
-      this
-      (assoc this :server (web/run
-                            (-> this :app :handler)
-                            (assoc {:host "0.0.0.0"}
-                                   :port (:port this)))))))
+(defmethod ig/init-key :akvo.lumen.component.http/http  [_ {:keys [port handler] :as opts}]
+  (if (:server opts)
+    opts
+    (assoc opts
+           :server (web/run
+                     (:handler handler)
+                     (assoc {:host "0.0.0.0"}
+                            :port port)))))
 
 (defmethod ig/halt-key! :akvo.lumen.component.http/http  [_ {:keys [server] :as opts}]
   (when server
