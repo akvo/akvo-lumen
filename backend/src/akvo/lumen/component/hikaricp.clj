8161785996fed37d77cebb1e36a4de69c2685372
Juan A. Ruz <juanantonioruz@gmail.com>
2019-01-22 12:57:14 +0100

    [#1928] Change hikaricp component config initialisation

--- backend/src/akvo/lumen/component/hikaricp.clj
+++ backend/src/akvo/lumen/component/hikaricp.clj
@@ -3,11 +3,9 @@
             [integrant.core :as ig]
             [duct.database.sql.hikaricp]))
 
-(defmethod ig/init-key :akvo.lumen.component.hikaricp/hikaricp  [_ {:keys [ config] :as opts}]
-  (ig/init-key :duct.database.sql/hikaricp
-               (-> config
-                   :db
-                   (clojure.set/rename-keys {:uri :jdbc-url}))))
+(defmethod ig/init-key :akvo.lumen.component.hikaricp/hikaricp  [_ opts]
+  (println opts)
+  (ig/init-key :duct.database.sql/hikaricp (clojure.set/rename-keys opts {:uri :jdbc-url})))
 
 (defmethod ig/halt-key! :akvo.lumen.component.hikaricp/hikaricp  [_ opts]
   (ig/halt-key! :duct.database.sql/hikaricp opts))
