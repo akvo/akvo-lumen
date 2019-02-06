a1bf88f520978393cb436ddc19b433f7d96cc3c2
Daniel Sunnerek <daniel@kardans.com>
2019-02-06 11:34:29 +0100

    [#1949] WIP idea on library filtering

--- backend/src/akvo/lumen/endpoint/library.clj
+++ backend/src/akvo/lumen/endpoint/library.clj
@@ -11,16 +11,58 @@
             [compojure.core :refer :all]
             [integrant.core :as ig]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Experimental
+;;;
+
+(defn forms-per-instance [datasets]
+  (reduce (fn [m {{:strs [formId instance surveryId]} :source}]
+            (update m instance #(conj % formId)))
+          {}
+          datasets))
+
+(defn allowed-forms
+  "By the help of the forms-per-instance map get a allowed forms"
+  [forms-per-instance]
+  (let [fake-allowed-forms (set forms-per-instance)]
+    fake-allowed-forms))
+
+(defn filter-library
+  [allowed-forms library]
+  library)
+
+(defn apply-flow-permissons
+  "Depending on if the Flow API is benefited by getting a list of Forms or
+  rather just list everything for a user:
+  1. Build map of instance:
+  {:instance-a [form-a form-b]
+   :instance-b [form-c form-d]}
+  2. Call each instance and maybe post forms to a filter or get a list of forms
+  per user back and filter flow forms locally.
+  3. Use the filtered flow form list and list library
+  4. Return to user
+  "
+  [refresh-token {:keys [datasets] :as library}]
+  (-> (forms-per-instance datasets)
+      (allowed-forms)
+      (filter-library library)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Endpoint
+;;;
+
 (defn endpoint [{:keys [tenant-manager]}]
   (context "/api/library" {:keys [tenant] :as request}
     (let-routes [tenant-conn (p/connection tenant-manager tenant)]
-      (GET "/" _
+      (GET "/" {{refresh-token "refreshToken"} :body}
         (lib/ok
-         {:dashboards (variant/value (dashboard/all tenant-conn))
-          :datasets (variant/value (dataset/all tenant-conn))
-          :rasters (variant/value (raster/all tenant-conn))
-          :visualisations (variant/value (visualisation/all tenant-conn))
-          :collections (variant/value (collection/all tenant-conn))})))))
+         (apply-flow-permissons refresh-token
+                                {:dashboards (variant/value (dashboard/all tenant-conn))
+                                 :datasets (variant/value (dataset/all tenant-conn) )
+                                 :rasters (variant/value (raster/all tenant-conn))
+                                 :visualisations (variant/value (visualisation/all tenant-conn))
+                                 :collections (variant/value (collection/all tenant-conn))}))))))
 
 (defmethod ig/init-key :akvo.lumen.endpoint.library/library  [_ opts]
   (endpoint opts))
