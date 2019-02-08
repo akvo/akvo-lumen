df53990735c0aa908b7801ffb32690dbe4d53743
Juan A. Ruz <juanantonioruz@gmail.com>
2019-02-08 21:00:29 +0100

    Spec a.l.component.hikaricp

--- backend/src/akvo/lumen/component/hikaricp.clj
+++ backend/src/akvo/lumen/component/hikaricp.clj
@@ -1,6 +1,8 @@
 (ns akvo.lumen.component.hikaricp
   (:require [clojure.set]
             [integrant.core :as ig]
+            [clojure.spec.alpha :as s]
+            [akvo.lumen.specs.components :refer (integrant-key)]
             [duct.database.sql.hikaricp]))
 
 (defmethod ig/init-key :akvo.lumen.component.hikaricp/hikaricp  [_ opts]
@@ -8,3 +10,17 @@
 
 (defmethod ig/halt-key! :akvo.lumen.component.hikaricp/hikaricp  [_ opts]
   (ig/halt-key! :duct.database.sql/hikaricp opts))
+
+(s/def ::uri string?)
+(s/def ::pool-name string?)
+(s/def ::maximum-pool-size pos-int?)
+(s/def ::minimum-idle pos-int?)
+(s/def ::metric-registry any?) ;; TODO
+
+(defmethod integrant-key :akvo.lumen.component.hikaricp/hikaricp [_]
+  (s/cat :kw keyword?
+         :config (s/keys :req-un [::uri
+                                  ::pool-name
+                                  ::maximum-pool-size
+                                  ::minimum-idle
+                                  ::metric-registry])))
