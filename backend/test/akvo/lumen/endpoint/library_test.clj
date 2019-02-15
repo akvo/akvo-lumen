082d6e5ab933e9504d3b53a3b260eb09f3de0e9f
Juan A. Ruz <juanantonioruz@gmail.com>
2019-02-15 12:20:04 +0100

    Remove unimplemented endpoint tests

--- backend/test/akvo/lumen/endpoint/library_test.clj
+++
@@ -1,9 +0,0 @@
-(ns akvo.lumen.endpoint.library-test
-  (:require [akvo.lumen.endpoint.library :as library]))
-
-(def handler
-  (library/handler {}))
-
-#_(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
--- backend/test/akvo/lumen/endpoint/transformation_test.clj
+++
@@ -1,5 +0,0 @@
-(ns akvo.lumen.endpoint.transformation-test
-  (:require [akvo.lumen.endpoint.transformation :as transformation]))
-
-(def handler
-  #_(transformation/endpoint {}))
