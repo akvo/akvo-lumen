f29df9d5a9f033b116ef416cc754f329e8a11aa1
Juan A. Ruz <juanantonioruz@gmail.com>
2019-02-21 15:46:55 +0100

    Fix spec in user-test

--- backend/test/akvo/lumen/lib/user_test.clj
+++ backend/test/akvo/lumen/lib/user_test.clj
@@ -28,7 +28,7 @@
 
 (defn fixture [f]
   (migrate-tenant test-tenant)
-  (binding [*emailer* (ig/init-key :akvo.lumen.component.emailer/dev-emailer {})
+  (binding [*emailer* (ig/init-key :akvo.lumen.component.emailer/dev-emailer {:config {:from-email "" :from-name ""}})
             *keycloak* (ig/init-key :akvo.lumen.component.keycloak keycloak-config)
             *tenant-conn* (test-tenant-conn test-tenant)]
     (f)))
