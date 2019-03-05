65b6422cb4e1ece4ef0eb0a62f184f2a71f8c24b
Daniel Sunnerek <daniel@kardans.com>
2019-03-05 12:36:58 +0100

    [#2022] Remove unused import

--- client/src/components/visualisation/VisualisationHeader.jsx
+++ client/src/components/visualisation/VisualisationHeader.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage, intlShape, injectIntl } from 'react-intl';
-import queryString from 'querystringify';
 
 import EntityTypeHeader from '../entity-editor/EntityTypeHeader';
 import LoadingSpinner from '../common/LoadingSpinner';
