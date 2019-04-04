21bfcb08e7f3c9faceefc8f523166dbce2a43366
Juan A. Ruz <juanantonioruz@gmail.com>
2019-04-04 13:17:25 +0200

    [#1949] Spec lib.auth/new-dbqs

---
+++ backend/specs/akvo/lumen/specs/auth.clj
@@ -0,0 +1,17 @@
+(ns akvo.lumen.specs.auth
+  (:require [clojure.tools.logging :as log]
+            [akvo.lumen.lib.auth :as l.auth]
+            [akvo.lumen.specs.db :as db.s]
+            [akvo.lumen.specs.dataset :as dataset.s] 
+            [akvo.lumen.specs.visualisation :as visualisation.s]
+            [clojure.spec.alpha :as s]))
+
+(s/def ::auth-visualisations (s/coll-of ::visualisation.s/id :distinct true))
+(s/def ::auth-datasets (s/coll-of ::dataset.s/id :distinct true))
+(s/def ::auth-uuid-tree (s/keys :req-un [::auth-datasets ::auth-visualisations]))
+
+(s/fdef l.auth/new-dbqs
+  :args (s/cat
+         :db-conn ::db.s/tenant-connection
+	 :auth-uuid-tree ::auth-uuid-tree)
+  :ret any?)
