e25fef4a9f31ae94a0dac5baae2351111f4eb4f3
Juan A. Ruz <juanantonioruz@gmail.com>
2019-04-09 11:49:24 +0200

    [#1949] Auth spec approach

--- backend/specs/akvo/lumen/specs/dataset.clj
+++ backend/specs/akvo/lumen/specs/dataset.clj
@@ -1,17 +1,9 @@
 (ns akvo.lumen.specs.dataset
-  (:require [akvo.lumen.lib.dataset.utils :as dataset.utils]
-            [akvo.lumen.specs.db.dataset-version :as db.dsv.s]
-            [akvo.lumen.specs :as lumen.s]
+  (:require [akvo.lumen.specs :as lumen.s]
             [clojure.spec.alpha :as s]))
 
+(def ^:dynamic *id?*  lumen.s/str-uuid?)
+
 (s/def ::id (s/with-gen
-              lumen.s/str-uuid?
+              #'*id?*
               lumen.s/str-uuid-gen))
-
-
-(s/fdef dataset.utils/find-column
-  :args (s/alt :by-name (s/cat :columns ::db.dsv.s/columns
-                               :v (s/nilable string?))
-               :by-filter (s/cat :columns ::db.dsv.s/columns
-                                 :v (s/nilable string?)
-                                 :filter-by keyword?)))
