8cb7f1ea3facfe4761b64f9f73a83d51fb0fb05e
Juan A. Ruz <juanantonioruz@gmail.com>
2019-04-12 16:24:29 +0200

    [#1949] Auth col-id in "* /api/collections/:id" with middleware

--- backend/src/akvo/lumen/endpoint/collection.clj
+++ backend/src/akvo/lumen/endpoint/collection.clj
@@ -3,6 +3,8 @@
             [akvo.lumen.lib.collection :as collection]
             [clojure.spec.alpha :as s]
             [akvo.lumen.lib :as lib]
+            [akvo.lumen.lib.auth :as l.auth]
+            [akvo.lumen.specs.collection :as collection.s]
             [akvo.lumen.component.tenant-manager :as tenant-manager]
             [clojure.tools.logging :as log]
             [akvo.lumen.lib.collection :as collection]
@@ -21,23 +23,32 @@
              :parameters {:body map?}
              :handler (fn [{tenant :tenant
                             body :body}]
+                        (log/error :POST-body body)
                         (collection/create (p/connection tenant-manager tenant) (stringify-keys body)))}}]
-   ["/:id" {:get {:responses {200 {}}
-                  :parameters {:path-params {:id string?}}
+   ["/:id"
+    {:middleware [(fn [handler]
+                    (fn [{{:keys [id]} :path-params
+                          auth-service :auth-service
+                          :as req}]
+                      (if (p/allow? auth-service (l.auth/ids ::collection.s/id id))
+                        (handler req)
+                        (lib/not-authorized {:id id}))))]}
+    ["" {:get {:responses {200 {}}
+               :parameters {:path-params {:id string?}}
+               :handler (fn [{tenant :tenant
+                              {:keys [id]} :path-params}]
+                          (collection/fetch (p/connection tenant-manager tenant) id))}
+         :put {:responses {200 {}}
+               :parameters {:body map?
+                            :path-params {:id string?}}
+               :handler (fn [{tenant :tenant
+                              body :body
+                              {:keys [id]} :path-params}]
+                          (collection/update (p/connection tenant-manager tenant) id body))}
+         :delete {:parameters {:path-params {:id string?}}
                   :handler (fn [{tenant :tenant
                                  {:keys [id]} :path-params}]
-                             (collection/fetch (p/connection tenant-manager tenant) id))}
-            :put {:responses {200 {}}
-                  :parameters {:body map?
-                               :path-params {:id string?}}
-                  :handler (fn [{tenant :tenant
-                                 body :body
-                                 {:keys [id]} :path-params}]
-                             (collection/update (p/connection tenant-manager tenant) id body))}
-            :delete {:parameters {:path-params {:id string?}}
-                         :handler (fn [{tenant :tenant
-                                        {:keys [id]} :path-params}]
-                                    (collection/delete (p/connection tenant-manager tenant) id))}}]])
+                             (collection/delete (p/connection tenant-manager tenant) id))}}]]])
 
 (defmethod ig/init-key :akvo.lumen.endpoint.collection/collection  [_ opts]
   (routes opts))
