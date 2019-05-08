f314f7808ae88fa4b3d8a874b492f08f2553d3c0
Daniel Sunnerek <daniel@kardans.com>
2019-05-08 09:06:44 +0200

    [#2059] Update E2E tests to use Sentry ENV vars

--- ci/e2e-test.sh
+++ ci/e2e-test.sh
@@ -3,29 +3,35 @@
 set -o errexit
 set -o nounset
 
+CYPRESS_RECORD_KEY="${CYPRESS_RECORD_KEY:-}"
 DOCKER_COMPOSE_PROJECT="${1:-akvolumen}"
+LUMEN_PASSWORD="${4:-password}"
+LUMEN_SENTRY_ENVIRONMENT="Travis E2E tests"
+LUMEN_SENTRY_SERVER_NAME="green"
 LUMEN_URL="${2:-http://t1.lumen.local:3030/}"
 LUMEN_USER="${3:-jerome}"
-LUMEN_PASSWORD="${4:-password}"
-CYPRESS_RECORD_KEY="${CYPRESS_RECORD_KEY:-}"
 
 if [[ "${DOCKER_COMPOSE_PROJECT}" == "akvolumen" ]]; then
     docker-compose \
-	run --no-deps \
+        run --no-deps \
         -e CYPRESS_LUMEN_URL="${LUMEN_URL}" \
-	-e CYPRESS_LUMEN_USER="${LUMEN_USER}" \
-	-e CYPRESS_LUMEN_PASSWORD="${LUMEN_PASSWORD}" \
-	-e CYPRESS_RECORD_KEY="${CYPRESS_RECORD_KEY}" \
-	fe-e2e-tests run.sh
+        -e CYPRESS_LUMEN_USER="${LUMEN_USER}" \
+        -e CYPRESS_LUMEN_PASSWORD="${LUMEN_PASSWORD}" \
+        -e CYPRESS_RECORD_KEY="${CYPRESS_RECORD_KEY}" \
+        fe-e2e-tests run.sh
 else
     docker-compose \
-	-p akvo-lumen-ci \
-	-f docker-compose.yml \
-	-f docker-compose.ci.yml \
-	run --no-deps \
-	-e CYPRESS_LUMEN_URL="${LUMEN_URL}" \
-	-e CYPRESS_LUMEN_USER="${LUMEN_USER}" \
-	-e CYPRESS_LUMEN_PASSWORD="${LUMEN_PASSWORD}" \
-	-e CYPRESS_RECORD_KEY="${CYPRESS_RECORD_KEY}" \
-	fe-e2e-tests run.sh
+        -p akvo-lumen-ci \
+        -f docker-compose.yml \
+        -f docker-compose.ci.yml \
+        run --no-deps \
+        -e CYPRESS_LUMEN_URL="${LUMEN_URL}" \
+        -e CYPRESS_LUMEN_USER="${LUMEN_USER}" \
+        -e CYPRESS_LUMEN_PASSWORD="${LUMEN_PASSWORD}" \
+        -e CYPRESS_RECORD_KEY="${CYPRESS_RECORD_KEY}" \
+        -e LUMEN_SENTRY_BACKEND_DSN="${LUMEN_SENTRY_BACKEND_DSN}" \
+        -e LUMEN_SENTRY_SERVER_NAME="${LUMEN_SENTRY_SERVER_NAME}" \
+        -e LUMEN_SENTRY_RELEASE="${TRAVIS_COMMIT}" \
+        -e LUMEN_SENTRY_ENVIRONMENT="${LUMEN_SENTRY_ENVIRONMENT}" \
+        fe-e2e-tests run.sh
 fi
