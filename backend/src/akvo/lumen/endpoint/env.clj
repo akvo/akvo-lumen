a6cf0fa39d91018fd298ffbc4a4dd3b270eb1b29
Juan A. Ruz <juanantonioruz@gmail.com>
2019-06-20 18:31:29 +0200

    [#2211] Return dynamic authURL from env endpoint
    keycloak and auth0 availables

--- backend/src/akvo/lumen/endpoint/env.clj
+++ backend/src/akvo/lumen/endpoint/env.clj
@@ -1,21 +1,27 @@
 (ns akvo.lumen.endpoint.env
   (:require [integrant.core :as ig]
             [clojure.spec.alpha :as s]
+            [akvo.lumen.component.keycloak]
+            [akvo.lumen.component.auth0]
             [clojure.tools.logging :as log]
             [ring.util.response :as response]))
 
 (s/def ::auth-type #{"keycloak" "auth0"})
 
-(defn handler [{:keys [keycloak-public-client-id keycloak flow-api
+(defn handler [{:keys [keycloak-public-client-id keycloak flow-api auth0
                         piwik-site-id sentry-client-dsn] :as opts}]
   (fn [{tenant :tenant
         query-params :query-params
         :as request}]
-    (let [auth-type (get query-params "auth")]
+    (let [auth-type (get query-params "auth")
+          auth-url (condp = auth-type
+                     "keycloak" (:url keycloak)
+                     "auth0" (:url auth0)
+                     (:url keycloak))]
       (if (s/valid? ::auth-type auth-type)
         (response/response
          (cond-> {"keycloakClient" keycloak-public-client-id
-                  "authURL" (:url keycloak)
+                  "authURL" auth-url
                   "flowApiUrl" (:url flow-api)
                   "piwikSiteId" piwik-site-id
                   "tenant" (:tenant request)}
@@ -35,6 +41,7 @@
 
 (s/def ::keycloak-public-client-id string?)
 (s/def ::keycloak :akvo.lumen.component.keycloak/data)
+(s/def ::auth0 :akvo.lumen.component.auth0/data)
 (s/def ::flow-api :akvo.lumen.component.flow/config)
 (s/def ::sentry-client-dsn string?)
 (s/def ::piwik-site-id string?)
@@ -42,6 +49,7 @@
 (defmethod ig/pre-init-spec :akvo.lumen.endpoint.env/env [_]
   (s/keys :req-un [::keycloak-public-client-id
                    ::keycloak
+                   ::auth0
                    ::flow-api
                    ::sentry-client-dsn
                    ::piwik-site-id]))
