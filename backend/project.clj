454fd392166aab349ea2cfdb2e3efed269016b63
Daniel Sunnerek <daniel@kardans.com>
2019-07-05 11:12:46 +0200

    [#2231] Update version for CI

--- backend/project.clj
+++ backend/project.clj
@@ -1,4 +1,4 @@
-(defproject org.akvo/lumen "0.25.0-SNAPSHOT"
+(defproject org.akvo/lumen "0.26.0-SNAPSHOT"
   :description "Akvo Lumen backend"
   :url "https://github.com/akvo/akvo-lumen"
   :license {:name "GNU Affero General Public License 3.0"
