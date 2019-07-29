b046ca83fdf744e8e26d9081a057c98da20ac808
Juan A. Ruz <juanantonioruz@gmail.com>
2019-07-29 14:06:42 +0200

    Refactor

--- backend/src/akvo/lumen/lib/transformation/merge_datasets.clj
+++ backend/src/akvo/lumen/lib/transformation/merge_datasets.clj
@@ -192,8 +192,8 @@
       {:diff diff
        :dataset-id (:datasetId merge-source-op)})))
 
-(defn consistency-error? [tenant-conn latest-dataset-version]
-  (let [merged-sources (->> latest-dataset-version
+(defn consistency-error? [tenant-conn dataset-id]
+  (let [merged-sources (->> (latest-dataset-version-by-dataset-id tenant-conn {:dataset-id dataset-id})
                             :transformations
                             walk/keywordize-keys
                             (filter #(= "core/merge-datasets" (:op %)))
