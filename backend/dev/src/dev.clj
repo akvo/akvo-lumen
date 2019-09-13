2f74f8f4e49c2c10b67f4e385e8077cadb5df868
Juan A. Ruz <juanantonioruz@gmail.com>
2019-09-13 09:59:25 +0200

    [#2296] Improve devs nss

--- backend/dev/src/dev.clj
+++ backend/dev/src/dev.clj
@@ -23,10 +23,6 @@
             [duct.generate :as gen]
             [integrant.core :as ig]
             [integrant.repl :as ir]
-            [akvo.lumen.admin.add-tenant :as add-tenant]
-            [akvo.lumen.admin.db :as admin.db]
-            [akvo.lumen.admin.system :as admin.system]
-            [akvo.lumen.admin.remove-tenant :as remove-tenant]
             [integrant.repl.state :as state :refer (system)])
   (:import [org.postgresql.util PSQLException PGobject]))
 
@@ -87,45 +83,5 @@
 (defn db-conn
   ([label] (p/connection (:akvo.lumen.component.tenant-manager/tenant-manager system) label))
   ([] (db-conn "t1")))
-
-(defn dev-ig-derives []
-  (derive :akvo.lumen.component.emailer/dev-emailer :akvo.lumen.component.emailer/emailer))
-
-(comment
-  (do
-    (let [prod? false
-          [ks edn-file] (if prod?
-                          [(do (admin.system/ig-derives)
-                               (admin.system/ig-select-keys
-                                [:akvo.lumen.admin/remove-tenant
-                                 :akvo.lumen.admin/add-tenant])) "prod.edn"]
-                          [(do (dev-ig-derives)
-                               [:akvo.lumen.component.emailer/dev-emailer
-                                :akvo.lumen.admin.db/config
-                                :akvo.lumen.admin/remove-tenant
-                                :akvo.lumen.admin/add-tenant
-                                ]) "local-admin.edn"])
-          c* (commons/config ["akvo/lumen/config.edn" "akvo/lumen/admin.edn" "test.edn" edn-file] prod?)
-          s (admin.system/new-system c* ks)
-          admin-add-tenant (:akvo.lumen.admin/add-tenant s)
-          admin-remove-tenant (:akvo.lumen.admin/remove-tenant s)]
-      (binding [admin.db/env-vars (:root (:dbs admin-add-tenant))]
-        (let [encryption-key (-> admin-add-tenant :db-settings :encryption-key)
-              drop-if-exists? (-> admin-add-tenant :drop-if-exists?)
-              label "milo4"
-              email "juan@akvo.org"
-              url (format "https://%s.akvolumen.org" label)
-              title "milo4"]
-          (add-tenant/exec admin-add-tenant {:url url :title title :email email :auth-type "keycloak"})
-          #_(remove-tenant/exec admin-remove-tenant label)))
-      
-      )
-    
-
-    )
-
-
-)
-
 
 
