42c9810c7bd7e38b9a09e7978bf47a3ea5e5a6bc
Daniel Sunnerek <daniel@akvo.org>
2019-09-18 08:32:29 +0200

    [#2322] Squeeze the createDataset modal height

--- client/src/containers/Modal.jsx
+++ client/src/containers/Modal.jsx
@@ -20,7 +20,7 @@ const getWidth = (modal) => {
 
 const getHeight = (modal) => {
   switch (modal) {
-    case 'create-dataset': return 600;
+    case 'create-dataset': return 500;
     default: return 250;
   }
 };
