ab3fcaa1f81de78f1e34d4b5fda2c13ee01c2452
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-09-18 12:40:27 +0200

    [#2333] Do not run build if it is a promotion

--- ci/build.sh
+++ ci/build.sh
@@ -13,6 +13,11 @@ if [ -z "$TRAVIS_COMMIT" ]; then
     export TRAVIS_COMMIT=local
 fi
 
+if [[ "${TRAVIS_TAG:-}" =~ promote-.* ]]; then
+    log "Skipping build as it is a prod promotion"
+    exit 0
+fi
+
 log Running Backend unit tests and building uberjar
 backend_image_version=$(awk -F':' '/backend-dev/ {print $3}' docker-compose.override.yml)
 docker run -v "$HOME/.m2:/home/akvo/.m2" -v "$(pwd)/backend:/app" "akvo/akvo-lumen-backend-dev:${backend_image_version}" run-as-user.sh lein do test, eastwood, uberjar
