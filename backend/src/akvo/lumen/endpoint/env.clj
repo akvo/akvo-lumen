3744ca4fd972f436c7b1358544c46895d8d80061
Daniel Sunnerek <daniel@akvo.org>
2019-09-19 14:18:36 +0200

    [#1834] Formatting and sorting

--- backend/src/akvo/lumen/endpoint/env.clj
+++ backend/src/akvo/lumen/endpoint/env.clj
@@ -10,10 +10,10 @@
 
 (def auth-data (juxt :url :client-id))
 
-(defn handler [{:keys [keycloak-public-client flow-api auth0-public-client
-                       piwik-site-id sentry-client-dsn lumen-deployment-color
-                       lumen-deployment-environment lumen-deployment-version]
-                :as opts}]
+(defn handler
+  [{:keys [auth0-public-client flow-api keycloak-public-client
+           lumen-deployment-color lumen-deployment-environment
+           lumen-deployment-version piwik-site-id sentry-client-dsn]}]
   (fn [{tenant :tenant
         query-params :query-params
         :as request}]
@@ -24,16 +24,16 @@
       (if (s/valid? ::auth-type auth-type)
         (response/response
          (cond-> {"authClientId" auth-client-id
-                  "authURL" auth-url
                   "authProvider" auth-type
+                  "authURL" auth-url
                   "flowApiUrl" (condp = auth-type
                                  "keycloak" (:url flow-api)
-                                 "auth0"    (:auth0-url flow-api))
-                  "piwikSiteId" piwik-site-id
-                  "tenant" (:tenant request)
+                                 "auth0" (:auth0-url flow-api))
                   "lumenDeploymentColor" lumen-deployment-color
                   "lumenDeploymentEnvironment" lumen-deployment-environment
-                  "lumenDeploymentVersion" lumen-deployment-version}
+                  "lumenDeploymentVersion" lumen-deployment-version
+                  "piwikSiteId" piwik-site-id
+                  "tenant" (:tenant request)}
            (string? sentry-client-dsn)
            (assoc "sentryDSN" sentry-client-dsn)))
         (-> (response/response (str "Auth-provided not implemented: " auth-type))
@@ -48,21 +48,21 @@
 (defmethod ig/init-key :akvo.lumen.endpoint.env/env  [_ opts]
   (routes opts))
 
-(s/def ::keycloak-public-client :akvo.lumen.component.keycloak/public-client)
 (s/def ::auth0-public-client :akvo.lumen.component.auth0/public-client)
 (s/def ::flow-api :akvo.lumen.component.flow/config)
-(s/def ::sentry-client-dsn string?)
-(s/def ::piwik-site-id string?)
+(s/def ::keycloak-public-client :akvo.lumen.component.keycloak/public-client)
 (s/def ::lumen-deployment-color string?)
 (s/def ::lumen-deployment-environment string?)
 (s/def ::lumen-deployment-version string?)
+(s/def ::piwik-site-id string?)
+(s/def ::sentry-client-dsn string?)
 
 (defmethod ig/pre-init-spec :akvo.lumen.endpoint.env/env [_]
-  (s/keys :req-un [::keycloak-public-client
-                   ::auth0-public-client
+  (s/keys :req-un [::auth0-public-client
                    ::flow-api
-                   ::sentry-client-dsn
-                   ::piwik-site-id
+                   ::keycloak-public-client
                    ::lumen-deployment-color
                    ::lumen-deployment-environment
-                   ::lumen-deployment-version]))
+                   ::lumen-deployment-version
+                   ::piwik-site-id
+                   ::sentry-client-dsn]))
