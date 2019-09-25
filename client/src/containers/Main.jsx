8a4a29fe3c3df69d91d3e90cd754b46815afd21f
Daniel Sunnerek <daniel@akvo.org>
2019-09-25 09:16:42 +0200

    [#2349] Only show user dialog on auth0 & empty names

--- client/src/containers/Main.jsx
+++ client/src/containers/Main.jsx
@@ -6,6 +6,7 @@ import Modal from './Modal';
 import { showModal } from '../actions/activeModal';
 import Notification from './Notification';
 import IMAGES from '../constants/images';
+import { isAuth0 } from '../utilities/utils';
 
 require('../styles/reset.global.scss');
 require('../styles/style.global.scss');
@@ -15,11 +16,12 @@ require('fixed-data-table-2/dist/fixed-data-table.css');
 class Main extends Component {
   componentDidMount() {
     const {
+      env,
       dispatch,
       profile: { firstName, lastName },
       location: { pathname },
     } = this.props;
-    if ((pathname.split('/').pop() !== 'export')
+    if (isAuth0(env) && (pathname.split('/').pop() !== 'export')
       && (!every([firstName, lastName], Boolean))) {
       dispatch(showModal('edit-user'));
     }
