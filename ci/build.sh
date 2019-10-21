d6a25b33c199cee407eedce72451b911220398db
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-10-21 15:27:54 +0200

    Add log to CI to know when are we starting the end to end test against local Docker Compose

--- ci/build.sh
+++ ci/build.sh
@@ -52,7 +52,7 @@ bash ci/helpers/wait-for-docker-compose-to-start.sh
 log Running Backend functional tests
 docker-compose -p akvo-lumen-ci -f docker-compose.yml -f docker-compose.ci.yml run --no-deps backend-functional-tests /app/import-and-run.sh functional-and-seed
 
-# log Running the end to end tests against local Docker Compose Environment
+log Running the end to end tests against local Docker Compose Environment
 ./ci/e2e-test.sh akvolumenci http://t1.lumen.local/?auth=keycloak
 
 log Done
