c7fc62f71bf27ce22b8543cc0e28a08080aafa05
Juan A. Ruz <juanantonioruz@gmail.com>
2019-10-25 09:58:59 +0200

    [#2396] Remove public-client/issuer-suffix-url config var
    This value could be added directly to the public-client url

--- backend/src/akvo/lumen/component/authentication.clj
+++ backend/src/akvo/lumen/component/authentication.clj
@@ -16,32 +16,29 @@
 
 (defmethod ig/init-key :akvo.lumen.component.authentication/public-client  [_ {:keys [url issuer-suffix-url rsa-suffix-url client-id] :as opts}]
   (try
-    (let [issuer         (format "%s%s" url issuer-suffix-url)
-          rsa-key        (-> (format  "%s%s" issuer rsa-suffix-url)
+    (let [rsa-key        (-> (format  "%s%s" url rsa-suffix-url)
                              (http.client/get* http-client-req-defaults)
                              :body
                              (jwt/rsa-key 0))
-          open-id-config (-> (format  "%s%s" issuer "/.well-known/openid-configuration")
+          open-id-config (-> (format  "%s%s" url "/.well-known/openid-configuration")
                              (http.client/get* http-client-req-defaults)
                              :body
                              (json/parse-string true))]
       (assoc opts
              :open-id-config open-id-config
-             :issuer issuer
+             :issuer url
              :rsa-key rsa-key))
     (catch Exception e
       (log/error e (str "Could not get cert from " url))
       (throw e))))
 
 (s/def ::url string?)
-(s/def ::issuer-suffix-url string?)
 (s/def ::rsa-suffix-url string?)
 (s/def ::client-id string?)
 (s/def ::end-session-endpoint-suffix string?)
 
 
-(s/def ::public-client (s/keys :req-un [::url ::client-id ::issuer-suffix-url ::rsa-suffix-url
-                                        ::end-session-endpoint-suffix]))
+(s/def ::public-client (s/keys :req-un [::url ::client-id ::rsa-suffix-url ::end-session-endpoint-suffix]))
 
 (defmethod ig/pre-init-spec :akvo.lumen.component.authentication/public-client [_]
   ::public-client)
