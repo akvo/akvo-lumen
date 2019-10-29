deded21e44b3e10210c287973a64555f4a026810
Juan A. Ruz <juanantonioruz@gmail.com>
2019-10-29 09:21:20 +0100

    [#2378] Call authentication using POST with client-id and client-pw

--- ci/e2e-test.sh
+++ ci/e2e-test.sh
@@ -4,15 +4,18 @@ set -o errexit
 set -o nounset
 
 DOCKER_COMPOSE_PROJECT="${1:-akvolumen}"
-LUMEN_URL="${2:-http://t1.lumen.local:3030/}"
-LUMEN_USER="${3:-jerome}"
-LUMEN_PASSWORD="${4:-password}"
+LUMEN_AUTH="${2:-keycloak}"
+LUMEN_URL="${3:-http://t1.lumen.local:3030/}"
+LUMEN_USER="${4:-jerome@t1.akvolumen.org}"
+LUMEN_PASSWORD="${5:-password}"
+LUMEN_AUTH_CLIENT_ID="${6:-}"
+LUMEN_AUTH_CLIENT_PASSWORD="${7:-}"
 CYPRESS_RECORD_KEY="${CYPRESS_RECORD_KEY:-}"
 
 if [[ "${DOCKER_COMPOSE_PROJECT}" == "akvolumen" ]]; then
     docker-compose \
 	run --no-deps \
-        -e CYPRESS_LUMEN_AUTH="keycloak" \
+        -e CYPRESS_LUMEN_AUTH="${LUMEN_AUTH}" \
         -e CYPRESS_LUMEN_URL="${LUMEN_URL}" \
 	-e CYPRESS_LUMEN_USER="${LUMEN_USER}" \
 	-e CYPRESS_LUMEN_PASSWORD="${LUMEN_PASSWORD}" \
@@ -24,6 +27,9 @@ else
 	-f docker-compose.yml \
 	-f docker-compose.ci.yml \
 	run --no-deps \
+        -e CYPRESS_LUMEN_AUTH="${LUMEN_AUTH}" \
+	-e CYPRESS_LUMEN_AUTH_CLIENT_ID="${LUMEN_AUTH_CLIENT_ID}" \
+	-e CYPRESS_LUMEN_AUTH_CLIENT_PASSWORD="${LUMEN_AUTH_CLIENT_PASSWORD}" \
 	-e CYPRESS_LUMEN_URL="${LUMEN_URL}" \
 	-e CYPRESS_LUMEN_USER="${LUMEN_USER}" \
 	-e CYPRESS_LUMEN_PASSWORD="${LUMEN_PASSWORD}" \
