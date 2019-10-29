00be84ba70e0dc96ad3103d2b40f61748111bb31
Daniel Sunnerek <daniel@akvo.org>
2019-10-29 08:47:34 +0100

    [#2362] Add simple test and hide test logs in csv import

--- backend/test/akvo/lumen/lib/transformation/derive/js_engine_test.clj
+++ backend/test/akvo/lumen/lib/transformation/derive/js_engine_test.clj
@@ -4,4 +4,6 @@
    [clojure.test :refer [deftest is]]))
 
 (deftest javascript-logic
+  (is (= (js-engine/eval* "1 + 1") 2))
+  (is (= (js-engine/eval* "'akvo'.toUpperCase();") "AKVO"))
   (is (not (js-engine/eval* "NaN === NaN;"))))
