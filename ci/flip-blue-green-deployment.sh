e310060be768490a83f7cdceb70b91712358fcf5
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-11-11 09:37:50 +0100

    [#2427] Optional slack notification script
    Generate a script that when run will post to Slack the changes to be
    promoted to dark production, or the changes after a production flip.
    
    Script requires a Slack legacy token (https://api.slack.com/custom-integrations/legacy-tokens)

--- ci/flip-blue-green-deployment.sh
+++ ci/flip-blue-green-deployment.sh
@@ -48,6 +48,8 @@ PROD_LIVE_VERSION=$VERSION
 read_version "$NEW_LIVE_COLOR"
 PROD_DARK_VERSION=$VERSION
 log "Diff between dark and live is https://github.com/akvo/akvo-lumen/compare/$PROD_LIVE_VERSION..$PROD_DARK_VERSION"
+log "Commits to be deployed:"
+git log --oneline $PROD_LIVE_VERSION..$PROD_DARK_VERSION | grep -v "Merge pull request" | grep -v "Merge branch"
 
 if [ "${CLUSTER}" == "production" ]; then
  read -r -e -p "Are you sure you want to flip production? [yn] " CONFIRM
@@ -62,6 +64,12 @@ if ! "${DIR}"/set-live-deployment.sh "${NEW_LIVE_COLOR}"; then
     log "PLEASE CHECK WHAT HAPPEN!!!!!!!!!!!!!"
     switch_back
     exit 5
+fi
+
+if [ "${CLUSTER}" == "production" ]; then
+  "${DIR}"/helpers/generate-slack-notification.sh "${PROD_LIVE_VERSION}" "${PROD_DARK_VERSION}" "Flipping *PROD!!!*" "warning"
+  log "Notifying the team about the changes deployed"
+  ./notify.slack.sh
 fi
 
 switch_back
---
+++ ci/helpers/generate-slack-notification.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+OLDER_GIT_VERSION=$1
+NEWEST_GIT_VERSION=$2
+MSG=$3
+COLOR=$4
+
+cat << EOF > notify.slack.sh
+#!/usr/bin/env bash
+
+if [ -z "\${SLACK_CLI_TOKEN}" ]; then
+  echo "You need a env var SLACK_CLI_TOKEN with a Slack legacy token"
+  echo "Go to https://api.slack.com/custom-integrations/legacy-tokens to create one"
+  echo "Create the env variable and rerun this script (\$0)"
+  exit 1
+fi
+
+slack_txt=\$(git log --oneline $OLDER_GIT_VERSION..$NEWEST_GIT_VERSION | grep -v "Merge pull request" | grep -v "Merge branch" | cut -f 2- -d\  | sed 's/\[#\([0-9]*\)\]/<https:\/\/github.com\/akvo\/akvo-lumen\/issues\/\1|[#\1]>/')
+docker run --rm -e SLACK_CLI_TOKEN 512k/slack-cli \
+    chat send \
+    --channel='#lumen-dev' \
+    --pretext="$MSG. <https://github.com/akvo/akvo-lumen/compare/$OLDER_GIT_VERSION..$NEWEST_GIT_VERSION|Full diff>." \
+    --color $COLOR \
+    --text "\$slack_txt" > /dev/null
+EOF
+
+chmod u+x notify.slack.sh
