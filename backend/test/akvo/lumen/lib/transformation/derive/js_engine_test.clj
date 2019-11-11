4d78caf5da4748aeba1ada4abfc7b7c62911f5bd
Daniel Sunnerek <daniel@akvo.org>
2019-11-11 13:14:12 +0100

    [2362] Make sure we run Java 11

--- backend/test/akvo/lumen/lib/transformation/derive/js_engine_test.clj
+++ backend/test/akvo/lumen/lib/transformation/derive/js_engine_test.clj
@@ -1,7 +1,16 @@
 (ns akvo.lumen.lib.transformation.derive.js-engine-test
   (:require
    [akvo.lumen.lib.transformation.derive.js-engine :as js-engine]
+   [clojure.edn :as edn]
+   [clojure.string :as string]
    [clojure.test :refer [deftest is]]))
+
+(deftest java-version-11
+  (is (>= (-> (System/getProperty "java.version")
+              (string/split #"\.")
+              first
+              edn/read-string)
+          11)))
 
 (deftest javascript-logic
   (is (= (js-engine/eval* "1 + 1") 2))
