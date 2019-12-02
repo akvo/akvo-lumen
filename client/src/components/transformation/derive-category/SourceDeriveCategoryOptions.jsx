f856a978a5b3594cc68a623bb79b95b8f23f6b7e
Juan A. Ruz <juanantonioruz@gmail.com>
2019-12-02 16:14:13 +0100

    [#2451] Adapt DeriveCategory

--- client/src/components/transformation/derive-category/SourceDeriveCategoryOptions.jsx
+++ client/src/components/transformation/derive-category/SourceDeriveCategoryOptions.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'react-grid-system';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+
+import { filterColumns, columnSelectOptions, columnSelectSelectedOption } from '../../../utilities/column';
+import { ensureImmutable } from '../../../utilities/utils';
 
 import SelectMenu from '../../common/SelectMenu';
 
 import './SourceDeriveCategoryOptions.scss';
 
-function conditionColumn({ type }) {
-  return type === 'text' || type === 'number';
-}
-
-const SourceDeriveCategoryOptions = ({ dataset, onChange, selected }) => (
-  <Container className="SourceDeriveCategoryOptions">
-    <Row>
-      <Col md={6} offset={{ md: 3 }}>
-        <h2>
-          <FormattedMessage id="select_source_column" />
-        </h2>
-        {dataset.columns && (
-          <SelectMenu
-            name="sourceColumnName"
-            value={selected}
-            searchable
-            options={
-              dataset.columns.filter(conditionColumn)
-                .map(({ columnName, title, type }) => ({
-                  value: columnName,
-                  label: `${title} (${type})`,
-                }))
-            }
-            onChange={onChange}
-          />
-        )}
-      </Col>
-    </Row>
-  </Container>
-);
+const SourceDeriveCategoryOptions = ({ dataset, onChange, selected, intl }) => {
+  const columnsSelect = ensureImmutable(filterColumns(dataset.columns, ['text', 'number']));
+  return (
+    <Container className="SourceDeriveCategoryOptions">
+      <Row>
+        <Col md={6} offset={{ md: 3 }}>
+          <h2>
+            <FormattedMessage id="select_source_column" />
+          </h2>
+          {dataset.columns && (
+            <SelectMenu
+              name="sourceColumnName"
+              value={columnSelectSelectedOption(selected, columnsSelect)}
+              searchable
+              options={columnSelectOptions(intl, columnsSelect)}
+              onChange={onChange}
+            />
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default injectIntl(SourceDeriveCategoryOptions);
 
@@ -44,4 +40,5 @@ SourceDeriveCategoryOptions.propTypes = {
   dataset: PropTypes.object.isRequired,
   onChange: PropTypes.func.isRequired,
   selected: PropTypes.string,
+  intl: intlShape,
 };
