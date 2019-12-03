9e4aadb28f57fdae55a56b609266056c910f70cf
Juan A. Ruz <juanantonioruz@gmail.com>
2019-12-03 18:48:57 +0100

    [#2427] Change slack channel to flumen-dev

--- ci/helpers/generate-slack-notification.sh
+++ ci/helpers/generate-slack-notification.sh
@@ -18,7 +18,7 @@ fi
 slack_txt=\$(git log --oneline $OLDER_GIT_VERSION..$NEWEST_GIT_VERSION | grep -v "Merge pull request" | grep -v "Merge branch" | cut -f 2- -d\  | sed 's/\[#\([0-9]*\)\]/<https:\/\/github.com\/akvo\/akvo-lumen\/issues\/\1|[#\1]>/' | tr \" \')
 docker run --rm -e SLACK_CLI_TOKEN 512k/slack-cli \
     chat send \
-    --channel='#lumen-dev' \
+    --channel='#flumen-dev' \
     --pretext="$MSG. <https://github.com/akvo/akvo-lumen/compare/$OLDER_GIT_VERSION..$NEWEST_GIT_VERSION|Full diff>." \
     --color $COLOR \
     --text "\$slack_txt" > /dev/null
