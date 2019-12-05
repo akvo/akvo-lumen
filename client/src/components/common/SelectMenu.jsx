997250e168e49e84ab25beeb4f90b0838f850443
Daniel Sunnerek <daniel@akvo.org>
2019-12-05 11:09:20 +0100

    [#2437] WIP Select menu styles

--- client/src/components/common/SelectMenu.jsx
+++ client/src/components/common/SelectMenu.jsx
@@ -21,11 +21,43 @@ function SelectMenu(props) {
     options,
   }));
   const value = props.value && typeof props.value === 'string' ? options.filter(o => o.value === props.value)[0] : props.value;
+  
+  /**  WIP **/
+  const open = true;
+  const colorStyles = {
+    // option: styles => ({ ...styles, backgroundColor: 'yellow' }),
+    menu: styles => ({
+      ...styles,
+      border: '1px solid #e5e6ed',
+      borderTop: '0',
+    }),
+    option: (provided, state) => {
+      let bgc = null;
+      if (state.isSelected) {
+        bgc = '#ccc';
+      } else if (state.isFocused) {
+        bgc = '#e5e6ed';
+      }
+      return {
+        ...provided,
+        // backgroundColor: if (state.isSelected ) {'#888'} else {} state.isFocused ? '#e5e6ed' : null,
+        backgroundColor: bgc,
+      };
+    },
+    group: styles => ({
+      ...styles,
+      backgroundColor: '#fff',
+      fontSize: '1em',
+      padding: '1opx',
+      fontWeight: 'normal',
+    }),
+  };
   return (
     <div className={`SelectMenu ${props.disabled ? 'disabled' : 'enabled'}`}>
       <Select
         {...props}
         value={value}
+        menuIsOpen={open} // WIP
         options={options}
         onChange={(option) => {
           if (option) {
@@ -38,6 +70,8 @@ function SelectMenu(props) {
         searchable={props.searchable || false}
         placeholder={placeholder}
         inputProps={props.inputProps}
+        className="TheSelect"
+        styles={colorStyles} // Styles added
       />
     </div>
   );
