4b573f0273e07e4fffe4bd629f000ff4d42b1030
Daniel Sunnerek <daniel@akvo.org>
2019-12-12 16:38:40 +0100

    [2439] Call deploy2.sh not in promotion

--- ci/deploy2.sh
+++ ci/deploy2.sh
@@ -7,3 +7,21 @@ function log {
 }
 
 export PROJECT_NAME=akvo-lumen
+
+if [[ "${TRAVIS_BRANCH}" != "issue/2439-sem-deploy2" ]]; then
+    exit 0
+fi
+
+log Making sure gcloud and kubectl are installed and up to date
+gcloud components install kubectl
+gcloud components update
+gcloud version
+which gcloud kubectl
+
+log Authentication with gcloud and kubectl
+gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.json
+
+gcloud config set project akvo-lumen
+gcloud config set container/cluster europe-west1-d
+gcloud config set compute/zone europe-west1-d
+gcloud config set container/use_client_certificate True
