769e0c39a41a3de355a9146852f35684e482b07f
Juan A. Ruz <juanantonioruz@gmail.com>
2019-12-16 17:01:35 +0100

    [#2472] Adapt/fix geo-point tx

--- backend/src/akvo/lumen/lib/transformation/geo.clj
+++ backend/src/akvo/lumen/lib/transformation/geo.clj
@@ -25,35 +25,37 @@
 
 (defmethod engine/apply-operation "core/generate-geopoints"
   [{:keys [tenant-conn]} table-name columns op-spec]
-  (let [{:strs [columnNameLat columnNameLong columnTitleGeo]} (engine/args op-spec)
-        get-client-type (partial engine/column-type columns)
-        column-types (map get-client-type [columnNameLat columnNameLong])]
-    (if (every? #(= "number" %) column-types)
-      (try
-        (let [column-name-geo (engine/next-column-name columns)
-              opts {:table-name table-name :column-name-geo column-name-geo}]
-          (jdbc/with-db-transaction [conn tenant-conn]
-            (db.tx.geo/add-geometry-column conn opts)
-            (add-index conn table-name column-name-geo)
-            (db.tx.geo/generate-geopoints conn (conj opts {:column-name-lat columnNameLat
-                                                 :column-name-long columnNameLong})))
-          (jdbc/execute! tenant-conn "DEALLOCATE ALL")
-          {:success? true
-           :execution-log [(format "Generated geopoints for %s" table-name)]
-           :columns (conj columns {"title" columnTitleGeo
-                                   "type" "geopoint"
-                                   "sort" nil
-                                   "hidden" false
-                                   "direction" nil
-                                   "columnName" column-name-geo})})
-        (catch Exception e
-          (log/debug e)
-          {:success? false
-           :message (.getMessage e)}))
-      (let [msg "Selected columns are not all numeric"]
-        (log/debug msg)
-        {:success? false
-         :message msg}))))
+  (if-let [response-error (engine/column-title-error? (get (engine/args op-spec) "columnTitleGeo") columns)]
+    response-error
+   (let [{:strs [columnNameLat columnNameLong columnTitleGeo]} (engine/args op-spec)
+         get-client-type (partial engine/column-type columns)
+         column-types (map get-client-type [columnNameLat columnNameLong])]
+     (if (every? #(= "number" %) column-types)
+       (try
+         (let [column-name-geo (engine/next-column-name columns)
+               opts {:table-name table-name :column-name-geo column-name-geo}]
+           (jdbc/with-db-transaction [conn tenant-conn]
+             (db.tx.geo/add-geometry-column conn opts)
+             (add-index conn table-name column-name-geo)
+             (db.tx.geo/generate-geopoints conn (conj opts {:column-name-lat columnNameLat
+                                                            :column-name-long columnNameLong})))
+           (jdbc/execute! tenant-conn "DEALLOCATE ALL")
+           {:success? true
+            :execution-log [(format "Generated geopoints for %s" table-name)]
+            :columns (conj columns {"title" columnTitleGeo
+                                    "type" "geopoint"
+                                    "sort" nil
+                                    "hidden" false
+                                    "direction" nil
+                                    "columnName" column-name-geo})})
+         (catch Exception e
+           (log/debug e)
+           {:success? false
+            :message (.getMessage e)}))
+       (let [msg "Selected columns are not all numeric"]
+         (log/debug msg)
+         {:success? false
+          :message msg})))))
 
 (defmethod engine/columns-used "core/generate-geopoints"
   [applied-transformation columns]
