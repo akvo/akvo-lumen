300a575530a0dbdb11bda5060ec6780b0157acd6
Daniel Sunnerek <daniel@akvo.org>
2019-12-16 13:50:29 +0100

    [#2439] Inverse container/use_client_certificate

--- ci/deploy2.sh
+++ ci/deploy2.sh
@@ -23,7 +23,7 @@ gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.jso
 gcloud config set project akvo-lumen
 gcloud config set container/cluster europe-west1-d
 gcloud config set compute/zone europe-west1-d
-gcloud config set container/use_client_certificate True
+gcloud config set container/use_client_certificate False
 
 if [[ "${CI_TAG:-}" =~ promote-.* ]]; then
     log Environment is production
