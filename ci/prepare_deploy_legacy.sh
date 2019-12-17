8dd88625b76c761fe81d49e13e979896ade466a7
Daniel Sunnerek <daniel@akvo.org>
2019-12-17 09:24:29 +0100

    [#2439] Refactor deployment files

--- ci/prepare_deploy_legacy.sh
+++ ci/prepare_deploy_legacy.sh
@@ -13,8 +13,10 @@ function log {
 export PROJECT_NAME=akvo-lumen
 
 log Authentication with gcloud and kubectl
-gcloud auth activate-service-account --key-file=/home/semaphore/.secrets/gcp.json
+openssl aes-256-cbc -K $encrypted_13abf95e958f_key -iv $encrypted_13abf95e958f_iv \
+	-in ci/gcloud-service-account.json.enc -out ci/gcloud-service-account.json -d
+gcloud auth activate-service-account --key-file ci/gcloud-service-account.json
 gcloud config set project akvo-lumen
 gcloud config set container/cluster europe-west1-d
 gcloud config set compute/zone europe-west1-d
-gcloud config set container/use_client_certificate False
+gcloud config set container/use_client_certificate True
