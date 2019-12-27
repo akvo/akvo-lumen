e8f27b0d4eefa37386a38b1f5489fb6aa1586c1d
Juan A. Ruz <juanantonioruz@gmail.com>
2019-12-27 17:06:55 +0100

    [#2059] WIp

--- backend/test/akvo/lumen/test_utils.clj
+++ backend/test/akvo/lumen/test_utils.clj
@@ -114,8 +114,6 @@
 
 ;; system utils
 
-(derive :akvo.lumen.component.error-tracker/void :akvo.lumen.component.error-tracker/client)
-
 (derive :akvo.lumen.utils.dev-emailer/emailer :akvo.lumen.component.emailer/emailer)
 (derive :akvo.lumen.component.caddisfly/local :akvo.lumen.component.caddisfly/caddisfly)
 (derive :akvo.lumen.utils.local-error-tracker/local :akvo.lumen.component.error-tracker/error-tracker)
@@ -124,7 +122,7 @@
 (defn dissoc-prod-components [c more-ks]
   (let [ks [:akvo.lumen.component.emailer/mailjet-v3-emailer
             :akvo.lumen.component.caddisfly/prod
-            :akvo.lumen.component.error-tracker/sentry
+            :akvo.lumen.component.error-tracker/prod
             ]
         ks (if more-ks (apply conj ks more-ks) ks)]
     (apply dissoc c ks)))
