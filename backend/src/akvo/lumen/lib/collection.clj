dfdb45e02377d8526f69c524f3c195fe0f231eea
Juan A. Ruz <juanantonioruz@gmail.com>
2019-12-31 12:27:39 +0100

    [#2080] Adapt backend payload to include collection entity types
    now each collection has :datasets, :rasters, :visualisations and
    :dashboards
    
    :entities should be removed on following commmits
    
    {:datasets ["5e0b24e0-7ee7-4793-81ea-b83f0b364818"],
     :rasters [],
     :visualisations ["5e0b24e5-05d6-4112-9b26-162e06fa8483"],
     :dashboards ["5e0b24f0-74db-4881-86aa-d5ad35100f3f"],
     :entities
     ["5e0b24e0-7ee7-4793-81ea-b83f0b364818"
      "5e0b24e5-05d6-4112-9b26-162e06fa8483"
      "5e0b24f0-74db-4881-86aa-d5ad35100f3f"],
     :created 1577788666782,
     :modified 1577788677391,
     :title "yup",
     :id "9cd4afe0-cc00-40e5-b374-d6c094262799"}

--- backend/src/akvo/lumen/lib/collection.clj
+++ backend/src/akvo/lumen/lib/collection.clj
@@ -1,22 +1,47 @@
 (ns akvo.lumen.lib.collection
-  (:require [akvo.lumen.lib :as lib]
-            [akvo.lumen.db.collection :as db.collection]
+  (:require [akvo.lumen.db.collection :as db.collection]
+            [akvo.lumen.lib :as lib]
+            [cheshire.generate :refer [add-encoder]]
             [clojure.java.jdbc :as jdbc]
-            [clojure.set :as set])
+            [clojure.set :as set]
+            [clojure.string :as str])
   (:import [java.sql SQLException Connection]))
+
+(defrecord Entity [uuid type])
+
+(defn new-entity [[uuid type]]
+  (Entity. uuid (keyword type)))
+
+(add-encoder akvo.lumen.lib.collection.Entity
+             (fn [entity jsonGenerator]
+               (.writeString jsonGenerator (:uuid entity))))
+
+(defn feed-entities [collection]
+  (when collection
+    (let [model (->> (.getArray (:entities collection))
+                     (mapv (fn [e] (new-entity (str/split e #"::")))))
+          collection (assoc collection :entities model)
+          data (reduce (fn [m e]
+                         (condp = (:type e)
+                           :raster-dataset-id (update m :rasters #(conj % e))
+                           :dataset-id (update m :datasets #(conj % e))
+                           :visualisation-id (update m :visualisations #(conj % e))
+                           :dashboard-id (update m :dashboards #(conj % e))))
+                       {:dashboards []
+                        :datasets []
+                        :rasters []
+                        :visualisations []} model)]
+      (merge collection data))))
+
 
 (defn all
   ([tenant-conn]
    (all tenant-conn nil))
   ([tenant-conn ids]
-   (mapv (fn [collection]
-           (update collection :entities #(vec (.getArray %))))
-         (db.collection/all-collections tenant-conn (if ids {:ids ids} {})))))
+   (mapv feed-entities (db.collection/all-collections tenant-conn (if ids {:ids ids} {})))))
 
 (defn fetch [tenant-conn id]
-  (if-let [collection (db.collection/fetch-collection tenant-conn {:id id})]
-    (lib/ok (update collection :entities #(vec (.getArray %))))
-    (lib/not-found {:id id})))
+  (feed-entities (db.collection/fetch-collection tenant-conn {:id id})))
 
 (defn- text-array
   "Creates the sql type text[] from a collection of strings"
