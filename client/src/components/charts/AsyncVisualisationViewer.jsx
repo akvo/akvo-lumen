c06f4c11d235bc9b2c1c45234288519aba034115
Juan A. Ruz <juanantonioruz@gmail.com>
2020-03-30 14:43:27 +0200

    [#2569] Fix https://github.com/akvo/akvo-lumen/commit/19ba109ef17be6d40c366de0ea796a639ff732a3#diff-560ca6eea4f3dfc6b02eb7bcf841b469

--- client/src/components/charts/AsyncVisualisationViewer.jsx
+++ client/src/components/charts/AsyncVisualisationViewer.jsx
@@ -12,8 +12,6 @@ export default class AsyncVisualisationViewer extends Component {
   constructor(props) {
     super(props);
     const { visualisation } = props;
-    let output;
-
     if (visualisation.visualisationType === 'map') {
       require.ensure(['leaflet'], () => {
         this.state = {
@@ -31,48 +29,49 @@ export default class AsyncVisualisationViewer extends Component {
         },
       };
     } else {
-      require.ensure([
-        '@potion/color',
-        '@potion/element',
-        '@potion/extra',
-        '@potion/layout',
-        '@vx/axis',
-        '@vx/grid',
-        '@vx/legend',
-        '@vx/scale',
-        '@nivo/core',
-        'd3-array',
-        'd3-scale',
-        'd3-scale-chromatic',
-        'd3-shape',
-        'datalib',
-      ], () => {
-        // eslint-disable-next-line global-require
-        const Chart = require('./Chart').default;
-
-        switch (visualisation.visualisationType) {
-          case 'bar':
-          case 'line':
-          case 'area':
-          case 'donut':
-          case 'pie':
-          case 'polararea':
-          case 'scatter':
-          case 'bubble':
-            output = Chart;
-            break;
-
-          default:
-            throw new Error(`Unknown chart type ${visualisation.visualisationType}
-              supplied to AsyncVisualisationViewer`);
-        }
-        this.state = {
-          asyncComponents: {
-            output,
-          },
-        };
-      });
+      this.state = {};
     }
+  }
+  componentDidMount() {
+    const { visualisation } = this.props;
+    require.ensure([
+      '@potion/color',
+      '@potion/element',
+      '@potion/extra',
+      '@potion/layout',
+      '@vx/axis',
+      '@vx/grid',
+      '@vx/legend',
+      '@vx/scale',
+      '@nivo/core',
+      'd3-array',
+      'd3-scale',
+      'd3-scale-chromatic',
+      'd3-shape',
+      'datalib',
+    ], () => {
+      switch (visualisation.visualisationType) {
+        case 'bar':
+        case 'line':
+        case 'area':
+        case 'donut':
+        case 'pie':
+        case 'polararea':
+        case 'scatter':
+        case 'bubble':
+          this.setState({
+            asyncComponents: {
+              // eslint-disable-next-line global-require
+              output: require('./Chart').default,
+            },
+          });
+          break;
+
+        default:
+          throw new Error(`Unknown chart type ${visualisation.visualisationType}
+                supplied to AsyncVisualisationViewer`);
+      }
+    });
   }
 
   render() {
