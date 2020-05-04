befccde0252dbb3269c77e30ab864b3d178f304d
Juan A. Ruz <juanantonioruz@gmail.com>
2020-05-04 10:25:07 +0200

    [#2537] Remove sampling/random and limit from scatter aggregation

--- backend/src/akvo/lumen/lib/aggregation/scatter.clj
+++ backend/src/akvo/lumen/lib/aggregation/scatter.clj
@@ -29,35 +29,29 @@
 
         aggregation (partial sql-aggregation-subquery (:metricAggregation query))
 
-        subquery (format "(SELECT * FROM %1$s WHERE %2$s ORDER BY random() LIMIT %3$s)z"
-                         table-name filter-sql max-points)
-
         sql-text-with-aggregation
         (format "SELECT %1$s AS x, %2$s AS y, %3$s AS size, %4$s AS category, %5$s AS label 
                  FROM %6$s
+                 WHERE %7$s
                  GROUP BY %5$s"
                 (aggregation column-x)
                 (aggregation column-y)
                 (aggregation column-size)
                 (aggregation column-category)
                 (:columnName column-bucket)
-                subquery)
-        sql-text-without-aggregation (format "SELECT * FROM
-                                               (SELECT * FROM 
-                                                 (SELECT %1$s AS x, %2$s AS y, %3$s AS size, %4$s AS category, %5$s AS label 
-                                                  FROM %6$s 
-                                                  WHERE %7$s)z
-                                                ORDER BY random() 
-                                                LIMIT %8$s)zz
-                                              ORDER BY zz.x"
+                table-name
+                filter-sql)
+        sql-text-without-aggregation (format "SELECT %1$s AS x, %2$s AS y, %3$s AS size, %4$s AS category, %5$s AS label 
+                                              FROM %6$s 
+                                              WHERE %7$s
+                                              ORDER BY x"
                          (:columnName column-x)
                          (:columnName column-y)
                          (:columnName column-size)
                          (:columnName column-category)
                          (:columnName column-label)
                          table-name
-                         filter-sql
-                         max-points)
+                         filter-sql)
 
         sql-text (if column-bucket sql-text-with-aggregation sql-text-without-aggregation)
         sql-response (run-query tenant-conn sql-text)]
