d21d02170030310acc4adb949c4e3c0d60ee18c5
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-12 10:02:33 +0200

    [#2681] Allow for n|N

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -81,7 +81,7 @@ TAG_NAME="promote-$(date +"%Y%m%d-%H%M%S")"
 
 echo ""
 read -r -e -p "Does this deployment contain a hotfix, rollback or fix-forward for a previous deployment? [yn] " FIX
-if [ "${FIX}" != "n" ]; then
+if [ "${FIX}" != "n" ] || [ "${FIX}" != "N" ]; then
    PROMOTION_REASON="FIX_RELEASE"
 else
    PROMOTION_REASON="REGULAR_RELEASE"
