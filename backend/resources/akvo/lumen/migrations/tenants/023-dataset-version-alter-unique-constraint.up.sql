1264c2fe62b0a255bd8a691ef5ef286671aa411e
Juan A. Ruz <juanantonioruz@gmail.com>
2020-06-22 10:35:19 +0200

    [#2127] Use columns md5 in dataset_version unique index
    Relates 534fc057c4f9857868c849305a14604dddfa76a6#commitcomment-40010281

--- backend/resources/akvo/lumen/migrations/tenants/023-dataset-version-alter-unique-constraint.up.sql
+++ backend/resources/akvo/lumen/migrations/tenants/023-dataset-version-alter-unique-constraint.up.sql
@@ -1,5 +1,4 @@
 ALTER TABLE public.dataset_version
       DROP CONSTRAINT IF EXISTS dataset_version_dataset_id_version_key;
 
-ALTER TABLE public.dataset_version
-      ADD UNIQUE (dataset_id, version, table_name);
+CREATE UNIQUE INDEX dataset_version_dataset_id_version_table_name_key ON public.dataset_version (dataset_id, version, md5(columns::text));
