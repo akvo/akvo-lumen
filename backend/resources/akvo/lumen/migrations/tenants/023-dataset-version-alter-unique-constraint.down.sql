3da3e9d7968b40bf53fe37b45a1f67a22f684d7c
Iván Perdomo <ivan@perdomo.me>
2020-06-24 15:13:48 +0200

    [#2127] Unique index on dataset_id, version, columns
    It seems that is not possible to use the shorthand
    
    `UNIQUE(dataset_id, verison, md5(columns::text))`
    
    so I opted wit the long form
    
    `CREATE UNIQUE INDEX ...`

--- backend/resources/akvo/lumen/migrations/tenants/023-dataset-version-alter-unique-constraint.down.sql
+++ backend/resources/akvo/lumen/migrations/tenants/023-dataset-version-alter-unique-constraint.down.sql
@@ -1,5 +1,4 @@
-ALTER TABLE public.dataset_version
-      DROP CONSTRAINT IF EXISTS dataset_version_dataset_id_version_table_name_key;
+DROP INDEX IF EXISTS dataset_version_dataset_id_version_columns_key;
 
 ALTER TABLE public.dataset_version
       ADD UNIQUE (dataset_id, version);
--- backend/resources/akvo/lumen/migrations/tenants/023-dataset-version-alter-unique-constraint.up.sql
+++ backend/resources/akvo/lumen/migrations/tenants/023-dataset-version-alter-unique-constraint.up.sql
@@ -1,5 +1,5 @@
 ALTER TABLE public.dataset_version
       DROP CONSTRAINT IF EXISTS dataset_version_dataset_id_version_key;
 
-ALTER TABLE public.dataset_version
-      ADD UNIQUE (dataset_id, version, table_name);
+CREATE UNIQUE INDEX dataset_version_dataset_id_version_columns_key
+ON dataset_version (dataset_id, version, md5(columns::text));
