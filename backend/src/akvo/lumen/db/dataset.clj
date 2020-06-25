b085a26adfa5a5e3d7e0054a4ff32b6dd577d311
Juan A. Ruz <juanantonioruz@gmail.com>
2020-06-25 13:28:51 +0200

    [#2127]Adapter to query dataset by id to return 1-N dataset_versions

--- backend/src/akvo/lumen/db/dataset.clj
+++ backend/src/akvo/lumen/db/dataset.clj
@@ -1,4 +1,9 @@
 (ns akvo.lumen.db.dataset
-  (:require [hugsql.core :as hugsql]))
+  (:require [hugsql.core :as hugsql]
+            [clojure.tools.logging :as log]))
 
 (hugsql/def-db-fns "akvo/lumen/lib/dataset.sql")
+
+(defn dataset-by-id [conn opts]
+  (first (dataset-by-id* conn opts)))
+
