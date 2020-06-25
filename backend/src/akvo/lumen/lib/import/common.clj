41eb5043c0d2b13b126fa02f23efb73df6777ea9
Juan A. Ruz <juanantonioruz@gmail.com>
2020-06-25 10:04:44 +0200

    [#2127] Adapt PR review

--- backend/src/akvo/lumen/lib/import/common.clj
+++ backend/src/akvo/lumen/lib/import/common.clj
@@ -50,7 +50,9 @@
        questions))
 
 (defn extract-question-response
-  "based on position"
-  [r idx]
-  (reduce (fn [c [k v]]
-            (assoc c k (nth v idx))) {} (seq r)))
+  "`record` is a map with collection values
+  e.g: {:a [\"1\" \"2\"] :b [\"3\" \"4\"]}
+  depending on idx return same map structure but with one value per key
+  e.g: using idx=0: {:a \"1\" :b \"3\"}"
+  [record idx]
+  (reduce (fn [c [k v]] (assoc c k (nth v idx))) {} (seq record)))
