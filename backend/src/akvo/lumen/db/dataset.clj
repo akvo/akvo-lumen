68007d65204639ad0773300505bf72fd945d4115
Juan A. Ruz <juanantonioruz@gmail.com>
2020-07-21 12:51:57 +0200

    [#2127] Adapt fetch dataset by group having different tables

--- backend/src/akvo/lumen/db/dataset.clj
+++ backend/src/akvo/lumen/db/dataset.clj
@@ -1,4 +1,20 @@
 (ns akvo.lumen.db.dataset
-  (:require [hugsql.core :as hugsql]))
+  (:require [hugsql.core :as hugsql]
+            [clojure.tools.logging :as log]))
 
 (hugsql/def-db-fns "akvo/lumen/lib/dataset.sql")
+
+(defn dataset-by-id [conn opts]
+  (first (dataset-by-id* conn opts)))
+
+(defn dataset-in-groups-by-id
+  "dataset arranged in groups"
+  [conn opts]
+  (let [dataset-col (dataset-by-id* conn opts)
+        commons-keys [:updated :created :source :modified :title :author :id]
+        specific-keys [:transformations :columns :table-name]]
+    (assoc (select-keys (first dataset-col) commons-keys)
+           :groups (reduce (fn [c ds]
+                             (let [ds-groups (map #(get % "groupId") (:columns ds))]
+                               (reduce #(assoc % %2 (select-keys ds specific-keys)) c ds-groups)))
+                           {} dataset-col))))
