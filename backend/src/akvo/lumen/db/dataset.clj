f87d5cea14dd72377dec757d3c1fe58846e7d841
Juan A. Ruz <juanantonioruz@gmail.com>
2020-07-23 11:14:30 +0200

    [#2127] Adapt new-dataset-version

--- backend/src/akvo/lumen/db/dataset.clj
+++ backend/src/akvo/lumen/db/dataset.clj
@@ -1,24 +1,23 @@
 (ns akvo.lumen.db.dataset
-  (:require [hugsql.core :as hugsql]))
+  (:require [akvo.lumen.db.dataset-version :as dv]
+            [hugsql.core :as hugsql]))
 
 (hugsql/def-db-fns "akvo/lumen/lib/dataset.sql")
 
-(def defaults {:ns "main"})
-
 (defn all-datasets [conn]
-  (db-all-datasets conn defaults))
+  (db-all-datasets conn dv/defaults))
 
 (defn dataset-by-id [conn opts]
-  (db-dataset-by-id conn (merge defaults opts)))
+  (db-dataset-by-id conn (merge dv/defaults opts)))
 
 (defn table-name-and-columns-by-dataset-id [conn opts]
-  (db-table-name-and-columns-by-dataset-id conn (merge defaults opts)))
+  (db-table-name-and-columns-by-dataset-id conn (merge dv/defaults opts)))
 
 (defn table-name-by-dataset-id [conn opts]
-  (db-table-name-by-dataset-id conn (merge defaults opts)))
+  (db-table-name-by-dataset-id conn (merge dv/defaults opts)))
 
 (defn imported-dataset-columns-by-dataset-id [conn opts]
-  (db-imported-dataset-columns-by-dataset-id conn (merge defaults opts)))
+  (db-imported-dataset-columns-by-dataset-id conn (merge dv/defaults opts)))
 
 (defn data-source-by-dataset-id [conn opts]
-  (db-data-source-by-dataset-id conn (merge defaults opts)))
+  (db-data-source-by-dataset-id conn (merge dv/defaults opts)))
