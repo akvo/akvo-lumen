3d8f10af9adc6f3650b21d3f2ab4b3460d23f8e7
Juan A. Ruz <juanantonioruz@gmail.com>
2020-07-23 11:22:05 +0200

    [#2127] Adapt latest-dataset-version-by-dataset-id

--- backend/src/akvo/lumen/db/transformation.clj
+++ backend/src/akvo/lumen/db/transformation.clj
@@ -1,4 +1,8 @@
 (ns akvo.lumen.db.transformation
-  (:require [hugsql.core :as hugsql]))
+  (:require [akvo.lumen.db.dataset-version :as dv]
+            [hugsql.core :as hugsql]))
 
 (hugsql/def-db-fns "akvo/lumen/lib/transformation.sql")
+
+(defn latest-dataset-version-by-dataset-id [conn opts]
+  (db-latest-dataset-version-by-dataset-id conn (merge dv/defaults opts)))
