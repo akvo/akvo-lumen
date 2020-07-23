6b6a57fa4435db4ac394aaa7a7c0523491ea1c2a
Juan A. Ruz <juanantonioruz@gmail.com>
2020-07-23 12:05:15 +0200

    [#2127] Fix latest-dataset-versions args fun call

--- backend/src/akvo/lumen/db/transformation.clj
+++ backend/src/akvo/lumen/db/transformation.clj
@@ -7,8 +7,8 @@
 (defn latest-dataset-version-by-dataset-id [conn opts]
   (db-latest-dataset-version-by-dataset-id conn (merge dv/defaults opts)))
 
-(defn latest-dataset-versions [conn opts]
-  (db-latest-dataset-versions conn (merge dv/defaults opts)))
+(defn latest-dataset-versions [conn]
+  (db-latest-dataset-versions conn dv/defaults))
 
 (defn latest-dataset-versions-by-dataset-ids [conn opts]
   (db-latest-dataset-versions-by-dataset-ids conn (merge dv/defaults opts)))
