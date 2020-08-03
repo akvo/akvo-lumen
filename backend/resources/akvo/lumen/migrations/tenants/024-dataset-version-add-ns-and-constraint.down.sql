793a7083fa97b8fef3db6cadb2a202bc6e47f0ff
Juan A. Ruz <juanantonioruz@gmail.com>
2020-08-03 10:04:24 +0200

    [#2127] Rename ns to namespace

--- backend/resources/akvo/lumen/migrations/tenants/024-dataset-version-add-ns-and-constraint.down.sql
+++ backend/resources/akvo/lumen/migrations/tenants/024-dataset-version-add-ns-and-constraint.down.sql
@@ -1,8 +1,8 @@
-ALTER TABLE history.dataset_version DROP COLUMN IF EXISTS "ns";
-ALTER TABLE public.dataset_version DROP COLUMN IF EXISTS "ns";
+ALTER TABLE history.dataset_version DROP COLUMN IF EXISTS "namespace";
+ALTER TABLE public.dataset_version DROP COLUMN IF EXISTS "namespace";
 
-ALTER TABLE dataset_version DROP CONSTRAINT IF EXISTS dataset_version_dataset_id_version_ns_key;
-DROP INDEX IF EXISTS dataset_version_dataset_id_version_ns_key;
+ALTER TABLE dataset_version DROP CONSTRAINT IF EXISTS dataset_version_dataset_id_version_namespace_key;
+DROP INDEX IF EXISTS dataset_version_dataset_id_version_namespace_key;
 
 CREATE UNIQUE INDEX dataset_version_dataset_id_version_columns_key
 ON dataset_version (dataset_id, version, md5(columns::text));
