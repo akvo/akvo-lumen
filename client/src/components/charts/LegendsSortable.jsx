98d53c499c0909dd204751c1026815b74f4db09d
Juan A. Ruz <juanantonioruz@gmail.com>
2020-08-11 10:35:44 +0200

    [#625] Refactor

--- client/src/components/charts/LegendsSortable.jsx
+++ client/src/components/charts/LegendsSortable.jsx
@@ -9,10 +9,8 @@ import { sortAlphabetically, sortChronologically } from '../../utilities/utils';
 import { palette } from '../../utilities/visualisationColors';
 import LegendShape from './LegendShape';
 
-
-export const sortLegendsFunctionFactory = visualisation => (get(visualisation, 'data.common.metadata.type') === 'text' ?
+const sortLegendsFunctionFactory = visualisation => (get(visualisation, 'data.common.metadata.type') === 'text' ?
   sortAlphabetically : sortChronologically);
-
 
 const sortableItemStyle = { display: 'flex', alignItems: 'center', flexDirection: 'row', margin: '5px 0px 5px 5px' };
 
@@ -22,6 +20,11 @@ const NoSortableItem = ({ value, color }) =>
   <div style={{ marginLeft: '4px' }}>{value}</div>
 </div>);
 
+NoSortableItem.propTypes = {
+  value: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
+
 const DragHandle = sortableHandle(() => <div style={{ marginRight: '4px' }}><span>::</span></div>);
 
 const SortableItem = sortableElement(({ index, value, color }) =>
@@ -30,7 +33,12 @@ const SortableItem = sortableElement(({ index, value, color }) =>
      <LegendShape fill={color} />
      <div style={{ marginLeft: '4px' }}>{value}</div></div>));
 
-const legendsFun = (specLegend, visualisation) => {
+SortableItem.propTypes = {
+  value: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+const getLegends = (specLegend, visualisation) => {
   const specLegendsList = get(specLegend, 'order.list');
 
   const visLegendsList = get(visualisation, 'data.common.data') || [];
@@ -47,7 +55,6 @@ export const ensureSpecLegend = (specLegend) => {
   return legend;
 };
 
-
 export const LegendsSortable = ({
   onChangeSpec,
   colors,
@@ -55,7 +62,7 @@ export const LegendsSortable = ({
   visualisation,
 }) => {
   const sortable = (get(specLegend, 'order.mode') || 'auto') !== 'auto';
-  const legends = legendsFun(specLegend, visualisation);
+  const legends = getLegends(specLegend, visualisation);
 
   const onSortEnd = ({ oldIndex, newIndex }) => {
     const currentItems = arrayMove(legends, oldIndex, newIndex);
@@ -83,17 +90,7 @@ export const LegendsSortable = ({
   return (
     <SortableList
       onSortEnd={onSortEnd} legendItems={legends}
-    />
-
-  );
-};
-NoSortableItem.propTypes = {
-  value: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
-};
-SortableItem.propTypes = {
-  value: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
+    />);
 };
 
 LegendsSortable.propTypes = {
@@ -105,7 +102,7 @@ LegendsSortable.propTypes = {
 
 export const resetLegend = (specLegend, visualisation, val) => {
   const legend = ensureSpecLegend(specLegend);
-  const legends = legendsFun(legend, visualisation);
+  const legends = getLegends(legend, visualisation);
   if (val) {
     legend.order.mode = val;
     if (val === 'auto') {
