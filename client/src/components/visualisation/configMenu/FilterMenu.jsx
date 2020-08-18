34630abc3dc98c78f34f02d03ebaad28497afa9f
Juan A. Ruz <juanantonioruz@gmail.com>
2020-08-18 19:15:41 +0200

    [#2620] Adapt add option column in visualisation filter [fix]
    Relates https://github.com/akvo/akvo-lumen/issues/2620#issuecomment-675462354

--- client/src/components/visualisation/configMenu/FilterMenu.jsx
+++ client/src/components/visualisation/configMenu/FilterMenu.jsx
@@ -116,6 +116,16 @@ class FilterMenu extends Component {
             value: 'isEmpty',
           },
         ],
+        option: [
+          {
+            label: formatMessage({ id: 'exactly_matches' }),
+            value: 'is',
+          },
+          {
+            label: formatMessage({ id: 'is_empty' }),
+            value: 'isEmpty',
+          },
+        ],
         number: [
           {
             label: formatMessage({ id: 'is_higher_than' }),
