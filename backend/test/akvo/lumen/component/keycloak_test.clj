aa6d6160bc8d2cde61bdc564e4d405eb15735ca6
Daniel Sunnerek <daniel@akvo.org>
2020-08-24 13:02:18 +0200

    Use test runner with fix

--- backend/test/akvo/lumen/component/keycloak_test.clj
+++ backend/test/akvo/lumen/component/keycloak_test.clj
@@ -10,25 +10,25 @@
    [integrant.core :as ig]))
 
 
-#_(def ^:dynamic *keycloak*)
+(def ^:dynamic *keycloak*)
 
-#_(defn fixture [f]
-    (let [system (ig/init (tu/start-config)
-                          [:akvo.lumen.component.keycloak/authorization-service])]
-      (binding [*keycloak* (:akvo.lumen.component.keycloak/authorization-service system)]
-        (f))
-      (ig/halt! system)))
+(defn fixture [f]
+  (let [system (ig/init (tu/start-config)
+                        [:akvo.lumen.component.keycloak/authorization-service])]
+    (binding [*keycloak* (:akvo.lumen.component.keycloak/authorization-service system)]
+      (f))
+    (ig/halt! system)))
 
-#_(use-fixtures :once fixture)
+(use-fixtures :once fixture)
 
-#_(deftest ^:functional keycloak-test
-    (testing "Jerome (admin) permissions to t1"
-      (is (= #{"t1/admin"}
-             (p/allowed-paths *keycloak* {:email "jerome@t1.akvolumen.org" :iat (tc/to-date (t/now))}))))
+(deftest ^:functional keycloak-test
+  (testing "Jerome (admin) permissions to t1"
+    (is (= #{"t1/admin"}
+           (p/allowed-paths *keycloak* {:email "jerome@t1.akvolumen.org" :iat (tc/to-date (t/now))}))))
 
-    (testing "Salim (member) permissions to t1"
-      (is (= #{"t1"}
-             (p/allowed-paths *keycloak* {:email "salim@t1.akvolumen.org" :iat (tc/to-date (t/now))}))))
+  (testing "Salim (member) permissions to t1"
+    (is (= #{"t1"}
+           (p/allowed-paths *keycloak* {:email "salim@t1.akvolumen.org" :iat (tc/to-date (t/now))}))))
 
-    (testing "Non existing user"
-      (is (= nil (p/allowed-paths *keycloak* {:email "nobody@t1.akvolumen.org" :iat (tc/to-date (t/now))} )))))
+  (testing "Non existing user"
+    (is (= nil (p/allowed-paths *keycloak* {:email "nobody@t1.akvolumen.org" :iat (tc/to-date (t/now))} )))))
