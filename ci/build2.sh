d41dd3cd5269f2776c237ec04fec243a21fffca2
Daniel Sunnerek <daniel@akvo.org>
2020-08-24 11:36:29 +0200

    Remove manual try for working dir

--- ci/build2.sh
+++ ci/build2.sh
@@ -18,9 +18,10 @@ if [[ "${CI_TAG:-}" =~ promote-.* ]]; then
     exit 0
 fi
 
+
 log Running Backend unit tests
 backend_image_version=$(awk -F':' '/backend-dev/ {print $3}' docker-compose.override.yml)
-docker run -v "$HOME/.m2:/home/akvo/.m2" -v "$(pwd)/backend:/app" "akvo/akvo-lumen-backend-dev:${backend_image_version}" /app/backend run-as-user.sh /bin/bash -c "cd /app/backend; clj -C:org.akvo.lumen.dev -R:org.akvo.lumen.dev  -A:org.akvo.lumen.test -e :functional"
+docker run -v "$HOME/.m2:/home/akvo/.m2" -v "$(pwd)/backend:/app" "akvo/akvo-lumen-backend-dev:${backend_image_version}" run-as-user.sh clj -C:org.akvo.lumen.dev -R:org.akvo.lumen.dev -A:org.akvo.lumen.test -e :functional
 
 # log Creating Production Backend image
 
