52f392a3572f178b97f538d13a78cf2c6bd674d0
Marvin Kome <marvinkome@gmail.com>
2020-09-10 10:23:29 +0100

    [#2881] - make create dataset modal height auto

--- client/src/containers/Modal.jsx
+++ client/src/containers/Modal.jsx
@@ -20,7 +20,7 @@ const getWidth = (modal) => {
 
 const getHeight = (modal) => {
   switch (modal) {
-    case 'create-dataset': return 450;
+    case 'create-dataset': return 'auto';
     default: return 250;
   }
 };
