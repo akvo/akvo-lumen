832c96f631bfcb9603ccd54f362feb798fd8f265
Juan A. Ruz <juanantonioruz@gmail.com>
2020-09-22 11:25:16 +0200

    W

--- backend/src/akvo/lumen/lib/transformation.sql
+++ backend/src/akvo/lumen/lib/transformation.sql
@@ -32,21 +32,28 @@ SELECT id, table_name AS "table-name", imported_table_name AS "imported-table-na
                     FROM dataset_version v
                    WHERE v.dataset_id = :dataset-id);
 
--- :name db-latest-dataset-versions-by-dataset-ids :? :*
+-- :name db-n-latest-dataset-version-by-dataset-id :? :*
+-- :doc Returns the most recent dataset version for a given dataset id
+SELECT id, table_name AS "table-name", imported_table_name AS "imported-table-name", columns, version, transformations, namespace 
+  FROM dataset_version
+ WHERE dataset_id = :dataset-id
+   AND version = (SELECT MAX(v.version)
+                    FROM dataset_version v
+                   WHERE v.dataset_id = :dataset-id);
+
+-- :name db-latest-dataset-version-with-columns-by-dataset-ids :? :*
 -- :doc Returns the most recent dataset version for a given dataset ids
-select DISTINCT ON (dataset_id) dataset_id, id, version, transformations, columns
+select DISTINCT ON (dataset_id, namespace) dataset_id, id, version, columns, namespace
 FROM dataset_version
 WHERE dataset_id IN (:v*:dataset-ids)
-AND dataset_version.namespace = :namespace
-order by dataset_id, version desc;
+order by dataset_id, namespace, version desc;
 
--- :name db-latest-dataset-versions :? :*
+-- :name db-latest-dataset-version-with-transformations :? :*
 -- :doc Returns the most recent dataset version for a given dataset id
-select DISTINCT ON (dataset_id) dataset_id, dataset_version.id as id, version, title, transformations
+select DISTINCT ON (dataset_id, namespace) dataset_id, dataset_version.id as id, version, title, transformations, namespace
 FROM dataset_version, dataset
 where dataset.id=dataset_id
-AND dataset_version.namespace = :namespace
-order by dataset_id, version desc;
+order by dataset_id, namespace, version desc;
 
 -- :name db-update-dataset-version :! :n
 -- :doc Update dataset version
@@ -55,6 +62,13 @@ where dataset_id= :dataset-id
 AND version= :version
 AND namespace = :namespace;
 
+-- :name db-initial-dataset-version-version-by-dataset-id :? :1
+SELECT version
+  FROM  dataset_version
+  WHERE dataset_id= :dataset-id AND transformations='[]'
+  AND namespace = 'main'
+  ORDER BY version DESC LIMIT 1;
+
 -- :name db-initial-dataset-version-to-update-by-dataset-id :? :1
 SELECT id, table_name AS "table-name", imported_table_name AS "imported-table-name", columns, version, transformations
   FROM  dataset_version
@@ -62,12 +76,16 @@ SELECT id, table_name AS "table-name", imported_table_name AS "imported-table-na
   AND namespace = :namespace
   ORDER BY version DESC LIMIT 1;
 
--- :name db-dataset-version-by-dataset-id :? :1
+-- :name db-n-initial-dataset-version-to-update-by-dataset-id :? :*
+SELECT id, table_name AS "table-name", imported_table_name AS "imported-table-name", columns, version, transformations, namespace
+  FROM  dataset_version
+  WHERE dataset_id= :dataset-id AND version= :version ;
+
+-- :name db-dataset-version-by-dataset-id-and-version :? :*
 -- :doc Returns the most recent dataset version for a given dataset id
-SELECT id, table_name AS "table-name", imported_table_name AS "imported-table-name", columns, version, transformations
+SELECT id, table_name AS "table-name", imported_table_name AS "imported-table-name", columns, version, transformations, namespace
   FROM dataset_version
  WHERE dataset_id = :dataset-id
-   AND namespace = :namespace
    AND version = :version;
 
 -- :name clear-dataset-version-data-table :! :n
