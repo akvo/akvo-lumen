e435b2ea18a5fe645d44566181aa5649e6978e3c
Daniel Sunnerek <daniel@akvo.org>
2020-09-25 09:20:27 +0200

    [#2925] Filter author claims

--- backend/src/akvo/lumen/lib/dataset_version.sql
+++ backend/src/akvo/lumen/lib/dataset_version.sql
@@ -5,11 +5,18 @@ INSERT INTO dataset_version (id, dataset_id, job_execution_id, version,
                              transformations, columns, namespace)
 VALUES (:id, :dataset-id, :job-execution-id, :version,
         :table-name, :imported-table-name, :transformations, :columns, :namespace)
+
 -- :name db-new-dataset-version-2 :! :n
 -- :doc Inserts a new dataset version 2
 INSERT INTO dataset_version_2 (id, dataset_id, job_execution_id, version, transformations, author)
-VALUES (:id, :dataset-id, :job-execution-id, :version, :transformations, :author)
-
+VALUES (
+       :id,
+       :dataset-id,
+       :job-execution-id,
+       :version,
+       :transformations,
+       (SELECT jsonb_object_agg(key, value) FROM jsonb_each(:author) WHERE key IN ('name', 'given_name', 'family_name', 'email'))
+);
 
 -- :name db-new-data-group :! :n
 -- :doc Inserts a new data-group
