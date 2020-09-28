94a7cc8e58c97ebd1283a21a8b1f96b0b3d78324
Daniel Sunnerek <daniel@akvo.org>
2020-09-28 15:11:33 +0200

    [#2127] Mac OS X support for tmate pairing CoAuthored with @iperdomo

--- backend/dev_helpers/pairing.sh
+++ backend/dev_helpers/pairing.sh
@@ -6,13 +6,25 @@ apt-get update
 apt-get install -y emacs-nox tmate silversearcher-ag
 
 mkdir -p /home/akvo/.emacs.d/
+mkdir -p /home/akvo/.ssh/
+mkdir -p /root/.emacs.d/
+mkdir -p /root/.ssh/
 cp /app/dev_helpers/init.el /home/akvo/.emacs.d/init.el
+cp /app/dev_helpers/init.el /root/.emacs.d/init.el
 
 {
     echo "export PATH=/usr/local/openjdk-11/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin/"
-    echo "[[ -d .ssh ]] || mkdir .ssh"
-    echo "[[ -f .ssh/id_rsa ]] || ssh-keygen -t rsa -b 4096 -P '' -f /home/akvo/.ssh/id_rsa"
+    echo "[[ -d ~/.ssh ]] || mkdir ~/.ssh"
+    echo "[[ -f ~/.ssh/id_rsa ]] || ssh-keygen -t rsa -b 4096 -P '' -f ~/.ssh/id_rsa"
+
 } >> /home/akvo/.bashrc
+
+{
+    echo "export PATH=/usr/local/openjdk-11/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin/"
+    echo "[[ -d ~/.ssh ]] || mkdir ~/.ssh"
+    echo "[[ -f ~/.ssh/id_rsa ]] || ssh-keygen -t rsa -b 4096 -P '' -f ~/.ssh/id_rsa"
+
+} >> /root/.bashrc
 
 [[ -f /app/.dir-locals.el ]] && mv /app/.dir-locals.el /app/.dir-locals.el.bk
 
