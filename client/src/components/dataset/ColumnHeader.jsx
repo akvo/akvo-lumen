d9ead46c5fd13190824a9c7891eca31a401b73dc
Martin Christov <martin.christov@gmail.com>
2020-10-13 10:51:18 +0300

    [#2707] cleanup

--- client/src/components/dataset/ColumnHeader.jsx
+++ client/src/components/dataset/ColumnHeader.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
-// import { FormattedMessage } from 'react-intl';
 import { MdExpandMore, MdExpandLess } from 'react-icons/md';
 import typeIconMap from './TypeIconMap';
 
