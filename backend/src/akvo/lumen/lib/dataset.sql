0fec673083a36c1c61f5086a885e5aa3f1ccb6ba
Juan A. Ruz <juanantonioruz@gmail.com>
2020-10-30 14:18:37 +0100

    [#2756] Add fallback if default value for all datagroups

--- backend/src/akvo/lumen/lib/dataset.sql
+++ backend/src/akvo/lumen/lib/dataset.sql
@@ -149,7 +149,7 @@ SELECT data_group.table_name AS "table-name",
    AND dataset_version_2.version=(SELECT max(version)
                                     FROM dataset_version_2
                                    WHERE dataset_id= :dataset-id)
-ORDER BY data_group.group_order;
+ORDER BY data_group.group_order, data_group.created;
 
 
 -- :name db-table-name-and-columns-by-dataset-id :? :1
