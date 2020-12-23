abdc2e8f5dcdd2b7c1847716baa1567cc12e6d35
Juan A. Ruz <juanantonioruz@gmail.com>
2020-12-23 15:03:15 +0100

    [#3083] Dispatch logic on data-groups flag and adapt merge-dataset consistency with data-groups

--- backend/src/akvo/lumen/lib/dataset.clj
+++ backend/src/akvo/lumen/lib/dataset.clj
@@ -208,7 +208,9 @@
   [tenant-conn caddisfly import-config error-tracker dataset-id {:strs [token email]}]
   (if-let [{data-source-spec :spec
             data-source-id   :id} (db.dataset/data-source-by-dataset-id tenant-conn {:dataset-id dataset-id})]
-    (if-let [error (transformation.merge-datasets/consistency-error? tenant-conn dataset-id)]
+    (if-let [error (if (get (env/all tenant-conn) "data-groups")
+                     (transformation.merge-datasets/consistency-error-2? tenant-conn dataset-id)
+                     (transformation.merge-datasets/consistency-error? tenant-conn dataset-id))]
       (lib/conflict error)
       (if-not (= (get-in data-source-spec ["source" "kind"]) "DATA_FILE")
         (update/update-dataset tenant-conn caddisfly import-config error-tracker dataset-id data-source-id
