c1297858729682f37afcd3ed86c2925e1e4e37ab
Juan A. Ruz <juanantonioruz@gmail.com>
2020-12-31 18:00:32 +0100

    Utils to write/read flow dataset to/from edn file
    So we can use these edn files to import/update in dev/tes envs

--- backend/dev/src/dev.clj
+++ backend/dev/src/dev.clj
@@ -1,6 +1,8 @@
 (ns dev
   (:refer-clojure :exclude [test])
   (:require [akvo.lumen.endpoint.commons]
+            [akvo.lumen.db.dataset :as db.dataset]
+            [akvo.lumen.lib.import.flow :as flow]
             [akvo.lumen.lib.aes :as aes]
             [akvo.lumen.migrate :as lumen-migrate]
             [akvo.lumen.db.env :as env]
@@ -24,7 +26,8 @@
             [integrant.core :as ig]
             [integrant.repl :as ir]
             [integrant.repl.state :as state :refer (system)])
-  (:import [org.postgresql.util PSQLException PGobject]))
+  (:import [org.postgresql.util PSQLException PGobject]
+           [java.time Instant]))
 
 (defn check-specs! []
   (log/warn "instrumenting specs!")
@@ -111,3 +114,45 @@
 
 (defn deactivate-flag [flag]
   (env/deactivate-flag (db-conn) flag))
+
+(defn read-edn-filename
+  "file should live in resources"
+  [filename]
+  (let [x (->> filename
+               (clojure.java.io/resource)
+               (clojure.java.io/file)
+               (slurp))]
+   ;;binding  [*default-data-reader-fn* tagged-literal]
+   (edn/read-string {:readers {'object (fn [o]
+                                         (condp = (first o)
+                                           'java.time.Instant (Instant/parse (last o))))}}
+                    (format "[%s]" x))))
+
+(defn read-edn-flow-dataset [instance survey-id form-id]
+  (let [base-name (format "%s-%s-%s" instance survey-id form-id)]
+   {:columns-v3 (read-edn-filename (format "%s-%s-%s.edn" base-name "cols" 3))
+    :columns-v4 (read-edn-filename (format "%s-%s-%s.edn" base-name "cols" 4))
+    :records-v3 (read-edn-filename (format "%s-%s-%s.edn" base-name "rows" 3))
+    :records-v4 (read-edn-filename (format "%s-%s-%s.edn" base-name "rows" 4))}))
+
+(comment
+  (read-edn-filename "uat1-638889127-638879132-cols-3.edn")
+  (with-redefs [flow/adapter (fn [file version rows-cols col*]
+                           (map (fn [data](let [file-name (format "./dev/resources/%s-%s-%s.edn" file (name rows-cols) version)]
+                                            (spit file-name data :append true)
+                                            data)) col*))
+            ]
+    (def dataset-id (tu/import-file (db-conn)
+                                    (:akvo.lumen.utils.local-error-tracker/local system)
+                                    {:dataset-name "dataset-name"
+                                     :kind "clj-flow"
+                                     :data (read-edn-flow-dataset "uat1" "638889127" "638879132")})))
+
+  (def dataset-id-updated (tu/update-file (db-conn)
+                                         (:akvo.lumen.component.caddisfly/local system)
+                                         (:akvo.lumen.utils.local-error-tracker/local system)
+                                         dataset-id
+                                         (:id (db.dataset/data-source-by-dataset-id (db-conn) {:dataset-id dataset-id}))
+                                         {:dataset-name "dataset-name"
+                                          :kind "clj-flow"
+                                          :data (read-edn-flow-dataset "uat1" "638889127" "638879132")})))
