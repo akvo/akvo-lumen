4a895b8c24ab503fef07b9ce2f17e56b7ddfbf10
Mac <mac@akpan.local>
2021-06-09 12:22:10 +0100

    fixed color picker positioning

--- client/src/components/common/ColorPicker.jsx
+++ client/src/components/common/ColorPicker.jsx
@@ -9,7 +9,7 @@ import LegendShape from '../charts/LegendShape';
 const ColorPicker = ({ title, left = 0, top = 0, placement, style, hideArrow, ...rest }) => (
   <Popover
     left={left}
-    top={top}
+    top={0}
     placement={placement}
     hideArrow={hideArrow}
     className="color-picker-popover"
