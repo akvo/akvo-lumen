df970d93adc929f7247903ac56f3c9ab98d05057
Juan A. Ruz <juanantonioruz@gmail.com>
2021-06-24 12:17:43 +0200

    Update notification stream name

--- ci/helpers/generate-team-notification.sh
+++ ci/helpers/generate-team-notification.sh
@@ -7,4 +7,4 @@ COLOR=$4
 
 docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
   -it akvo/akvo-devops:20201023.101935.7dacd92 \
-  generate-zulip-notification.sh "${OLDER_GIT_VERSION}" "${NEWEST_GIT_VERSION}" "${MSG}" "${COLOR}" "anything" "akvo-lumen" "K2 Engine"
+  generate-zulip-notification.sh "${OLDER_GIT_VERSION}" "${NEWEST_GIT_VERSION}" "${MSG}" "${COLOR}" "anything" "akvo-lumen" "Flumen"
