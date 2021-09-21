0ca50f653d2769d747a58af7c8314c36d9795388
Juan A. Ruz <juanantonioruz@gmail.com>
2021-09-21 12:57:35 +0200

    [#3158] Add db persisted_view and persisted_view_data_group funs

--- backend/src/akvo/lumen/db/persisted_view.clj
+++ backend/src/akvo/lumen/db/persisted_view.clj
@@ -1,20 +1,36 @@
 (ns akvo.lumen.db.persisted-view
   (:require [hugsql.core :as hugsql]
             [clojure.java.jdbc :as jdbc]
+            [akvo.lumen.util :as util]
             [clojure.tools.logging :as log]))
 
 (hugsql/def-db-fns "akvo/lumen/db/persisted_view.sql")
 
-(defn insert-persisted-view [db-conn id {:keys [visualisation-id dataset-version-id data-groups]}]
+(defn insert-persisted-view [db-conn {:keys [visualisation-id dataset-version-id data-groups]}]
+  (let [id (util/squuid)]
+    (jdbc/with-db-transaction [conn db-conn]
+      (db-insert-persisted-view
+       conn
+       {:id id
+        :visualisation-id visualisation-id
+        :dataset-version-id dataset-version-id})
+      (mapv #(db-insert-persisted-view-data-group
+              conn
+              {:id (akvo.lumen.util/squuid)
+               :data-group-id (:id %)
+               :persisted-view-id id}) data-groups))
+    (str id)))
+
+(defn update-persisted-view [db-conn {:keys [visualisation-id dataset-version-id data-groups id]}]
   (jdbc/with-db-transaction [conn db-conn]
-    (db-insert-persisted-view
-     conn
-     {:id id
-      :visualisation-id visualisation-id
-      :dataset-version-id dataset-version-id})
+    (log/error :data-groups data-groups :id id)
+    (db-delete-all-persistent-view-data-groups conn {:id id})
     (mapv #(db-insert-persisted-view-data-group
             conn
             {:id (akvo.lumen.util/squuid)
              :data-group-id (:id %)
              :persisted-view-id id}) data-groups))
   (str id))
+
+(defn get-persisted-view [db-conn {:keys [visualisation-id dataset-version-id] :as opts}]
+  (db-get-persisted-view db-conn opts))
--- backend/src/akvo/lumen/db/persisted_view.sql
+++ backend/src/akvo/lumen/db/persisted_view.sql
@@ -1,10 +1,10 @@
 -- :name db-get-persisted-view :? :*
--- :doc All persisted views by viz-is
+-- :doc All persisted views by viz-is and dsv-id
    SELECT persisted_view.id, data_group.columns, data_group.group_id as "group-id", data_group.group_name as "group-name", data_group.table_name as "table-name"
    FROM persisted_view
    JOIN persisted_view_data_group ON persisted_view.id=persisted_view_id
    JOIN data_group ON data_group.id=data_group_id
-   where visualisation_id=:id;
+   where visualisation_id=:visualisation-id and persisted_view.dataset_version_id=:dataset-version-id;
 
 -- :name db-insert-persisted-view :! :n
 -- :doc insert persisted view
@@ -14,3 +14,7 @@ insert into persisted_view(id, visualisation_id, dataset_version_id) VALUES(:id,
 -- :name db-insert-persisted-view-data-group :! :n
 -- :doc insert persisted view data-group
 insert into persisted_view_data_group(id, data_group_id, persisted_view_id) VALUES(:id, :data-group-id, :persisted-view-id)
+
+-- :name db-delete-all-persistent-view-data-groups :! :n
+-- :doc delete all persistent view data-groups
+delete from persisted_view_data_group where persisted_view_id=:id;
