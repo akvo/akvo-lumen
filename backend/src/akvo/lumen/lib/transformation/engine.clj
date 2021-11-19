eb8cd13c4452a77b331381c10947f23e01aa08ba
Juan A. Ruz <juanantonioruz@gmail.com>
2021-11-19 14:21:29 +0100

    Avoid consistency column-title-error until specification

--- backend/src/akvo/lumen/lib/transformation/engine.clj
+++ backend/src/akvo/lumen/lib/transformation/engine.clj
@@ -538,6 +538,6 @@
        :transformations  (w/keywordize-keys (vec applied-txs))})))
 
 (defn column-title-error? [column-title columns]
-  (when (not-empty (filter #(= column-title (get % "title")) columns))
-    #_{:success? false
+  #_(when (not-empty (filter #(= column-title (get % "title")) columns))
+    {:success? false
      :message  (format "In this dataset there's already a column with this name: %s. Please choose another non existing name" column-title)}))
